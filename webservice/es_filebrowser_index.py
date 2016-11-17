173bbdc00ae1744564278af9d87b1ee4927ef97e
Carlos Espinosa <caaespin@ucsc.edu>
2016-11-16 17:25:58 -0800

    Added the script to pull from ES and re-index so it fits the browser

---
+++ webservice/es_filebrowser_index.py
@@ -0,0 +1,44 @@
+#indexer_es_fb.py
+
+#start virtual environment
+#>>> pip install jsonlines, ast, json
+#need to be in same folder as validated.jsonl
+#>>> python2.7 indexer_es_fb.py
+#produces a fb_index.jsonl file to be added to elasticsearch
+
+import jsonlines, ast, json
+
+counter = 0;
+with open("fb_index.jsonl", "w") as fb_index: 
+   #metadata = open("validated.jsonl", "r")
+   with jsonlines.open("validated.jsonl") as reader:
+      for obj in reader:
+         #pull out center name, project, program, donor(submitter_donor_id)
+         center_name = obj['center_name']
+         project = obj['project']
+         program = obj['program']
+         donor = obj['submitter_donor_id']
+         #go to specimen
+         for speci in obj['specimen']:
+            #pull out specimen_type(submitter_specimen_type)
+            specimen_type = speci['submitter_specimen_type']
+            for sample in speci['samples']:
+               for analys in sample['analysis']:
+                  # pull out analysis_type, workflow(workflow_name), download_id(bundle_uuid)
+                  analysis_type = analys['analysis_type']
+                  workflow = analys['workflow_name']
+                  download_id = analys['bundle_uuid']
+                  for file in analys['workflow_outputs']:
+                     #pull out file_type, title(file_path)
+                     file_type = file['file_type']
+                     title = file['file_path']
+                     #creating the header
+                     indexing = {"index":{"_id": counter, "_type":"meta"}}
+                     indexing = str(indexing).replace("'",'"')
+                     counter += 1
+                     #add all stuff to dictionary
+                     udict = {'center_name': center_name, 'project': project, 'program': program, 'donor': donor, 'specimen_type': specimen_type, 'analysis_type': analysis_type, 'workflow': workflow, 'download_id': download_id, 'file_type': file_type, 'title': title}
+                     adict = ast.literal_eval(json.dumps(udict))
+                     adict = str(adict).replace("'",'"')
+                     #push header and dictionary to .jsonl
+                     fb_index.write(indexing+"\n"+adict+"\n")
