9216390e2a54cd437f34912b00da52f02ee52492
alex-hancock <alexbhancock@gmail.com>
2016-11-17 17:20:52 -0800

    * Changed request.py to append all objects in luigi to elasticsearch.jsonl.   In future, will automate using elasticsearch library, once the index is a   fixture. * Does not contain any check for duplication or change in previous pipelines. * Added small changes to spawned and run for testing purposes.

--- webservice/luigi-interface/request.py
+++ webservice/luigi-interface/request.py
@@ -1,32 +1,31 @@
 import json
 import urllib2
 
-'''
+"""
 TODO:
 * How to capture stdout/stderr?
-'''
+"""
 
 def get_error(server, job_id):    
-    error_url = server + 'fetch_error?data=%7B%22task_id%22%3A%22' + job_id + '%22%7D'
+    error_url = server + "fetch_error?data=%7B%22task_id%22%3A%22" + job_id + "%22%7D"
     req_data = urllib2.Request(error_url)
     response_data = urllib2.urlopen(req_data)
     text_data = response_data.read()
     json_data = json.loads(text_data)
 
-    err_parameters = json_data['response']
-    return err_parameters['error']
+    err_parameters = json_data["response"]
+    return err_parameters["error"]
 
-server = 'http://localhost:8082/api/'
+server = "http://localhost:8082/api/"
 
-running_url   = server + 'task_list?data=%7B%22status%22%3A%22RUNNING%22%2C%22upstream_status%22%3A%22%22%2C%22search%22%3A%22%22%7D'
-batch_url     = server + 'task_list?data=%7B%22status%22%3A%22BATCH_RUNNING%22%2C%22upstream_status%22%3A%22%22%2C%22search%22%3A%22%22%7D'
-failed_url    = server + 'task_list?data=%7B%22status%22%3A%22FAILED%22%2C%22upstream_status%22%3A%22%22%2C%22search%22%3A%22%22%7D'
-upfail_url    = server + 'task_list?data=%7B%22status%22%3A%22PENDING%22%2C%22upstream_status%22%3A%22UPSTREAM_FAILED%22%2C%22search%22%3A%22%22%7D'
-disable_url   = server + 'task_list?data=%7B%22status%22%3A%22DISABLED%22%2C%22upstream_status%22%3A%22%22%2C%22search%22%3A%22%22%7D'
-updisable_url = server + 'task_list?data=%7B%22status%22%3A%22PENDING%22%2C%22upstream_status%22%3A%22UPSTREAM_DISABLED%22%2C%22search%22%3A%22%22%7D'
-pending_url   = server + 'task_list?data=%7B%22status%22%3A%22PENDING%22%2C%22upstream_status%22%3A%22%22%2C%22search%22%3A%22%22%7D'
-done_url      = server + 'task_list?data=%7B%22status%22%3A%22DONE%22%2C%22upstream_status%22%3A%22%22%2C%22search%22%3A%22%22%7D'
-
+running_url   = server + "task_list?data=%7B%22status%22%3A%22RUNNING%22%2C%22upstream_status%22%3A%22%22%2C%22search%22%3A%22%22%7D"
+batch_url     = server + "task_list?data=%7B%22status%22%3A%22BATCH_RUNNING%22%2C%22upstream_status%22%3A%22%22%2C%22search%22%3A%22%22%7D"
+failed_url    = server + "task_list?data=%7B%22status%22%3A%22FAILED%22%2C%22upstream_status%22%3A%22%22%2C%22search%22%3A%22%22%7D"
+upfail_url    = server + "task_list?data=%7B%22status%22%3A%22PENDING%22%2C%22upstream_status%22%3A%22UPSTREAM_FAILED%22%2C%22search%22%3A%22%22%7D"
+disable_url   = server + "task_list?data=%7B%22status%22%3A%22DISABLED%22%2C%22upstream_status%22%3A%22%22%2C%22search%22%3A%22%22%7D"
+updisable_url = server + "task_list?data=%7B%22status%22%3A%22PENDING%22%2C%22upstream_status%22%3A%22UPSTREAM_DISABLED%22%2C%22search%22%3A%22%22%7D"
+pending_url   = server + "task_list?data=%7B%22status%22%3A%22PENDING%22%2C%22upstream_status%22%3A%22%22%2C%22search%22%3A%22%22%7D"
+done_url      = server + "task_list?data=%7B%22status%22%3A%22DONE%22%2C%22upstream_status%22%3A%22%22%2C%22search%22%3A%22%22%7D"
 
 list_of_URLs = [running_url, batch_url, failed_url, upfail_url, 
                 disable_url, updisable_url, pending_url, done_url]
@@ -35,17 +34,20 @@ relevant_attributes = ["status", "name", "start_time", "params"]
 
 required_parameters = ["project", "donor_id", "sample_id", "pipeline_name"]
 
+esjson = open("elasticsearch.jsonl","a")
+counter = 0
+
 for URL in list_of_URLs:
+
     name = URL[62:]
-    suffix = ''
-    if 'UPSTREAM' in name:
-        if 'FAILED' in name:
-            name = 'UPSTREAM_FAILED'
+    suffix = ""
+    if "UPSTREAM" in name:
+        if "FAILED" in name:
+            name = "UPSTREAM_FAILED"
         else:
-            name = 'UPSTREAM_DISABLED'
+            name = "UPSTREAM_DISABLED"
     else:
-        name = name.split('%')[0] + suffix
-    print "\n", name
+        name = name.split("%")[0] + suffix
 
     error_text = None
 
@@ -55,31 +57,42 @@ for URL in list_of_URLs:
     text_tools = response.read()
     json_tools = json.loads(text_tools)
 
-    job_list = json_tools['response']
+    job_list = json_tools["response"]
 
     if not job_list:
         # Just skip an empty response
         continue
 
+    jobject = {}
+    jobject["name"] = name
+    #print "\n", jobject["name"]
+    jobject["error_text"] = error_text
+
+
     for job_id in job_list:
 
         # Get error information
-        if job_list[job_id]['status'] == "FAILED":
-            error_text = get_error(server, job_id)
+        if job_list[job_id]["status"] == "FAILED":
+            jobject["error_text"] = get_error(server, job_id)
             
         for attr in relevant_attributes:
 
             node = job_list[job_id][attr]
 
-            if attr == 'params':
-                for parameter in node:
-                    print parameter, node[parameter]
+            if attr == "params":
+                for parameter in required_parameters:
+                    jobject[parameter] = job_list[job_id]["params"][parameter]
             else:
-                print attr, node
+                jobject[attr] = node
 
-        print "Error:", error_text
-        print "\n"
+        header = {}
+        header["index"] = {}
+        header["index"]["_id"] = str(counter)
+        header["index"]["_type"] = "meta"
+        esjson.write(json.dumps(header) + "\n")
+        esjson.write(json.dumps(jobject) + "\n")
+        counter += 1
 
+        # Now, we need to purge the records.
 
-
-
+esjson.close()
--- webservice/luigi-interface/spawned.py
+++ webservice/luigi-interface/spawned.py
@@ -2,16 +2,25 @@ import luigi
 from sys import exit
 from time import sleep
 
+from elasticsearch import Elasticsearch
+
+es = Elasticsearch()
+
+
 class spawnFlop(luigi.Task):
-	integer = luigi.IntParameter(default=0)
+	project = luigi.Parameter(default=None)
+	donor_id = luigi.Parameter(default=None)
+	sample_id = luigi.Parameter(default=None)
+	pipeline_name = luigi.Parameter(default=None)
 
 	def run(self):
-		sleep(5)
+		print self.project
+		print self.donor_id
+		print self.sample_id
+		print self.pipeline_name
 
-		print "Hello, World!"
-
-		if self.integer % 2:
-			exit()
+		if int(self.donor_id) % 2:
+			raise Exception("This was a failure.")
 
 if __name__ == '__main__':
-    luigi.run(["--scheduler-remove-delay","604800"])
+    luigi.run()
