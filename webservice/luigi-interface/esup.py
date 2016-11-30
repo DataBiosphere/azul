7f8bfa443563cae8691eb92f43fd8b79c64c074c
alex-hancock <alexbhancock@gmail.com>
2016-11-29 16:58:35 -0800

    Added function to update elasticsearch index. Testing begins soon.

--- webservice/luigi-interface/esup.py
+++ webservice/luigi-interface/esup.py
@@ -9,7 +9,7 @@ print "Elasticsearch server at %s:%s exists?: %s\n" % \
 query_body = {
 	'query': {
 		'match': {
-			'sample_id': 5
+			'sample_id': 11
 		}
 	}
 }
@@ -17,25 +17,25 @@ query_body = {
 json_return = es.search(index='luigi_index', body=query_body)
 # hits hits 0 source gets through arbitrary elasticsearch juju to 
 # the returns from the Luigi Job
-json_return = json_return["hits"]["hits"][0]["_source"]
-string_of_return = json.dumps(json_return)
+json_return = json_return["hits"]["hits"]
 
-print string_of_return
+if not json_return:
+	print "This is empty!"
 
-for key in json_return:
-	print key
+#for key in json_return:
+#	print key
 
 # 
 # elasticsearch_server is a json dictionary of the form  
 # {
-# 	'host':
-# 	'port': 
+# 	'host': /*host here*/
+# 	'port': /*port here*/
 # }
 # 
 # jobject should be a json dictionary of with information
 # from Luigi. It should have the following attributes:
 # 	* status 
-# 	* name 
+# 	* job_id
 # 	* start_time 
 # 	* project 
 # 	* donor_id 
@@ -51,17 +51,29 @@ def update_index(es_server, jobject):
 	server_up = es.ping()
 
 	if not server_up:
+		print "Server's down, head for the hills."
 		return 1
 
-	
 	# DEBUG: Is server up?
 	# print "Elasticsearch server at %s:%s exists?: %s\n" % (es_server['host'], str(es_server['port']), )
 
+	# Okay, now we have a jobject to add to the es_server
+	query_body = {
+		'query': {
+			'match': {
+				'job_id': jobject['job_id']
+			}
+		}
+	}
 
+	json_return = es.search(index='luigi_index', body=query_body)
+	json_return = json_return["hits"]["hits"]
 
-
-
-
-
+	if json_return:
+		# The job exists! 
+		 es.update(index='luigi_index', doc_type='job', id=jobject['job_id'], body={"doc": jobject})
+	else:
+		 # Job doesn't exist. Insert the jobject, go on your merry way
+		 es.index(index='luigi_index', doc_type='job', id=jobject['job_id'], body=jobject)
 
 
