93d4a8c04ffc7ef5b9b5a3fecb817df204a95084
alex-hancock <alexbhancock@gmail.com>
2017-02-21 18:28:53 -0800

    Finalized query for use in dashboard API.

--- webservice/luigi-interface/script_dev/query.py
+++ webservice/luigi-interface/script_dev/query.py
@@ -1,55 +1,37 @@
 # Alex Hancock, UCSC CGL
 
 import json
-from sqlalchemy  import *
+from sqlalchemy import create_engine, MetaData, String, Table, Float, Column, select
 
-def proxyConversion(resultProxy):
-	save = []
-	for row in resultProxy:
-		save.append(row)
-	return save
-
-#
-# Database initialization, creation if table doesn't exist
-#
-# Change echo to True to show SQL code... unnecessary
-# Any time facets?
-# When started:
-# When ended:
-# Last touched:
-# Time elapsed:
 db = create_engine('postgresql:///monitor', echo=False)
-db = create_engine('postgresql:///hancock', echo=False)
 conn = db.connect()
 metadata = MetaData(db)
 luigi = Table('luigi', metadata,
 	Column("luigi_job", String(100), primary_key=True),
 	Column("status", String(20)),
-	Column("submitter_specimen_id", String(40)),
-	Column("specimen_uuid", String(60)),
-	Column("workflow_name", String(40)),
-	Column("center_name", String(40)),
-	Column("submitter_donor_id", String(40)),
-	Column("consonance_id", String(40)),
-	#Column("consonance_job_uuid", String(40)),
-	Column("submitter_donor_primary_site", String(40)),
-	Column("project", String(40)),
-	Column("analysis_type", String(40)),
-	Column("program", String(40)),
-	Column("donor_uuid", String(60)),
-	Column("submitter_sample_id", String(40)),
-	Column("submitter_experimental_design", String(40)),
-	Column("submitter_specimen_type", String(40)),
-	Column("workflow_version", String(40)),
-	Column("sample_uuid", String(60)),
+
+	Column("submitter_specimen_id", String(100)),
+	Column("specimen_uuid", String(100)),
+	Column("workflow_name", String(100)),
+	Column("center_name", String(100)),
+	Column("submitter_donor_id", String(100)),
+	Column("consonance_job_uuid", String(100)),
+	Column("submitter_donor_primary_site", String(100)),
+	Column("project", String(100)),
+	Column("analysis_type", String(100)),
+	Column("program", String(100)),
+	Column("donor_uuid", String(100)),
+	Column("submitter_sample_id", String(100)),
+	Column("submitter_experimental_design", String(100)),
+	Column("submitter_specimen_type", String(100)),
+	Column("workflow_version", String(100)),
+	Column("sample_uuid", String(100)),
 
 	Column("start_time", Float),
 	Column("last_updated", Float)
 )
-if not db.dialect.has_table(db, luigi):
-	luigi.create()
 
 select_query = select([luigi])
-select_exist_result = conn.execute(select_query)
-for row in select_exist_result:
-	print row
+select_result = conn.execute(select_query)
+result_list = [dict(row) for row in select_result]
+print result_list
