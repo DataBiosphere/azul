665a73ca84f4d07412a2943d093d05d9c1bc36c8
caaespin <caaespin@ucsc.edu>
2017-04-05 03:15:33 +0000

    Modifying supervisord config so it initializes the billing db

--- webservice/run.sh
+++ webservice/run.sh
@@ -1,10 +1,22 @@
 #!/bin/sh
 
-while ! pg_isready -U postgres -h db
+while ! pg_isready -U ${POSTGRES_USER} -h db
 do
 echo "$(date) - waiting for database to start"
     sleep 2
 done
+
+echo "Action Service Database Ready "
+
+while ! pg_isready -U ${B_POSTGRES_USER} -h boardwalk-billing
+do
+echo "$(date) - waiting for database to start"
+    sleep 2
+done
+
+echo "Billing Service Database Ready"
+
+flask db upgrade
 
 #sed -i "s/___name___/$AWS_PROFILE/g" /etc/boto.cfg/credentials
 #sed -i "s/___awskey___/$AWS_ACCESS_KEY_ID/g" /etc/boto.cfg/credentials
