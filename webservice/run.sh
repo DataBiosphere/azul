41d5721ef2ec49d6b693f0097d8e7cd141dbe352
alex-hancock <alexbhancock@gmail.com>
2017-04-13 20:07:16 +0000

    Removed comments for deprecated boto privilege editing

--- webservice/run.sh
+++ webservice/run.sh
@@ -3,8 +3,6 @@ echo "now substituting variables in Consonance config"
 
 sed -i "s~___CONSONANCE_ADDRESS___~$CONSONANCE_ADDRESS~g" /root/.consonance/config
 sed -i "s/___CONSONANCE_TOKEN___/$CONSONANCE_TOKEN/g" /root/.consonance/config
-#sed -i "s/___awskey___/$AWS_ACCESS_KEY_ID/g" /etc/boto.cfg/credentials
-#sed -i "s/___secretkey___/$AWS_SECRET_ACCESS_KEY/g" /etc/boto.cfg/credentials
 
 echo "Substitution complete. File output:"
 cat /root/.consonance/config
