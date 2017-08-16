778f019eb2807b7687307ae6fea7842df87c9249
caaespin <caaespin@ucsc.edu>
2017-08-15 17:48:53 -0700

    Code for doing the mapping between JSON file and api response

--- webservice/api_response.py
+++ webservice/api_response.py
@@ -70,8 +70,7 @@ class OtherObj(JsonObject):
     """
     Class defining OtherObj in the HitEntry object
     """
-    tcgaSampleBarcode = ListProperty(StringProperty)
-    tcgaAliquotBarcode = ListProperty(StringProperty)
+    redwoodDonorUUID = ListProperty(StringProperty)
 
 
 class DonorObj(JsonObject):
@@ -153,21 +152,76 @@ class KeywordSearchResponse(AbstractResponse):
     Class for the keyword search response. Based on the AbstractResponse class
     """
     def return_response(self):
-        pass
+        return self.apiResponse
 
     def fetch_entry_value(self, mapping, entry, key):
+        """
+        Helper method for getting the value of key on the mapping
+        :param mapping: Mapping in question. Values should be at the root level
+        :param entry: Dictionary where the contents are to be looking for in
+        :param key: Key to be used to get the right value
+        :return: Returns entry[mapping[key]] if present. Other
+        """
         if mapping[key]:
             return entry[mapping[key]]
         else:
             return None
 
     def make_data_categorization(self, mapping, entry):
+        """
+        Returns a DataCategorizationObj based on the mapping entry params
+        :param mapping: The mapping for the object
+        :param entry: The entry in ElasticSearch containing the results.
+        :return: Returns a DataCategorizationObj
+        """
         return DataCategorizationObj(
             dataType=self.fetch_entry_value(mapping, entry, 'dataType'),
             experimentalStrategy=self.fetch_entry_value(mapping, entry, 'experimentalStrategy')
         )
 
+    def make_analysis_method(self, mapping, entry):
+        """
+        Returns an AnalysisObj based on the mapping entry params
+        :param mapping: The mapping for the object
+        :param entry: The entry in ElasticSearch containing the results.
+        :return: Returns an AnalysisObj
+        """
+        return AnalysisObj(
+            analysisType=self.fetch_entry_value(mapping, entry, 'analysisType'),
+            software=self.fetch_entry_value(mapping, entry, 'software')
+        )
+
+    def make_reference_genome(self, mapping, entry):
+        """
+        Returns a ReferenceGenomeObj based on the mapping entry params
+        :param mapping: The mapping for the object
+        :param entry: The entry in ElasticSearch containing the results.
+        :return: Returns a ReferenceGenomeObj
+        """
+        return ReferenceGenomeObj(
+            genomeBuild=self.fetch_entry_value(mapping, entry, 'genomeBuild'),
+            referenceName=self.fetch_entry_value(mapping, entry, 'referenceName'),
+            downloadUrl=self.fetch_entry_value(mapping, entry, 'downloadUrl')
+        )
+
+    def make_other_obj(self, mapping, entry):
+        """
+        Returns an OtherObj based on the mapping entry params
+        :param mapping: The mapping for the object
+        :param entry: The entry in ElasticSearch containing the results.
+        :return: Returns an OtherObj
+        """
+        return OtherObj(
+            redwoodDonorUUID=[self.fetch_entry_value(mapping, entry, 'RedwoodDonorUUID')]
+        )
+
     def make_file_copy(self, mapping, entry):
+        """
+        Returns a FileCopyObj based on the mapping entry params
+        :param mapping: The mapping for the object
+        :param entry: The entry in ElasticSearch containing the results.
+        :return: Returns a FileCopyObj
+        """
         return FileCopyObj(
             repoDataBundleId=self.fetch_entry_value(mapping, entry, 'repoDataBundleId'),
             repoDataSetIds=[self.fetch_entry_value(mapping, entry, 'repoDataSetIds')],
@@ -186,6 +240,27 @@ class KeywordSearchResponse(AbstractResponse):
             lastModified=self.fetch_entry_value(mapping, entry, 'lastModified')
         )
 
+    def make_donor(self, mapping, entry):
+        """
+        Returns a DonorObj based on the mapping entry params
+        :param mapping: The mapping for the object
+        :param entry: The entry in ElasticSearch containing the results.
+        :return: Returns a DonorObj
+        """
+        return DonorObj(
+            donorId=self.fetch_entry_value(mapping, entry, 'donorId'),
+            primarySite=self.fetch_entry_value(mapping, entry, 'primarySite'),
+            projectCode=self.fetch_entry_value(mapping, entry, 'projectCode'),
+            study=self.fetch_entry_value(mapping, entry, 'study'),
+            sampleId=[self.fetch_entry_value(mapping, entry, 'sampleId')],
+            specimenId=[self.fetch_entry_value(mapping, entry, 'specimenId')],
+            specimenType=[self.fetch_entry_value(mapping, entry, 'specimenType')],
+            submittedDonorId=self.fetch_entry_value(mapping, entry, 'submittedDonorId'),
+            submittedSampleId=[self.fetch_entry_value(mapping, entry, 'submittedSampleId')],
+            submittedSpecimenId=[self.fetch_entry_value(mapping, entry, 'submittedSpecimenId')],
+            otherIdentifiers=self.make_other_obj(mapping, entry)
+        )
+
     def map_entries(self, mapping, entry):
         """
         Returns a HitEntry Object. Takes the mapping and maps the appropriate fields from entry to
@@ -199,9 +274,13 @@ class KeywordSearchResponse(AbstractResponse):
             objectId=self.fetch_entry_value(mapping, entry, 'objectId'),
             access=self.fetch_entry_value(mapping, entry, 'access'),
             study=[self.fetch_entry_value(mapping, entry, 'study')],
-            dataCategorization=self.make_data_categorization(mapping, entry),
-            fileCopies=[self.make_file_copy(mapping, entry)],
-
+            dataCategorization=self.make_data_categorization(mapping['dataCategorization'], entry),
+            fileCopies=[self.make_file_copy(mapping['fileCopies'][0], entry)],
+            centerName=self.fetch_entry_value(mapping, entry, 'center_name'),
+            program=self.fetch_entry_value(mapping, entry, 'program'),
+            donors=[self.make_donor(mapping['donors'][0], entry)],
+            analysisMethod=self.make_analysis_method(mapping['analysisMethod'], entry),
+            referenceGenome=self.make_reference_genome(mapping['referenceGenome'], entry)
         )
         return mapped_entry
 
