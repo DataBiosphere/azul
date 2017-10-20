ed3a29468f1f39634fda57ccce9c895f79973115
caaespin <caaespin@ucsc.edu>
2017-10-20 13:16:31 -0700

    Apparently there is no .raw in the es-py-dsl. Instead, according to the issues page, you should use __ instead to access the raw field

--- webservice/responseobjects/elastic_request_builder.py
+++ webservice/responseobjects/elastic_request_builder.py
@@ -131,7 +131,7 @@ class ElasticTransformDump(object):
         # Do a post_filter using the filter query
         es_search = es_search.post_filter(es_filter_query)
         # Apply a prefix query with the query string
-        es_search = es_search.query(Q('prefix', **{'{}.raw'.format(search_field): _query}))
+        es_search = es_search.query(Q('prefix', **{'{}__raw'.format(search_field): _query}))
         return es_search
 
     @staticmethod
