31d0f5797c8331ab95170cd2e94b32672716fda7
Carlos Alberto Espinosa Méndez <carlos.espinosa805@gmail.com>
2017-11-16 16:58:41 -0800

    Update populator.sh

--- webservice/test/populator.sh
+++ webservice/test/populator.sh
@@ -26,10 +26,10 @@ curl -XPUT $ES_PROTOCOL://$ES_SERVICE:$ES_PORT/analysis_buffer/ -d @test/donor_s
 curl -XPUT $ES_PROTOCOL://$ES_SERVICE:$ES_PORT/analysis_buffer/_mapping/meta?update_all_types  -d @test/donor_mapping.json
 curl -XPUT $ES_PROTOCOL://$ES_SERVICE:$ES_PORT/analysis_buffer/_bulk?pretty --data-binary @test/donor_index.jsonl
 
-# Change alias to point to fb_buffer
+# Change alias to point to analysis_buffer
 curl -XPOST $ES_PROTOCOL://$ES_SERVICE:$ES_PORT/_aliases?pretty -d' { "actions" : [ { "remove" : { "index" : "analysis_real", "alias" : "fb_index" } }, { "add" : { "index" : "analysis_buffer", "alias" : "analysis_index" } } ] }'
 
-# Index/Update the data in fb_index
+# Index/Update the data in analysis_index
 curl -XDELETE $ES_PROTOCOL://$ES_SERVICE:$ES_PORT/analysis_real/
 curl -XPUT $ES_PROTOCOL://$ES_SERVICE:$ES_PORT/analysis_real/ -d @test/donor_settings.json
 curl -XPUT $ES_PROTOCOL://$ES_SERVICE:$ES_PORT/analysis_real/_mapping/meta?update_all_types  -d @test/donor_mapping.json
