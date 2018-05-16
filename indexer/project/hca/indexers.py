eefb5390553425d5623b4430a46277072df81b0a
caaespin <caaespin@ucsc.edu>
2018-05-16 17:31:29 -0400

    Pulled data from the metadata into a semiharmonized structure. Needs to be assambled and merged

--- indexer/project/hca/indexers.py
+++ indexer/project/hca/indexers.py
@@ -1,8 +1,12 @@
 from abc import ABC, abstractmethod
-from project.hca.extractors import * # <- TODO: CHANGE MY FRIEND
+from collections import defaultdict
+from itertools import chain, filterfalse, tee
 import jmespath
+import project.hca.extractors as extractors
 import re
-from typing import Mapping, Sequence
+from typing import Mapping, Sequence, Iterable
+
+# TODO: consider moving the current "create_specimens", etc. to "extract_speciments" and then make a new method called "assign_specimens", etc
 
 
 class ElasticSearchDocument:
@@ -23,6 +27,15 @@ class Transformer(ABC):
     def __init__(self):
         pass
 
+    @staticmethod
+    def partition(predicate, iterable):
+        """
+        Use a predicate to partition entries into false entries and
+        true entries
+        """
+        t1, t2 = tee(iterable)
+        return filterfalse(predicate, t1), filter(predicate, t2)
+
     @classmethod
     def get_version(cls, metadata_json: dict) -> str:
         schema_url = metadata_json["describedBy"]
@@ -39,7 +52,7 @@ class Transformer(ABC):
         pass
 
     @abstractmethod
-    def _create_samples(self, metadata_dictionary: dict) -> Sequence[dict]:
+    def _create_specimens(self, metadata_dictionary: dict) -> Sequence[dict]:
         pass
 
     @abstractmethod
@@ -64,21 +77,94 @@ class FileTransformer(Transformer):
             files_dictionary: dict,
             metadata_dictionary: dict=None
     ) -> Sequence[dict]:
-        if metadata_dictionary is None:
-            return [files_dictionary]
-        else:
+        # Handle the file.json if it's present
+        if metadata_dictionary is not None:
             metadata_version = self.get_version(metadata_dictionary)
-            extractor = getattr()
+            file_extractor = getattr(
+                extractors, "FileExtractor.v{}".format(metadata_version))
+            fields_from_metadata = file_extractor(metadata_dictionary)
+            for _file, contents in fields_from_metadata.items():
+                files_dictionary[_file].update(contents)
+        return [_file for _file in files_dictionary.values()]
 
-    def _create_samples(self, metadata_dictionary: dict) -> Sequence[dict]:
-        pass
+    def _create_specimens(self, metadata_dictionary: dict) -> Sequence[dict]:
+        metadata_version = self.get_version(metadata_dictionary)
+        biomaterial_extractor = getattr(
+            extractors, "BiomaterialExtractor.v{}".format(metadata_version))
+        biomaterials = [biomaterial_extractor(biomaterial) for biomaterial
+                        in metadata_dictionary["biomaterials"]]
+        # Now mangle the samples into separate entities as appropriate.
+        # Separate biomaterials into roots and not_roots
+        not_roots, roots = self.partition(
+            lambda x: "specimen_from_organism" in x["source"], biomaterials)
 
-    def _create_projects(self, metadata_dictionary: dict) -> Sequence[dict]:
-        pass
+        def find_descendants(nodes: Iterable[dict],
+                             parent_id: str=None) -> Iterable[dict]:
+                for child in filter(lambda x: parent_id in x["parent"], nodes):
+                    yield from find_descendants(nodes, child["biomaterial_id"])
+                    yield child
+
+        def find_ancestors(nodes: Iterable[dict],
+                           parent_id: str=None) -> Iterable[dict]:
+            for parent in filter(lambda x: parent_id in x["biomaterial_id"],
+                                 nodes):
+                if "parent" in parent:
+                    yield from find_ancestors(nodes, parent["parent"])
+                yield parent
+
+        # Add ancestors and descendants fields to each sample
+        samples_list = []
+        for root in roots:
+            ancestors = find_ancestors(not_roots, root["parent"])
+            descendants = find_descendants(not_roots, root["parent"])
+            root_id = root["biomaterial_id"]
+            merged_sample = defaultdict(list)
+            for node in chain(ancestors, descendants, root):
+                for key, value in node.items():
+                    merged_sample[key] += value
+            merged_sample["biomaterial_id"] = root_id
+            samples_list.append(merged_sample)
+
+
+        # I think that I can use a hash table with all the ids from links and
+        # That way figure out which processes are being used in this run
+
+        return samples_list
+
+    def _create_processes(self, metadata_dictionary: dict) -> Sequence[dict]:
+        metadata_version = self.get_version(metadata_dictionary)
+        process_extractor = getattr(
+            extractors, "ProcessExtractor.v{}".format(metadata_version))
+        processes = [process_extractor(process)
+                     for process in metadata_dictionary["processes"]]
+        return processes
+
+    def _create_protocols(self, metadata_dictionary: dict) -> Sequence[dict]:
+        metadata_version = self.get_version(metadata_dictionary)
+        protocol_extractor = getattr(
+            extractors, "ProtocolExtractor.v{}".format(metadata_version))
+        protocols = [protocol_extractor(protocol)
+                     for protocol in metadata_dictionary["protocols"]]
+        return protocols
+
+    def _create_project(self, metadata_dictionary: dict) -> dict:
+        metadata_version = self.get_version(metadata_dictionary)
+        project_extractor = getattr(
+            extractors, "ProjectExtractor.v{}".format(metadata_version))
+        project = project_extractor(metadata_dictionary)
+        return project
 
     def create_documents(
             self,
             metadata_files: Mapping[str, dict],
             data_files: Mapping[str, dict]
     ) -> Sequence[ElasticSearchDocument]:
+        # Maybe climb up the link.json by first starting at the entity level
+        # (start by using the file uuid)
         pass
+
+class SampleTransformer:
+    pass
+
+class ProjectTransformer:
+    pass
