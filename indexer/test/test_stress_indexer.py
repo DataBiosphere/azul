1f048520e4e4883824a32e60e31927f7ee663d0c
Jyotheeswar Arvind Manickavasagar <jyotheeshwar@gmail.com>
2018-05-17 10:17:24 -0400

    fixes #20 , stress test framework for query implemented

--- indexer/test/test_stress_indexer.py
+++ indexer/test/test_stress_indexer.py
@@ -12,24 +12,28 @@ import os
 #QUERY_GROUP priority
 INDEXER_QUERY_PRIORITY = 1
 
-TEST_INDEXER_URL = "/"
+TEST_INDEXER_URL = "/test_data/_search"
 
 class IndexerTester(TaskSet):
 
 	@task(INDEXER_QUERY_PRIORITY)
 	def query_indexer(self):
 
-		with self.client.post(TEST_INDEXER_URL, catch_response=True, name=TEST_INDEXER_URL, json={}) as response:
+		with self.client.post(TEST_INDEXER_URL, catch_response=True, name=TEST_INDEXER_URL, json={'query': {'match_all': {}}}) as response:
 			if 'error' in response.json():
 				response.error(INDEXER_QUERY_PRIORITY + " is not responding properly to a valid query")
 			else:
 				try:
-					return response.json()['key']
+					print (response.json())
+					#return response.json()['key']
 				except ValueError:
 					response.failure(TEST_INDEXER_URL + " did not return a json")
 
 
 class IndexerStress(HttpLocust):
+	host = "<YOUR_ES_HOST_URL_HERE>"
 	task_set = IndexerTester
+	#This sets the minimum time in ms between requests made by different sets of users
 	min_wait = 1000
+	#This sets the maximum time in ms between requests made by different sets of users
 	max_wait = 1000
