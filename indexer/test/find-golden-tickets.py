963bedc978d253b7121746552e9b65117c20b9b0
Michael Baumann <mbaumann@ucsc.edu>
2018-06-07 12:33:48 -0700

    Update to current version of `find_golden_tickets.py`
    Updated Commons branch with the current version of
    `find_golden_tickets.py` from the `develop` branch of this repo.

--- indexer/test/find-golden-tickets.py
+++ indexer/test/find-golden-tickets.py
@@ -1,86 +1,152 @@
-from future import standard_library
-standard_library.install_aliases()
-import os
-import json, ssl, argparse
-import threading
-import time
+# -*- coding: utf-8 -*-
+"""Command line utility to trigger indexing of bundles based on a query."""
+import argparse
+from collections import defaultdict
+from hca.dss import DSSClient
+import json
+from pprint import pprint
+from time import sleep
 from urllib.request import urlopen, Request
+from urllib.error import HTTPError
+from uuid import uuid4
 
-#es_service = os.environ.get("ES_SERVICE", "localhost")
 
-parser = argparse.ArgumentParser(description='Process options the finder of golden bundles.')
-#parser.add_argument('--assay-id', dest='assay_id', action='store',
-#                    default='Q3_DEMO-assay1', help='assay id')
-parser.add_argument('--dss-url', dest='dss_url', action='store',
-                    default='https://dss.staging.data.humancellatlas.org/v1/search?replica=aws', help='The url for the storage system.')
-parser.add_argument('--indexer-url', dest='repoCode', action='store',
-                    default='https://9b92wjnlgh.execute-api.us-west-2.amazonaws.com/dev/', help='The indexer URL')
+class DefaultProperties:
+    """Default Properties for this script."""
 
-#Get the arguments into args
+    def __init__(self):
+        """Initialize the default values."""
+        self._dss_url = "https://dss.staging.data.humancellatlas.org/v1"
+        self._indexer_url = "https://9b92wjnlgh.execute-api.us-west-2.\
+        amazonaws.com/dev/"
+        self._es_query = {"query": {"match_all": {}}}
+
+    @property
+    def dss_url(self):
+        """Return the url of the dss."""
+        return self._dss_url
+
+    @property
+    def indexer_url(self):
+        """Return the url of the indexer."""
+        return self._indexer_url
+
+    @property
+    def es_query(self):
+        """Return the ElasticSearch query."""
+        return self._es_query
+
+
+default = DefaultProperties()
+dss_client = DSSClient()
+
+parser = argparse.ArgumentParser(
+    description='Process options the finder of golden bundles.')
+parser.add_argument('--dss-url',
+                    dest='dss_url',
+                    action='store',
+                    default=default.dss_url,
+                    help='The url for the storage system.')
+parser.add_argument('--indexer-url',
+                    dest='indexer_url',
+                    action='store',
+                    default=default.indexer_url,
+                    help='The indexer URL')
+parser.add_argument('--es-query',
+                    dest='es_query',
+                    action='store',
+                    default=default.es_query,
+                    type=json.loads,
+                    help='The ElasticSearch query to use')
+
 args = parser.parse_args()
 
-# headers = {}
-#json_str = urlopen(requestConstructor(str("https://metadata."+redwood_host+"/entities?page=0"), headers), context=ctx).read()
 
-def requestConstructor(url, headers, data):
-    '''
-    Helper function to make requests to use on with urlopen()
-    '''
+def request_constructor(url, headers, data):
+    """Create a request using urlopen."""
     req = Request(url, data.encode('utf-8'))
     for key, value in list(headers.items()):
-         req.add_header(key, value)
-
+        req.add_header(key, value)
     return req
 
-def executeRequest(req):
-    '''
-    Helper function to make the post request to the indexer
-    '''
+
+def execute_request(req):
+    """Execute a request."""
     f = urlopen(req)
     response = f.read()
     f.close()
     return response
 
-def parseResultEntry(result_entry):
-    '''
-    Helper function to parse the results from a single results entry
-    '''
-    bundle_id = result_entry['bundle_id']
-    bundle_uuid = bundle_id[:36]
-    bundle_version = bundle_id[37:]
+
+def uuid_and_version(result_entry):
+    """Parse the search result entry into bundle uuid and bundle version."""
+    bundle_fqid = result_entry['bundle_fqid']
+    bundle_uuid = bundle_fqid.partition('.')[0]
+    bundle_version = bundle_fqid.partition('.')[2]
     return (bundle_uuid, bundle_version)
 
-def main():
-    '''
-    Main function which will carry out the execution of the program
-    '''
-    headers = {"accept": "application/json", "content-type": "application/json"}
-    data = json.dumps({"es_query": {"query": {"match": {"files.project_json.content.core.schema_version": "4.6.1"}}}})
-    req = requestConstructor(args.dss_url, headers, data)
-    response = executeRequest(req)
-    response = json.loads(response)
-    bundle_list = [parseResultEntry(x) for x in response['results']]
-    # Post to the indexer endpoint
-    headers = {"content-type": "application/json"}
-    # post_result = postToIndexer(bundle_list, args.repoCode, headers)
-    threads = []
-    for bundle, version in bundle_list:
-        data = json.dumps({ "query": { "query": { "match_all":{}} }, "subscription_id": "ba50df7b-5a97-4e87-b9ce-c0935a817f0b", "transaction_id": "ff6b7fa3-dc79-4a79-a313-296801de76b9", "match": { "bundle_version": version, "bundle_uuid": bundle } })
-        req = requestConstructor(args.repoCode, headers, data)
-        threads.append(threading.Thread(target=executeRequest, args=(req,)))
 
-        print("Bundle: {}, Version: {}".format(bundle, version))
-        try:
-            response = executeRequest(req)
-        except Exception as e:
-            print (e)
-    print("Total of {} bundles".format(len(bundle_list)))
-    start = time.time()
-#    for thread in threads:
-#        thread.start()
-#    for thread in threads:
-#        thread.join()
-#    print "Elapsed Time: %s" % (time.time() - start)
+def post_bundle(bundle_uuid, bundle_version):
+    """Send a fake BlueBox notification to the indexer."""
+    simulated_event = {
+        "query": args.es_query,
+        "subscription_id": str(uuid4()),
+        "transaction_id": str(uuid4()),
+        "match": {
+            "bundle_uuid": bundle_uuid,
+            "bundle_version": bundle_version
+        }
+    }
+    request = request_constructor(args.indexer_url,
+                                  {"content-type": "application/json"},
+                                  json.dumps(simulated_event))
+    execute_request(request)
+
+
+def main():
+    """Entrypoint method for the script."""
+    dss_client.host = args.dss_url
+    parameters = {
+        "es_query": args.es_query,
+        "replica": "aws"
+    }
+    # TODO: Need to write this so it scales nicely
+    bundle_list = [uuid_and_version(r)
+                   for r in dss_client.post_search.iterate(**parameters)]
+    errors = defaultdict(int)
+    missing = {}
+    indexed = 0
+    total = 0
+    for bundle_uuid, bundle_version in bundle_list:
+        total += 1
+        print("Bundle: {}, Version: {}".format(bundle_uuid, bundle_version))
+        retries = 3
+        while True:
+            try:
+                post_bundle(bundle_uuid, bundle_version)
+                indexed += 1
+            except HTTPError as er:
+                # Current retry didn't work. Try again
+                print("Error sending bundle to indexer:\n{}".format(er))
+                print("{} retries left".format(retries))
+                if retries > 0:
+                    retries -= 1
+                    sleep(retries)
+                else:
+                    print("Out of retries, there might be a problem.")
+                    print("Error:\n{}".format(er))
+                    errors[er.code] += 1
+                    bundle_fqid = "{}.{}".format(bundle_uuid, bundle_version)
+                    missing[bundle_fqid] = er.code
+                    break
+            else:
+                break
+    print("Total of bundles read: {}".format(total))
+    print("Total of {} bundles indexed".format(indexed))
+    print("Total number of errors by code:")
+    pprint(errors)
+    print("Missing bundles and their error code:")
+    pprint(missing)
 
 
 if __name__ == "__main__":
