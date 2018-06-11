c022c129831de116e19de3eee21a833c009e2d7a
Jim Vlasblom <jim@dnastack.com>
2018-06-11 13:19:16 -0400

    Deleted commented out code and test file.

--- webservice/chalicelib/responseobjects/hca_response_v5.py
+++ webservice/chalicelib/responseobjects/hca_response_v5.py
@@ -133,24 +133,9 @@ class ManifestResponse(AbstractResponse):
         :param mapping: The mapping between the columns to values within ES
         :param manifest_entries: The columns that will be present in the tsv
         """
-        # Get a list of the hits in the raw response
-        # hits = [x['_source'] for x in raw_response['hits']['hits']]
 
-        # bundle_uuids = {jmespath.search('bundles[0].uuid', entry)
-        #                for entry in hits}
-
-        # def create_url(entry):
-        #     file_uuid = jmespath.search('files.uuid', entry)
-        #     file_version = jmespath.search('files.version', entry)
-        #     url = "{}/files/{}?replica=aws&version={}".format(
-        #         self.DSS_URL, file_uuid, file_version)
-        #     return url
-
-        # mapped_manifest = [[uuid] for uuid in bundle_uuids]
         # TODO: Fix manifests and improve memory usage.
         raise Exception("Not yet implemented")
-        # self.apiResponse = make_response_from_array(
-        #    mapped_manifest, 'tsv', file_name='manifest')
 
 
 class EntryFetcher:
