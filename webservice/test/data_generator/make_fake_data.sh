7b6d5c8b7db263b05bfc9add7844a3ea2a2ef4bf
Jim Vlasblom <jim@dnastack.com>
2018-06-16 03:00:57 -0400

    Fixed manifest downloads and updated tests to use latest schemas. Fixes #58

--- webservice/test/data_generator/make_fake_data.sh
+++ webservice/test/data_generator/make_fake_data.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 WD=`pwd`
 cd /app/test/data_generator
-python3 /app/test/data_generator/make_fake_data.py /app/test/data_generator/fake_data_template.json 50000 1
+python3 /app/test/data_generator/make_fake_data.py /app/test/data_generator/fake_data_template.json 5000 6
 cd ${WD}
