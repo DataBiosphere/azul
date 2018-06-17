5264001b4276e6ee22913e953c658948c947d905
Carlos Alberto Espinosa Méndez <carlos.espinosa805@gmail.com>
2018-06-17 02:07:52 -0700

    - Parallelize requests to elasticsearch via bulk requests and   `parallel_bulk`

--- indexer/utils/indexer.py
+++ indexer/utils/indexer.py
@@ -7,16 +7,15 @@ The based class Indexer serves as the basis for additional indexing classes.
 
 """
 from abc import ABC
-from elasticsearch import ConnectionError, ConflictError
+from collections import defaultdict
 import logging
-from time import sleep
-from typing import Mapping, Any, Union
+from typing import Mapping, Any, Union, Iterable
 
-from elasticsearch import ConnectionError, ConflictError
-
+from elasticsearch.helpers import parallel_bulk
 
 from utils.base_config import BaseIndexProperties
 from utils.downloader import MetadataDownloader
+from utils.transformer import ElasticSearchDocument
 
 # create logger
 module_logger = logging.getLogger(__name__)
@@ -37,51 +36,112 @@ class BaseIndexer(ABC):
         es_client = self.properties.elastic_search_client
         # Create and populate the Indexes
         for index_name in self.properties.index_names:
+            # Create the index and apply the mapping at the same time.
             es_client.indices.create(index=index_name,
                                      body=self.properties.settings,
                                      ignore=[400])
             es_client.indices.put_mapping(index=index_name,
                                           doc_type="doc",
                                           body=self.properties.mapping)
-        # Call each transformer and populate their respective index
+        # Keep track of documents to index and errored out documents
+        indexable_documents = {}
+        errored_documents = defaultdict(int)
+        conflict_documents = defaultdict(int)
+        # Collect the documents to be indexed
         for transformer in transformers:
-            es_documents = transformer.create_documents(metadata, data, bundle_uuid, bundle_version)
+            es_documents = transformer.create_documents(metadata,
+                                                        data,
+                                                        bundle_uuid,
+                                                        bundle_version)
             for es_document in es_documents:
-                retries = 3
-                while True:
-                    existing = es_client.get(index=es_document.document_index,
-                                             doc_type=es_document.document_type,
-                                             id=es_document.document_id,
-                                             ignore=[404])
-                    # replace with an empty dictionary if no existing doc
-                    existing_source = existing.get("_source", {})
-                    updated_version = existing.get("_version", 0) + 1
-                    new_content = es_document.document_content
-                    updated_document = self.merge(new_content, existing_source)
+                indexable_documents[es_document.document_id] = es_document
+
+        def create_mget_body(documents_dictionary: Mapping[str, ElasticSearchDocument]) -> \
+                Mapping[str, Iterable]:
+            docs = []
+            for d in documents_dictionary.values():
+                _doc = {
+                    "_index": d.document_index,
+                    "_type": d.document_type,
+                    "_id": d.document_id
+                }
+                docs.append(_doc)
+            _mget_body = {"docs": docs}
+            return _mget_body
+
+        def create_bulk_body(
+                documents_dictionary: Mapping[str, ElasticSearchDocument]) -> Iterable[dict]:
+            for d in documents_dictionary.values():
+                _action = {
+                    "_index": d.document_index,
+                    "_type": d.document_type,
+                    "_id": d.document_id,
+                    "_op_type": "index",
+                    "version": d.document_version,
+                    "version_type": "external",
+                    "_source": d.document_content
+                }
+                yield _action
+
+        while indexable_documents:
+            # First query for any existing documents
+            mget_body = create_mget_body(indexable_documents)
+            existing_docs = es_client.mget(body=mget_body)
+            # Merge any existing document
+            for doc in existing_docs["docs"]:
+                if doc["found"]:
+                    es_document = indexable_documents[doc["_id"]]
+                    new_doc = es_document.document_content
+                    # This should update 'indexable_documents' by reference
+                    updated_document = self.merge(new_doc, doc["_source"])
+                    updated_version = doc.get("_version", 0) + 1
                     es_document.document_content = updated_document
-                    try:
-                        es_client.index(index=es_document.document_index,
-                                        doc_type=es_document.document_type,
-                                        id=es_document.document_id,
-                                        body=es_document.document_content,
-                                        version=updated_version,
-                                        version_type="external")
-                        break
-                    except ConflictError as er:
-                        module_logger.info(
-                            "There was a version conflict... retrying")
-                        module_logger.debug(er.info)
-                    except ConnectionError as er:
-                        module_logger.info("There was a connection error")
-                        module_logger.info("{} retries left".format(retries))
-                        if retries > 0:
-                            retries -= 1
-                            sleep(retries)
-                        else:
-                            module_logger.error("Out of retries. There is a connection problem.")
-                            module_logger.error(er.error)
-                            module_logger.error(er.info)
-                            raise er
+                    es_document.document_version = updated_version
+
+            retry_ids = set()
+            # Process each result
+            for success, info in parallel_bulk(es_client,
+                                               create_bulk_body(
+                                                   indexable_documents),
+                                               raise_on_error=False,
+                                               max_chunk_bytes=10485760):
+                # We don't care about successes
+                if success:
+                    continue
+                retry_ids.add(info["index"]["_id"])
+                if info["index"]['status'] == 409:
+                    module_logger.warning(
+                        "There was a conflict error on document %s, index %s",
+                        info["index"]["_id"], info["index"]["_index"])
+                    module_logger.warning("Indexer will try to re-index")
+                    conflict_documents[info["index"]["_id"]] += 1
+
+                else:
+                    # Limit other errors to 3 times.
+                    if errored_documents[info["index"]["_id"]] < 3:
+                        errored_documents[info["index"]["_id"]] += 1
+                        module_logger.warning(
+                            "This isn't a conflict error. %i attempt",
+                            errored_documents[
+                                info["index"]["_id"]])
+                        module_logger.warning(
+                            "There was an error on document %s, index %s",
+                            info["index"]["_id"], info["index"]["_index"])
+                    else:
+                        module_logger.error("i% tries on id %s",
+                                            errored_documents[
+                                                info["index"]["_id"]],
+                                            info["index"]["_id"])
+                        module_logger.error(
+                            "There was an error on document %s, index %s",
+                            info["index"]["_id"],
+                            info["index"]["_index"])
+
+            # Update the indexable documents
+            indexable_documents = {k: v for k, v in indexable_documents.items()
+                                   if k in retry_ids}
+        module_logger.debug("Errored documents: %s", str(errored_documents))
+        module_logger.debug("Conflict documents: %s", str(conflict_documents))
 
     @staticmethod
     def merge(new_document: Mapping[str, Any],
