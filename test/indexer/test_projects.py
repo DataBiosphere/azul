f0023a46973093c4ae83a19c9ebb8b7c3a23e5b2
caaespin <caaespin@ucsc.edu>
2018-07-10 15:21:46 -0700

    Accessor Library API and its implementation

--- test/indexer/test_projects.py
+++ test/indexer/test_projects.py
@@ -1,11 +1,14 @@
-from datetime import datetime
+import logging
 import unittest
-from azul.project.hca.indexer import Indexer
-from azul.project.hca.config import IndexProperties
+from datetime import datetime
 from typing import Mapping, Any
 from uuid import uuid4
-import logging
 
+from azul.project.hca.config import IndexProperties
+from azul.project.hca.indexer import Indexer
+from azul.project.hca.metadata_api.bundle import Bundle
+
+logging.basicConfig(level=logging.INFO)
 module_logger = logging.getLogger(__name__)
 
 
@@ -104,6 +107,51 @@ class TestDataExtractor(unittest.TestCase):
                                        doc_type="doc",
                                        size=100)
         self.assertEqual("pass", "pass")
+
+    def test_accessor_class(self):
+        from azul.downloader import MetadataDownloader
+        from azul.dss_bundle import DSSBundle
+        bundle_uuid = "b2216048-7eaa-45f4-8077-5a3fb4204953"
+        bundle_version = "2018-03-29T142048.835519Z"
+        fake_event = make_fake_notification(bundle_uuid, bundle_version)
+        metadata_downloader = MetadataDownloader("https://dss.data.humancellatlas.org/v1")
+        metadata, manifest = metadata_downloader.extract_bundle(fake_event)
+        dss_bundle = DSSBundle(uuid=bundle_uuid,
+                               version=bundle_version,
+                               manifest=manifest,
+                               metadata_files=metadata)
+        reconstructed_bundle = Bundle(dss_bundle)
+        sequencing_input = reconstructed_bundle.sequencing_input
+        for si in sequencing_input:
+            print(si.biomaterial_id)
+        specimens = reconstructed_bundle.specimens
+        for sp in specimens:
+            print(sp.biomaterial_id)
+        print(reconstructed_bundle.project)
+
+        self.assertEqual("pass", "pass")
+
+    def test_no_duplicate_files_in_specimen(self):
+        # Trigger the indexing operation
+        dss_url = "https://dss.data.humancellatlas.org/v1"
+        index_properties = IndexProperties(dss_url, es_endpoint=("localhost", 9200))
+        hca_indexer = Indexer(index_properties)
+        fake_event = make_fake_notification("8543d32f-4c01-48d5-a79f-1c5439659da3",
+                                            "2018-03-29T143828.884167Z")
+        module_logger.info("Start computation %s",
+                           datetime.now().isoformat(timespec='microseconds'))
+        hca_indexer.index(fake_event)
+        module_logger.info("Indexing operation finished for %s. Check values in ElasticSearch",
+                           "8543d32f-4c01-48d5-a79f-1c5439659da3" + "2018-03-29T143828.884167Z")
+        module_logger.info("End computation %s",
+                           datetime.now().isoformat(timespec='microseconds'))
+        # Check values in ElasticSearch
+        es_client = index_properties.elastic_search_client
+        results = es_client.get(index="browser_specimens_dev",
+                                id="b3623b88-c369-46c9-a2e9-a16042d2c589")
+        file_ids = [f["uuid"] for f in
+                    results["_source"]["bundles"][0]["contents"]["files"]]
+        self.assertTrue(len(file_ids) == len(set(file_ids)))
 
 
 if __name__ == "__main__":
