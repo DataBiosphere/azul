9213ed564cbcb58fe3192b24410ce43bb3ae7063
caaespin <caaespin@ucsc.edu>
2018-07-10 15:21:46 -0700

    Accessor Library API and its implementation

---
+++ src/azul/project/hca/metadata_api/expando.py
@@ -0,0 +1,97 @@
+from typing import Any
+
+
+class Expando(dict):
+    """
+    Pass inital attributes to the constructor:
+
+    >>> o = Expando(foo=42)
+    >>> o.foo
+    42
+
+    Dynamically create new attributes:
+
+    >>> o.bar = 'hi'
+    >>> o.bar
+    'hi'
+
+    Expando is a dictionary:
+
+    >>> isinstance(o,dict)
+    True
+    >>> o['foo']
+    42
+
+    Works great with JSON:
+
+    >>> import json
+    >>> s='{"foo":42}'
+    >>> o = json.loads(s,object_hook=Expando)
+    >>> o.foo
+    42
+    >>> o.bar = 'hi'
+    >>> o.bar
+    'hi'
+
+    And since Expando is a dict, it serializes back to JSON just fine:
+
+    >>> json.dumps(o, sort_keys=True)
+    '{"bar": "hi", "foo": 42}'
+
+    Attributes can be deleted, too:
+
+    >>> o = Expando(foo=42)
+    >>> o.foo
+    42
+    >>> del o.foo
+    >>> o.foo
+    Traceback (most recent call last):
+    ...
+    AttributeError: 'Expando' object has no attribute 'foo'
+    >>> o['foo']
+    Traceback (most recent call last):
+    ...
+    KeyError: 'foo'
+
+    >>> del o.foo
+    Traceback (most recent call last):
+    ...
+    AttributeError: foo
+
+    And copied:
+
+    >>> o = Expando(foo=42)
+    >>> p = o.copy()
+    >>> isinstance(p,Expando)
+    True
+    >>> o == p
+    True
+    >>> o is p
+    False
+
+    Same with MagicExpando ...
+
+    >>> o = MagicExpando()
+    >>> o.foo.bar = 42
+    >>> p = o.copy()
+    >>> isinstance(p,MagicExpando)
+    True
+    >>> o == p
+    True
+    >>> o is p
+    False
+
+    ... but the copy is shallow:
+
+    >>> o.foo is p.foo
+    True
+    """
+
+    def __init__( self, *args, **kwargs ):
+        super( Expando, self ).__init__( *args, **kwargs )
+        self.__slots__ = None
+        self.__dict__ = self
+
+    def copy(self):
+        return type(self)(self)
+
