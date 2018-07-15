0c45980b393bc22319e260d89a79d95864b14151
Hannes Schmidt <hannes@ucsc.edu>
2018-07-14 22:20:46 -0700

    User-friendly domain name for REST APIs (fixes #65)

---
+++ indexer/terraform/common.tf.json.template.py
@@ -0,0 +1,20 @@
+from utils.template import emit
+
+emit({
+    "data": [
+        {
+            "aws_caller_identity": {
+                "current": {}
+            }
+        },
+        {
+            "aws_region": {
+                "current": {}
+            }
+        },
+    ],
+    "locals": {
+        "account_id": "${data.aws_caller_identity.current.account_id}",
+        "region": "${data.aws_region.current.name}"
+    }
+})
