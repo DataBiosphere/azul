3427bfd4011df43e06ad928f37c3cda4799abf64
Hannes Schmidt <hannes@ucsc.edu>
2018-07-18 10:26:56 -0700

    ACM certs for API Gateway domain names must reside in us-east-1

--- terraform/providers.tf.json.template.py
+++ terraform/providers.tf.json.template.py
@@ -3,15 +3,25 @@ from azul.template import emit
 
 emit(
     {
-        "provider": {
-            "aws": {
-                "version": "~> 1.22",
-                "profile": aws.profile['source_profile'],
-                "assume_role": {
-                    "role_arn": aws.profile['role_arn']
+        "provider": [
+            {
+                "aws": {
+                    "version": "~> 1.22",
+                    "profile": aws.profile['source_profile'],
+                    "assume_role": {
+                        "role_arn": aws.profile['role_arn']
+                    }
+                } if 'role_arn' in aws.profile else {
                 }
-            } if 'role_arn' in aws.profile else {
+            },
+            {
+                "aws": {
+                    # Pin the region for the certificates of the API Gateway custom domain names. Certificates of
+                    # edge-optimized custom domain names have to reside in us-east-1.
+                    "alias": "us-east-1",
+                    "region": "us-east-1"
+                }
             }
-        }
+        ]
     }
 )
