b13c8ed4cc3bd62808a37346afe322d511f9d078
Hannes Schmidt <hannes@ucsc.edu>
2018-07-19 10:51:47 -0700

    Move default DSS subscription query into plugin

--- src/azul/project/hca/__init__.py
+++ src/azul/project/hca/__init__.py
@@ -1,2 +1,23 @@
 from .config import IndexProperties
 from .indexer import Indexer
+
+dss_subscription_query = {
+    "query": {
+        "bool": {
+            "must_not": [
+                {
+                    "term": {
+                        "admin_deleted": True
+                    }
+                }
+            ],
+            "must": [
+                {
+                    "exists": {
+                        "field": "files.biomaterial_json"
+                    }
+                }
+            ]
+        }
+    }
+}
