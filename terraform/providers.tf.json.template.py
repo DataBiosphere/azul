DataBiosphere/azul

commit a76fd122eb56343bdb4b7ac3320670ead8ea469d

ACM certs for API Gateway domain names must reside in us-east-1

terraform/providers.tf.json.template.py
@@ -3,15 +3,25 @@ from azul.template import emit
 
 emit(
     {
-        "provider": {
-            "aws": {
-                "version": "~> 1.22",
-                "profile": aws.profile['source_profile'],
-                "assume_role": {
-                    "role_arn": aws.profile['role_arn']
+        "provider": [
+            {
+                "aws": {
+                    "version": "~> 1.22",
+                    "profile": aws.profile['source_profile'],
+                    "assume_role": {
+                        "role_arn": aws.profile['role_arn']
+                    }
+                } if 'role_arn' in aws.profile else {
                 }
-            } if 'role_arn' in aws.profile else {
+            },
+            {
+                "aws": {
+                    # Pin the region for the certificates of the API Gateway custom domain names. Certificates of
+                    # edge-optimized custom domain names have to reside in us-east-1.
+                    "alias": "us-east-1",
+                    "region": "us-east-1"
+                }
             }
-        }
+        ]
     }
 )

terraform/api_gateway_domain_name.tf.json.template.py
@@ -43,12 +43,14 @@ emit({
                 lambda_name: {
                     "domain_name": config.api_lambda_domain(lambda_name),
                     "validation_method": "DNS",
+                    "provider": "aws.us-east-1"
                 }
             },
             "aws_acm_certificate_validation": {
                 lambda_name: {
                     "certificate_arn": "${aws_acm_certificate.%s.arn}" % lambda_name,
-                    "validation_record_fqdns": ["${aws_route53_record.%s_domain_validation.fqdn}" % lambda_name]
+                    "validation_record_fqdns": ["${aws_route53_record.%s_domain_validation.fqdn}" % lambda_name],
+                    "provider": "aws.us-east-1"
                 }
             },
             "aws_route53_record": {

commit ee53a05827bd529db4f7f825e65bd8592dbecd71

Fix: `make terraform` broken with STS credentials
Regression from a76fd122eb56343bdb4b7ac3320670ead8ea469d.

```
Error: Error refreshing state: 1 error(s) occurred:

* provider.aws.us-east-1: No valid credential sources found for AWS Provider.
	Please see https://terraform.io/docs/providers/aws/index.html for more information on
	providing credentials for the AWS Provider
```

terraform/providers.tf.json.template.py
@@ -6,22 +6,27 @@ emit(
         "provider": [
             {
                 "aws": {
-                    "version": "~> 1.22",
-                    "profile": aws.profile['source_profile'],
-                    "assume_role": {
-                        "role_arn": aws.profile['role_arn']
-                    }
-                } if 'role_arn' in aws.profile else {
+                    'version': "~> 1.22",
+                    **(
+                        {
+                            'region': region,
+                            'alias': region
+                        } if region else {
+                        }
+                    ),
+                    **(
+                        {
+                            'profile': aws.profile['source_profile'],
+                            'assume_role': {
+                                'role_arn': aws.profile['role_arn']
+                            }
+                        } if 'role_arn' in aws.profile else {
+                        }
+                    )
                 }
-            },
-            {
-                "aws": {
-                    # Pin the region for the certificates of the API Gateway custom domain names. Certificates of
-                    # edge-optimized custom domain names have to reside in us-east-1.
-                    "alias": "us-east-1",
-                    "region": "us-east-1"
-                }
-            }
+            } for region in (None, 'us-east-1')
+            # Generate a default `aws` provider and one that pins the region for the certificates of the API Gateway
+            # custom domain names. Certificates of edge-optimized custom domain names have to reside in us-east-1.
         ]
     }
 )