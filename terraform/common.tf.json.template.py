6d7746887bffbc5cace225863bf30d5b0309df65
Hannes Schmidt <hannes@ucsc.edu>
2018-07-19 14:26:41 -0700

    Automatically subscribe to DSS during deployment (fixes #104)

--- terraform/common.tf.json.template.py
+++ terraform/common.tf.json.template.py
@@ -12,9 +12,15 @@ emit({
                 "current": {}
             }
         },
+        {
+            "google_client_config": {
+                "current": {}
+            }
+        }
     ],
     "locals": {
         "account_id": "${data.aws_caller_identity.current.account_id}",
-        "region": "${data.aws_region.current.name}"
+        "region": "${data.aws_region.current.name}",
+        "google_project": "${data.google_client_config.current.project}"
     }
 })
