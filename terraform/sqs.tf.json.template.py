7e65c7b12e1fe27c5a8b2e8422b578eaa46a4fc2
Hannes Schmidt <hannes@ucsc.edu>
2018-07-22 16:24:19 -0700

    Limit indexer retries and add DLQ (fixes #122)

--- terraform/sqs.tf.json.template.py
+++ terraform/sqs.tf.json.template.py
@@ -1,3 +1,5 @@
+import json
+
 from azul import config
 from azul.template import emit
 
@@ -9,6 +11,14 @@ emit(
                     "notification_queue": {
                         "name": f"azul-notify-{config.deployment_stage}",
                         "message_retention_seconds": 24 * 60 * 60,
+                        "redrive_policy": json.dumps({
+                            "maxReceiveCount": 10,
+                            "deadLetterTargetArn": "${aws_sqs_queue.failure_queue.arn}"
+                        })
+                    },
+                    "failure_queue": {
+                        "name": f"azul-fail-{config.deployment_stage}",
+                        "message_retention_seconds": 14 * 24 * 60 * 60,
                     }
                 }
             }
