d964cdd8cbccf53b6e3e33e3daf075d92d059bd1
Hannes Schmidt <hannes@ucsc.edu>
2018-07-24 22:38:40 -0700

    Fix: `make terraform` breaks without GCP credentials (fixes #126)

--- terraform/common.tf.json.template.py
+++ terraform/common.tf.json.template.py
@@ -1,3 +1,4 @@
+from azul import config
 from azul.template import emit
 
 emit({
@@ -12,15 +13,15 @@ emit({
                 "current": {}
             }
         },
-        {
+        *([{
             "google_client_config": {
                 "current": {}
             }
-        }
+        }] if config.enable_gcp() else [])
     ],
     "locals": {
         "account_id": "${data.aws_caller_identity.current.account_id}",
         "region": "${data.aws_region.current.name}",
-        "google_project": "${data.google_client_config.current.project}"
+        "google_project": "${data.google_client_config.current.project}" if config.enable_gcp() else None
     }
 })
