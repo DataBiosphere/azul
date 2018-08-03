733d425bfaa2aa8ae059afe98eefa449ff96d9a0
Hannes Schmidt <hannes@ucsc.edu>
2018-08-02 22:46:56 -0700

    Minimize state in AzulTestCase (connected to #41)

--- test/indexer/__init__.py
+++ test/indexer/__init__.py
@@ -21,8 +21,7 @@ class IndexerTestCase(AzulTestCase):
         cls._old_dss_endpoint = os.environ.get('AZUL_DSS_ENDPOINT')
         os.environ['AZUL_DSS_ENDPOINT'] = "https://dss.data.humancellatlas.org/v1"
         cls.index_properties = IndexProperties(dss_url=config.dss_endpoint,
-                                               es_endpoint=(cls.es_host_ip,
-                                                            cls.es_host_port))
+                                               es_endpoint=config.es_endpoint)
         cls.hca_indexer = Indexer(cls.index_properties)
 
     @classmethod
--- test/shared.py
+++ test/shared.py
@@ -11,44 +11,39 @@ logger = logging.getLogger(__name__)
 
 
 class AzulTestCase(unittest.TestCase):
-    es_docker_container = None
-    es_host_port = None
-    es_host_ip = None
-    es_host = None
-    _es_client = None
 
-    @classmethod
-    def get_es_client(cls):
-        return cls._es_client
+    es_client = None
 
+    _es_docker_container = None
 
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
         docker_client = docker.from_env()
         api_container_port = '9200/tcp'
-        cls.es_docker_container = docker_client.containers.run("docker.elastic.co/elasticsearch/elasticsearch:5.5.3",
-                                                               detach=True,
-                                                               ports={api_container_port: ('127.0.0.1', None)},
-                                                               environment=["xpack.security.enabled=false",
-                                                                      "discovery.type=single-node"])
-        container_info = docker_client.api.inspect_container(cls.es_docker_container.name)
+        cls._es_docker_container = docker_client.containers.run("docker.elastic.co/elasticsearch/elasticsearch:5.5.3",
+                                                                detach=True,
+                                                                ports={api_container_port: ('127.0.0.1', None)},
+                                                                environment=["xpack.security.enabled=false",
+                                                                             "discovery.type=single-node"])
+        container_info = docker_client.api.inspect_container(cls._es_docker_container.name)
         container_ports = container_info['NetworkSettings']['Ports']
         container_port = container_ports[api_container_port][0]
-        cls.es_host_port = int(container_port['HostPort'])
-        cls.es_host_ip = container_port['HostIp']
-        cls.es_host = f'{cls.es_host_ip}: {cls.es_host_port}'
-        os.environ['AZUL_ES_ENDPOINT'] = cls.es_host
-        cls._es_client = Elasticsearch(hosts=[cls.es_host], use_ssl=False)
         # FIXME: https://github.com/DataBiosphere/azul/issues/134
         # deprecate use of production server in favor of local, farm-to-table data files
+        es_host_port = int(container_port['HostPort'])
+        es_host_ip = container_port['HostIp']
+        es_host = f'{es_host_ip}:{es_host_port}'
+        os.environ['AZUL_ES_ENDPOINT'] = es_host
+        cls.es_client = Elasticsearch(hosts=[es_host], use_ssl=False)
+        cls._wait_for_es()
 
-        # try wait here for the elasticsearch container
+    @classmethod
+    def _wait_for_es(cls):
         patched_log_level = logging.WARNING if logger.getEffectiveLevel() <= logging.DEBUG else logging.ERROR
-
         start_time = time.time()
         with patch.object(logging.getLogger('elasticsearch'), 'level', new=patched_log_level):
-            while not cls.get_es_client().ping():
+            while not cls.es_client.ping():
                 if time.time() - start_time > 60:
                     logger.error('Docker container took more than a minute to set up')
                     raise AssertionError
@@ -60,4 +55,5 @@ class AzulTestCase(unittest.TestCase):
     @classmethod
     def tearDownClass(cls):
         cls.es_docker_container.kill()
+        cls._es_docker_container = None
         super().tearDownClass()
