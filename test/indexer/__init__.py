e3329976e6450c9b108fab65615e60b8a3e11c5d
Hannes Schmidt <hannes@ucsc.edu>
2018-08-02 22:39:51 -0700

    Re-introduce tear down of AZUL_DSS_ENDPOINT (connected to #41)

--- test/indexer/__init__.py
+++ test/indexer/__init__.py
@@ -10,13 +10,22 @@ class IndexerTestCase(AzulTestCase):
     index_properties = None
     hca_indexer = None
 
+    _old_dss_endpoint = None
+
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
-
-        cls.old_dss_endpoint = os.environ.get('AZUL_DSS_ENDPOINT')
+        cls._old_dss_endpoint = os.environ.get('AZUL_DSS_ENDPOINT')
         os.environ['AZUL_DSS_ENDPOINT'] = "https://dss.data.humancellatlas.org/v1"
         cls.index_properties = IndexProperties(dss_url=config.dss_endpoint,
                                                es_endpoint=(cls.es_host_ip,
                                                             cls.es_host_port))
         cls.hca_indexer = Indexer(cls.index_properties)
+
+    @classmethod
+    def tearDownClass(cls):
+        if cls._old_dss_endpoint is None:
+            del os.environ['AZUL_DSS_ENDPOINT']
+        else:
+            os.environ['AZUL_DSS_ENDPOINT'] = cls._old_dss_endpoint
+        super().tearDownClass()
