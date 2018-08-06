fe0a845008fe302ee736c4026dbe975fdc1aee97
Geryl Pelayo <gpelayo1@ucsc.edu>
2018-08-06 12:13:32 -0700

    Refactored service tests, deleted unused test files (fixes #158)

--- test/indexer/data_generator/make_fake_data.sh
+++
@@ -1,6 +0,0 @@
-   #!/bin/bash
-
-   export ES_SERVICE="<YOUR_ES_ENDPOINT_HERE>"
-   export ES_PORT="443"
-   export ES_PROTOCOL="https"
-   python make_fake_data.py fake_data_template.json 40000 250
