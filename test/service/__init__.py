862813d30d50cc569350fa226997bebcb8aa628e
Geryl Pelayo <gpelayo1@ucsc.edu>
2018-08-06 12:13:32 -0700

    Refactored service tests, deleted unused test files (fixes #158)

--- test/service/__init__.py
+++ test/service/__init__.py
@@ -1,12 +1,5 @@
-import os
-import logging
-
 from shared import AzulTestCase
 from service.data_generator.fake_data_utils import ElasticsearchFakeDataLoader
-
-
-class setUpModule():
-    logging.basicConfig(level=logging.INFO)
 
 
 class WebServiceTestCase(AzulTestCase):
@@ -15,11 +8,7 @@ class WebServiceTestCase(AzulTestCase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
-        cls.data_loader = ElasticsearchFakeDataLoader(
-            faker_settings_filepath=os.path.join('data_generator', 'fake_data_template.json'),
-            settings_filepath=os.path.join('data_generator', 'td_settings.json'),
-            mapping_filepath=os.path.join('data_generator', 'td_mapping.json'),
-            number_of_documents=1000)
+        cls.data_loader = ElasticsearchFakeDataLoader()
         cls.data_loader.load_data()
 
     @classmethod
