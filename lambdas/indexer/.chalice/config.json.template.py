d7c6dd6a3699bad5c9ee960d79b6c6b229d73931
Hannes Schmidt <hannes@ucsc.edu>
2018-08-09 15:47:00 -0700

    Move generation of lambda environment to Config

--- lambdas/indexer/.chalice/config.json.template.py
+++ lambdas/indexer/.chalice/config.json.template.py
@@ -1,6 +1,3 @@
-import os
-import git
-
 from azul import config
 from azul.deployment import aws
 from azul.template import emit
@@ -8,23 +5,13 @@ from azul.template import emit
 suffix = '-' + config.deployment_stage
 assert config.indexer_name.endswith(suffix)
 
-host, port = config.es_endpoint
-repo = git.Repo(config.project_root)
-
 emit({
     "version": "2.0",
     "app_name": config.indexer_name[:-len(suffix)],  # Chalice appends stage name implicitly
     "api_gateway_stage": config.deployment_stage,
     "manage_iam_role": False,
     "iam_role_arn": f"arn:aws:iam::{aws.account}:role/{config.indexer_name}",
-    "environment_variables": {
-        **{k: v for k, v in os.environ.items() if k.startswith('AZUL_') and k != 'AZUL_HOME'},
-        # Hard-wire the ES endpoint, so we don't need to look it up at run-time, for every request/invocation
-        "AZUL_ES_ENDPOINT": f"{host}:{port}",
-        'azul_git_commit': repo.head.object.hexsha,
-        'azul_git_dirty': str(repo.is_dirty()),
-        "HOME": "/tmp"
-    },
+    "environment_variables": config.lambda_env,
     "lambda_timeout": 300,
     "lambda_memory_size": 1024
 })
