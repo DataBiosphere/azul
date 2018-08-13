DataBiosphere/azul

commit e7f3842f00d7973b97335d168148524f292c26c5

Move generation of lambda environment to Config

src/azul/__init__.py
@@ -3,7 +3,7 @@ import importlib
 import os
 import time
 
-from typing import Tuple
+from typing import Tuple, Mapping
 
 from hca import HCAConfig
 from hca.dss import DSSClient
@@ -98,6 +98,22 @@ class Config:
         return {
             'commit': os.environ['azul_git_commit'],
             'dirty': bool(os.environ['azul_git_dirty']),
+        }
+
+    @property
+    def lambda_env(self) -> Mapping[str, str]:
+        """
+        A dictionary with the enviroment variables to be used by a deployed AWS Lambda function or `chalice local`
+        """
+        import git
+        repo = git.Repo(self.project_root)
+        host, port = self.es_endpoint
+        return {
+            **{k: v for k, v in os.environ.items() if k.startswith('AZUL_') and k != 'AZUL_HOME'},
+            # Hard-wire the ES endpoint, so we don't need to look it up at run-time, for every request/invocation
+            'AZUL_ES_ENDPOINT': f"{host}:{port}",
+            'azul_git_commit': repo.head.object.hexsha,
+            'azul_git_dirty': str(repo.is_dirty()),
         }
 
     def google_service_account(self, lambda_name):

lambdas/indexer/.chalice/config.json.template.py
@@ -1,6 +1,3 @@
-import os
-import git
-
 from azul import config
 from azul.deployment import aws
 from azul.template import emit
@@ -8,23 +5,13 @@ from azul.template import emit
 suffix = '-' + config.deployment_stage
 assert config.indexer_name.endswith(suffix)
 
-host, port = config.es_endpoint
-repo = git.Repo(config.project_root)
-
 emit({
     "version": "2.0",
     "app_name": config.indexer_name[:-len(suffix)],  # Chalice appends stage name implicitly
     "api_gateway_stage": config.deployment_stage,
     "manage_iam_role": False,
     "iam_role_arn": f"arn:aws:iam::{aws.account}:role/{config.indexer_name}",
-    "environment_variables": {
-        **{k: v for k, v in os.environ.items() if k.startswith('AZUL_') and k != 'AZUL_HOME'},
-        # Hard-wire the ES endpoint, so we don't need to look it up at run-time, for every request/invocation
-        "AZUL_ES_ENDPOINT": f"{host}:{port}",
-        'azul_git_commit': repo.head.object.hexsha,
-        'azul_git_dirty': str(repo.is_dirty()),
-        "HOME": "/tmp"
-    },
+    "environment_variables": config.lambda_env,
     "lambda_timeout": 300,
     "lambda_memory_size": 1024
 })

lambdas/service/.chalice/config.json.template.py
@@ -1,6 +1,3 @@
-import os
-import git
-
 from azul import config
 from azul.deployment import aws
 from azul.template import emit
@@ -8,23 +5,13 @@ from azul.template import emit
 suffix = '-' + config.deployment_stage
 assert config.service_name.endswith(suffix)
 
-host, port = config.es_endpoint
-repo = git.Repo(config.project_root)
-
 emit({
     'version': '2.0',
     'app_name': config.service_name[:-len(suffix)],  # Chalice appends stage name implicitly
     'api_gateway_stage': config.deployment_stage,
     'manage_iam_role': False,
     'iam_role_arn': f'arn:aws:iam::{aws.account}:role/{config.service_name}',
-    'environment_variables': {
-        **{k: v for k, v in os.environ.items() if k.startswith('AZUL_') and k != 'AZUL_HOME'},
-        # Hard-wire the ES endpoint, so we don't need to look it up at run-time, for every request/invocation
-        'AZUL_ES_ENDPOINT': f'{host}:{port}',
-        'azul_git_commit': repo.head.object.hexsha,
-        'azul_git_dirty': str(repo.is_dirty()),
-        'HOME': '/tmp'
-    },
+    'environment_variables': config.lambda_env,
     'lambda_timeout': 31,
     'lambda_memory_size': 1024
 })

commit a11d77293a85c6fe478ceef7346f7d24a3864fc6

Fix: Worker fails due to read-only file system
OSError: [Errno 30] Read-only file system: '/home/sbx_user1068'

Regression from e7f3842f00d7973b97335d168148524f292c26c5

src/azul/__init__.py
@@ -114,6 +114,7 @@ class Config:
             'AZUL_ES_ENDPOINT': f"{host}:{port}",
             'azul_git_commit': repo.head.object.hexsha,
             'azul_git_dirty': str(repo.is_dirty()),
+            'HOME': '/tmp'
         }
 
     def google_service_account(self, lambda_name):