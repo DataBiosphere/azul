c0a0eb623d6515bf7471c2c8e903f9ada500704f
alex-hancock <alexbhancock@gmail.com>
2018-08-22 12:56:02 -0700

    Renamed file/variable names for test_response and test_request_builder

--- test/service/test_response.py
+++ test/service/test_response.py
@@ -11,8 +11,10 @@ from service import WebServiceTestCase
 
 
 class TestResponse(WebServiceTestCase):
-
-    data_folder_filepath = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'data')
+    def load_webservice_data(self, filename):
+        data_folder_filepath = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'data')
+        with open(os.path.join(data_folder_filepath, filename)) as fp:
+            return json.load(fp)
 
     def test_key_search_response(self):
         """
@@ -21,84 +23,35 @@ class TestResponse(WebServiceTestCase):
         appropriate by asserting the apiResponse attribute
         is the same as expected.
         """
-        with open('{}/test1index.json'.format(self.data_folder_filepath)) as json_test:
-            test_json_input = json.load(json_test)
-
-        with open('{}/keyword_test1.json'.format(self.data_folder_filepath)) as test1:
-            expected_output = json.load(test1)
-
         # Still need a way to test the response.
-        keyword_response = KeywordSearchResponse(test_json_input).return_response().to_json()
-        # Transform both json objects to a string
-        json_expected_output = json.dumps(expected_output, sort_keys=True)
-        json_actual_output = json.dumps(keyword_response, sort_keys=True)
-        self.assertEqual(json_actual_output, json_expected_output)
+        keyword_response = KeywordSearchResponse(
+            self.load_webservice_data("response_test_input.json")
+        ).return_response().to_json()
+
+        self.assertEqual(json.dumps(keyword_response, sort_keys=True),
+                         json.dumps(self.load_webservice_data("response_keysearch_output.json"), sort_keys=True))
 
     def test_file_search_response(self):
         """
+        n=1
         This method tests the FileSearchResponse object.
         It will make sure the functionality works as
         appropriate by asserting the apiResponse attribute
         is the same as expected.
-        """
-        with open('{}/test1index.json'.format(self.data_folder_filepath)) as json_test:
-            test_json = json.load(json_test)
 
-        with open('{}/filesearch_test1.json'.format(self.data_folder_filepath)) as test1:
-            file_search_test = json.load(test1)
-
-        # This is what will be used as the comparing standard
-        with open('{}/facets_test_input1.json'.format(
-                self.data_folder_filepath)) as facet_input:
-            facet_test = json.load(facet_input)
-
-        with open('{}/pagination_test_input1.json'.format(
-                self.data_folder_filepath)) as pagination_input:
-            pagination_test = json.load(pagination_input)
-
-        # Still need a way to test the response.
-        file_search_response = FileSearchResponse(
-            test_json,
-            pagination_test,
-            facet_test
-        ).return_response().to_json()
-
-        # Transform both json objects to a string
-        json_response = json.dumps(file_search_test, sort_keys=True)  # loaded from json
-        json_test = json.dumps(file_search_response, sort_keys=True)  # generated
-        self.assertEqual(json_test, json_response)
-
-    def test_file_search_response_sapagination(self):
-        """
+        n=2 (test_file_search_response_sapagination)
         This method tests the FileSearchResponse object,
         using 'search_after' pagination.
         """
-        with open('{}/test1index.json'.format(self.data_folder_filepath)) as json_test:
-            test_json = json.load(json_test)
+        for n in [1,2]:
+            filesearch_response = FileSearchResponse(
+                self.load_webservice_data("response_test_input.json"),
+                self.load_webservice_data(f"response_pagination_input{n}.json"),
+                self.load_webservice_data("response_facets_input.json")
+            ).return_response().to_json()
 
-        with open('{}/filesearch_test2.json'.format(self.data_folder_filepath)) as test1:
-            file_search_test = json.load(test1)
-
-        # This is what will be used as the comparing standard
-        with open('{}/facets_test_input1.json'.format(
-                self.data_folder_filepath)) as facet_input:
-            facet_test = json.load(facet_input)
-
-        with open('{}/pagination_test_input2.json'.format(
-                self.data_folder_filepath)) as pagination_input:
-            pagination_test = json.load(pagination_input)
-
-        # Still need a way to test the response.
-        file_search_response = FileSearchResponse(
-            test_json,
-            pagination_test,
-            facet_test
-        ).return_response().to_json()
-
-        # Transform both json objects to a string
-        json_response = json.dumps(file_search_test, sort_keys=True)  # loaded from json
-        json_test = json.dumps(file_search_response, sort_keys=True)  # generated
-        self.assertEqual(json_test, json_response)
+            self.assertEqual(json.dumps(filesearch_response, sort_keys=True),
+                             json.dumps(self.load_webservice_data(f"response_filesearch_output{n}.json"), sort_keys=True))
 
     def test_summary_endpoint(self):
         url = self.base_url + "repository/files/summary"
