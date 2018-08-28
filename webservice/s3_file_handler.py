a9987542bf361a19c3da43c5ae03e7815cbd50bb
Michael Krause <mkrause1@ucsc.edu>
2018-08-28 09:49:08 -0700

    Corrected docstrings

--- webservice/s3_file_handler.py
+++ webservice/s3_file_handler.py
@@ -9,10 +9,11 @@ class S3FileHandler:
     def __init__(self, region, access_key_id, secret_key):
         """
         :param region: AWS region
-        :type: str
+        :type region: str
         :param access_key_id: AWS access key ID
-        :type: str
+        :type access_key_id: str
         :param secret_key: AWS secret access key
+        :type secret_key: str
         """
 
         service = 's3'
@@ -28,17 +29,18 @@ class S3FileHandler:
         """
         Check whether bucket with this name exists in AWS account.
         :param bucket_name: name of bucket in account
-        :type str
+        :type bucket_name: str
         :return: true if it exists
-        :rtype boolean:
+        :rtype: boolean
         """
         return self.resource.Bucket(bucket_name) in self.resource.buckets.all()
 
     def create_bucket(self, bucket_name):
         """
         :param bucket_name: name of bucket in account
+        :type bucket_name: str
         :return: response with details
-        :rtype dict: JSON / Python dict
+        :rtype: JSON / Python dict
         """
         if not self.bucket_exists(bucket_name):
             self.bucket_name = bucket_name
@@ -49,9 +51,10 @@ class S3FileHandler:
 
     def list_objects_in_bucket(self, bucket_name):
         """
-        :param bucket_name: (str) name of bucket in account
+        :param bucket_name: name of bucket in account
+        :type bucket_name: str
         :return: list of objects in bucket_name
-        :rtype list:
+        :rtype: list
         """
         response = self.session.list_objects_v2(Bucket=bucket_name)
         if response.has_key('Contents'):
@@ -64,20 +67,20 @@ class S3FileHandler:
     def get_bucket_list(self):
         """
         :returns a list of all buckets in AWS account
-        :rtype list:"""
+        :rtype: list"""
         buckets = self.bucket.list_buckets()
         return [bucket['Name'] for bucket in buckets['Buckets']]
 
     def upload_object_to_bucket(self, bucket_name, fname, name_in_bucket):
         """Uploads some object (e.g., a file) to an S3 bucket.
         :param bucket_name: name of bucket in AWS account
-        :type str:
+        :type bucket_name: str
         :param fname: absolute name of the object to upload
-        :type str:
+        :type fname: str
         :param name_in_bucket: object name in bucket
-        :type str:
+        :type name_in_bucket: str
         :returns: True if it went well, False if bucket does not exist
-        :rtype boolean:
+        :rtype:bool
         """
 
         if self.bucket_exists(bucket_name):
