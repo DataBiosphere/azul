685c93576ccecda35ccd1b411807ad297f223030
Michael Krause <mkrause1@ucsc.edu>
2018-08-29 14:54:48 -0700

    Added test for exception in method upload_object_to_bucket

--- webservice/test/test_s3_file_handler.py
+++ webservice/test/test_s3_file_handler.py
@@ -2,7 +2,6 @@
 
 import unittest
 from mock import patch
-from mock import MagicMock
 from s3_file_handler import S3FileHandler as s3handler
 import botocore
 from botocore.exceptions import ClientError
@@ -49,13 +48,12 @@ class TestS3FileHandler(unittest.TestCase):
         status_code = self.bucket.bucket_exists('my_bucket')
         self.assertEqual(status_code, 200)
 
-        mock_bucket_exists.return_value = 403
+        mock_bucket_exists.return_value = 404
         status_code = self.bucket.bucket_exists('my_bucket')
-        self.assertEqual(status_code, 403)
+        self.assertEqual(status_code, 404)
 
     @patch('s3_file_handler.boto3')
-    def test_bucket_exists_client_error(self, boto3):
-        boto3 = MagicMock()
+    def test_bucket_exists_client_error403(self, boto3):
         boto3.resource('s3')
         boto3.resource.assert_called_with('s3')
 
@@ -72,15 +70,18 @@ class TestS3FileHandler(unittest.TestCase):
         L = self.bucket.get_bucket_list()
         self.assertListEqual(L, self.bucket_list)
 
-    @patch('s3_file_handler.S3FileHandler.upload_object_to_bucket')
-    def test_upload_object_to_bucket(self, mock_upload_object_to_bucket):
-        mock_upload_object_to_bucket.return_value = {'status_code': 200}
-        bucket_name = 'my_bucket'
-        filename = '/tmp/bdbag.zip'
-        key = 'bdbag.zip'
-        response = self.bucket.upload_object_to_bucket(
-            bucket_name, filename, key)
-        self.assertEqual(response, {'status_code': 200})
+    @patch('s3_file_handler.boto3')
+    def test_upload_object_to_bucket(self, boto3):
+        boto3.return_value.resource.return_value.Object.return_value.\
+            upload_file.side_effect = botocore.exceptions.ClientError(
+            {'Error': {'Code': '403', 'Message': 'something'}},
+            'PutObject'
+        )
+        bucket = 'test_bucket'
+        filename = '/test_file'
+        key = 'test_key'
+        status_code = self.bucket.upload_object_to_bucket(bucket, filename, key)
+        self.assertEqual(status_code, {'status_code': 403})
 
     @patch('s3_file_handler.S3FileHandler.list_objects_in_bucket')
     def test_list_objects_in_bucket(self, mock_list_objects_in_bucket):
