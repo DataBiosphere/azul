f7cb9ca3dc942f5e480bb0a11bec86a1ad37bef1
Michael Krause <mkrause1@ucsc.edu>
2018-08-29 09:39:38 -0700

    Added packages, tests, and corrected class construction
    * corrected set-up to reflect how the class is adjusted
    * added packages MagicMock, and ClientError from botocore.exceptions
    * added more tests that check test errors

--- webservice/test/test_s3_file_handler.py
+++ webservice/test/test_s3_file_handler.py
@@ -2,13 +2,18 @@
 
 import unittest
 from mock import patch
+from mock import MagicMock
 from s3_file_handler import S3FileHandler as s3handler
+import botocore
+from botocore.exceptions import ClientError
 
 
 class TestS3FileHandler(unittest.TestCase):
 
     def setUp(self):
-        self.bucket = s3handler(location='us-west-2')
+        self.bucket = s3handler(region='us-west-2',
+                                access_key_id='abc-123',
+                                secret_key='abc-123')
         # Real-world response from method create_bucket method for mocks.
         self.response = {
             u'Location': 'http://my_bucket.s3.amazonaws.com/',
@@ -40,10 +45,27 @@ class TestS3FileHandler(unittest.TestCase):
         self.assertDictEqual(r, self.response)
 
     @patch('s3_file_handler.S3FileHandler.bucket_exists')
-    def test_bucket_exist_bool(self, mock_bucket_exists):
-        mock_bucket_exists.return_value = True
-        tf = self.bucket.bucket_exists('my_bucket')
-        self.assertTrue(tf)
+    def test_bucket_exists(self, mock_bucket_exists):
+        mock_bucket_exists.return_value = 200
+        status_code = self.bucket.bucket_exists('my_bucket')
+        self.assertEqual(status_code, 200)
+
+        mock_bucket_exists.return_value = 403
+        status_code = self.bucket.bucket_exists('my_bucket')
+        self.assertEqual(status_code, 403)
+
+    @patch('s3_file_handler.boto3')
+    def test_bucket_exists_client_error(self, boto3):
+        boto3 = MagicMock()
+        boto3.resource('s3')
+        boto3.resource.assert_called_with('s3')
+
+        boto3.return_value.resource.return_value.Object.return_value.\
+            upload_file.side_effect = botocore.exceptions.ClientError(
+                {'Error': {'Code': '403', 'Message': 'something'}},
+                'PutObject')
+        status_code = self.bucket.bucket_exists('my_bucket')
+        self.assertEqual(status_code, 403)
 
     @patch('s3_file_handler.S3FileHandler.get_bucket_list')
     def test_get_bucket_list(self, mock_bucket_list):
@@ -51,21 +73,15 @@ class TestS3FileHandler(unittest.TestCase):
         L = self.bucket.get_bucket_list()
         self.assertListEqual(L, self.bucket_list)
 
-    # @patch('s3_file_handler.S3FileHandler.delete_bucket')
-    # def test_delete_bucket(self, mock_delete_bucket):
-    #     mock_delete_bucket.return_value = True
-    #     tf = self.bucket.delete_bucket('my_bucket')
-    #     self.assertTrue(tf)
-
     @patch('s3_file_handler.S3FileHandler.upload_object_to_bucket')
     def test_upload_object_to_bucket(self, mock_upload_object_to_bucket):
-        mock_upload_object_to_bucket.return_value = True
+        mock_upload_object_to_bucket.return_value = {'status_code': 200}
         bucket_name = 'my_bucket'
-        fname = '/tmp/bdbag.zip'
-        name_in_bucket = 'bdbag.zip'
-        tf = self.bucket.upload_object_to_bucket(
-            bucket_name, fname, name_in_bucket)
-        self.assertTrue(tf)
+        filename = '/tmp/bdbag.zip'
+        key = 'bdbag.zip'
+        response = self.bucket.upload_object_to_bucket(
+            bucket_name, filename, key)
+        self.assertEqual(response, {'status_code': 200})
 
     @patch('s3_file_handler.S3FileHandler.list_objects_in_bucket')
     def test_list_objects_in_bucket(self, mock_list_objects_in_bucket):
