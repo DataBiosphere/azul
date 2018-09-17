b7d1739ac5939766dbbc2f67b86fcead80d35995
Charles Overbeck <coverbec@ucsc.edu>
2018-09-17 15:05:56 -0700

    Pluralize file names (fixes #326)
    On top of pluralizing the file names, also changed line 172 in
    test_bagitutils.py to compare sets, as the order of
    `fcmp.same_files` is not deterministic.

--- webservice/test/test_bagitutils.py
+++ webservice/test/test_bagitutils.py
@@ -17,8 +17,8 @@ class TestBagHandlerMethods(unittest.TestCase):
         # the test folder.
 
         self.bag_manifest = 'test/manifest_bag'
-        self.bag_participant_tsv = 'test/manifest_bag/participant.tsv'
-        self.bag_sample_tsv = 'test/manifest_bag/sample.tsv'
+        self.bag_participant_tsv = 'test/manifest_bag/participants.tsv'
+        self.bag_sample_tsv = 'test/manifest_bag/samples.tsv'
         self.data = "Program	Project	Center Name	Submitter Donor ID	Donor UUID	Submitter Donor Primary Site	Submitter Specimen ID	Specimen UUID	Submitter Specimen Type	Submitter Experimental Design	Submitter Sample ID	Sample UUID	Analysis Type	Workflow Name	Workflow Version	File Type	File Path	Upload File ID	Data Bundle UUID	Metadata.json	File URLs	File DOS URL\n\
         NHLBI TOPMed: Whole Genome Sequencing and Related Phenotypes in the Framingham Heart Study	Framingham	Broad	20428	09df7aef-246a-57eb-9685-e1d4d18b55ab	BLOOD	SRS1353998	2923638f-0784-5704-8d93-5b97b4ca3092	Normal - Blood	Seq_DNA_SNP_CNV; Seq_DNA_WholeGenome	NWD692354	dd8337dd-f731-5c3b-9a03-bdae77ca47a9	alignment	topmed-spinnaker	Alpha Build 1	crai	NWD692354.b38.irc.v1.cram.crai	5a00cc38-2f8d-4d34-98e0-0a847579b988	50cfaf90-0998-5ef5-aa0b-cfaea71d5a7d		[u'gs://topmed-irc-share/genomes/NWD692354.b38.irc.v1.cram.crai', u's3://nih-nhlbi-datacommons/NWD692354.b38.irc.v1.cram.crai']	dos://dos-dss.ucsc-cgp-dev.org/ga4gh/dos/v1/dataobjects/5a00cc38-2f8d-4d34-98e0-0a847579b988?version=2018-02-28T160411.061319Z\n\
         NHLBI TOPMed: Whole Genome Sequencing and Related Phenotypes in the Framingham Heart Study	Framingham	Broad	20428	09df7aef-246a-57eb-9685-e1d4d18b55ab	BLOOD	SRS1353998	2923638f-0784-5704-8d93-5b97b4ca3092	Normal - Blood	Seq_DNA_SNP_CNV; Seq_DNA_WholeGenome	NWD692354	dd8337dd-f731-5c3b-9a03-bdae77ca47a9	alignment	topmed-spinnaker	Alpha Build 1	cram	NWD692354.b38.irc.v1.cram	b4cf8998-34a1-4e00-aa23-bcdf8d6b23b5	50cfaf90-0998-5ef5-aa0b-cfaea71d5a7d		[u's3://nih-nhlbi-datacommons/NWD692354.b38.irc.v1.cram', u'gs://topmed-irc-share/genomes/NWD692354.b38.irc.v1.cram']	dos://dos-dss.ucsc-cgp-dev.org/ga4gh/dos/v1/dataobjects/b4cf8998-34a1-4e00-aa23-bcdf8d6b23b5?version=2018-02-28T160408.957538Z\n"
@@ -34,7 +34,7 @@ NIH Data Commons	NIH Data Commons Pilot	Broad Public Datasets	ABC123456	c2b4c298
         with zipfile.ZipFile(zip_name) as myzip:
             for name in myzip.namelist():
                 self.assertTrue(name.startswith('manifest'))
-                if 'participant' in name:
+                if 'participants' in name:
                     self.assertIn('data/', name)
         os.remove(zip_name)
 
@@ -163,13 +163,13 @@ NIH Data Commons	NIH Data Commons Pilot	Broad Public Datasets	ABC123456	c2b4c298
 
         # Compare the two output files with the truth files.
         self.assertTrue(filecmp.cmp(self.bag_participant_tsv,
-                    tmpdir + '/participant.tsv'))
+                    tmpdir + '/participants.tsv'))
         self.assertTrue(filecmp.cmp(self.bag_sample_tsv,
-                    tmpdir + '/sample.tsv'))
+                    tmpdir + '/samples.tsv'))
 
         # Compare entire manifest output directory.
         fcmp = filecmp.dircmp(self.bag_manifest, tmpdir)
-        self.assertTrue(fcmp.same_files == ['participant.tsv', 'sample.tsv'])
+        self.assertTrue(set(fcmp.same_files) == set(['participants.tsv', 'samples.tsv']))
 
         shutil.rmtree(tmpdir)
 
