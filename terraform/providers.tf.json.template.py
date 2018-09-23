e416cccbecf6dac76334a944489d888a3e1d8ee0
Hannes Schmidt <hannes@ucsc.edu>
2018-09-22 23:44:43 -0700

    Bump Terraform provider versions

--- terraform/providers.tf.json.template.py
+++ terraform/providers.tf.json.template.py
@@ -1,32 +1,35 @@
 from azul.deployment import aws
 from azul.template import emit
 
-emit(
-    {
-        "provider": [
-            {
-                "aws": {
-                    'version': "~> 1.22",
-                    **(
-                        {
-                            'region': region,
-                            'alias': region
-                        } if region else {
+emit({
+    "provider": [
+        {
+            "google": {
+                'version': "~> 1.18"
+            }
+        },
+        *({
+            "aws": {
+                'version': "~> 1.37",
+                **(
+                    {
+                        'region': region,
+                        'alias': region
+                    } if region else {
+                    }
+                ),
+                **(
+                    {
+                        'profile': aws.profile['source_profile'],
+                        'assume_role': {
+                            'role_arn': aws.profile['role_arn']
                         }
-                    ),
-                    **(
-                        {
-                            'profile': aws.profile['source_profile'],
-                            'assume_role': {
-                                'role_arn': aws.profile['role_arn']
-                            }
-                        } if 'role_arn' in aws.profile else {
-                        }
-                    )
-                }
-            } for region in (None, 'us-east-1')
-            # Generate a default `aws` provider and one that pins the region for the certificates of the API Gateway
-            # custom domain names. Certificates of edge-optimized custom domain names have to reside in us-east-1.
-        ]
-    }
-)
+                    } if 'role_arn' in aws.profile else {
+                    }
+                )
+            }
+        } for region in (None, 'us-east-1'))
+        # Generate a default `aws` provider and one that pins the region for the certificates of the API Gateway
+        # custom domain names. Certificates of edge-optimized custom domain names have to reside in us-east-1.
+    ]
+})
