48aa77fb6e3c907ad7df3ec8437fa022da416c09
Juti Noppornpitak <jnopporn@shiroyuki.com>
2018-10-09 10:46:54 -0400

    Catch a specific exception, instead of a generic one.

--- src/azul/service/responseobjects/storage_service.py
+++ src/azul/service/responseobjects/storage_service.py
@@ -15,7 +15,7 @@ class StorageService:
     def get(self, object_key: str):
         try:
             return self.__s3.get_object(Bucket=self.__bucket_name, Key=object_key)['Body'].read().decode()
-        except Exception:
+        except self.__s3.exceptions.NoSuchKey:
             # NOTE: Normally, we should expect specific error classes, like botocore.errorfactory.NoSuchKey (class).
             #       However, that exception is created on demand and it is impossible for us to catch a specific
             #       exception. Hence, we have to catch all sorts of exceptions.
