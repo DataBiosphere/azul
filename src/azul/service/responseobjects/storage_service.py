1675af4f558cc22810e69eb38643e4a84e59ffc9
Juti Noppornpitak <jnopporn@shiroyuki.com>
2018-10-09 10:59:07 -0400

    Removed the comment.

--- src/azul/service/responseobjects/storage_service.py
+++ src/azul/service/responseobjects/storage_service.py
@@ -16,9 +16,6 @@ class StorageService:
         try:
             return self.__s3.get_object(Bucket=self.__bucket_name, Key=object_key)['Body'].read().decode()
         except self.__s3.exceptions.NoSuchKey:
-            # NOTE: Normally, we should expect specific error classes, like botocore.errorfactory.NoSuchKey (class).
-            #       However, that exception is created on demand and it is impossible for us to catch a specific
-            #       exception. Hence, we have to catch all sorts of exceptions.
             raise GetObjectError(object_key)
 
     def put(self, object_key: str, data: bytes, content_type: Optional[str] = None) -> str:
