b2f36783e3aefbbac6686d7765e8376418271101
Benjamin Wu <benjaminwu124@gmail.com>
2018-10-17 13:24:22 -0400

    Add more tests for dynamo accessor

--- test/service/test_dynamo_accessor.py
+++ test/service/test_dynamo_accessor.py
@@ -12,34 +12,98 @@ class TestDynamoAccessor(TestCase):
         cls.dynamo_accessor = DynamoDataAccessor()
 
     def create_tables(self):
-        self.dynamo_accessor.create_table('Carts',
-                                          keys={'user_id': 'HASH', 'name': 'RANGE'},
-                                          attributes={'user_id': 'S', 'name': 'S'})
+        self.dynamo_accessor.dynamo_client.create_table(TableName='Carts',
+                                                        KeySchema=[
+                                                            {
+                                                                'AttributeName': 'UserId',
+                                                                'KeyType': 'HASH'
+                                                            },
+                                                            {
+                                                                'AttributeName': 'Name',
+                                                                'KeyType': 'Range'
+                                                            }
+                                                        ],
+                                                        AttributeDefinitions=[
+                                                            {
+                                                                'AttributeName': 'UserId',
+                                                                'AttributeType': 'S'
+                                                            },
+                                                            {
+                                                                'AttributeName': 'Name',
+                                                                'AttributeType': 'S'
+                                                            },
+                                                            {
+                                                                'AttributeName': 'EntityType',
+                                                                'AttributeType': 'S'
+                                                            }
+                                                        ],
+                                                        ProvisionedThroughput={
+                                                            'ReadCapacityUnits': 1,
+                                                            'WriteCapacityUnits': 1
+                                                        },
+                                                        GlobalSecondaryIndexes=[
+                                                            {
+                                                                'IndexName': 'EntityTypeIndex',
+                                                                'KeySchema': [{
+                                                                    'AttributeName': 'EntityType',
+                                                                    'KeyType': 'HASH'
+                                                                }],
+                                                                'Projection': {
+                                                                    'ProjectionType': 'ALL'
+                                                                },
+                                                                'ProvisionedThroughput': {
+                                                                    'ReadCapacityUnits': 1,
+                                                                    'WriteCapacityUnits': 1
+                                                                }
+                                                            }
+                                                        ])
 
     @mock_dynamodb2
     @mock_sts
     def test_query_empty(self):
         self.create_tables()
-        result = self.dynamo_accessor.query('Carts', {'user_id': '123'})
+        result = self.dynamo_accessor.query('Carts', {'UserId': '123'})
         self.assertEqual(0, len(result))
 
     @mock_dynamodb2
     @mock_sts
     def test_insert_one_and_query(self):
         self.create_tables()
-        self.dynamo_accessor.insert_item('Carts', {'user_id': '123', 'name': 'test1', 'entity_type': 'files'})
-        valid_result = self.dynamo_accessor.query('Carts', {'user_id': '123'})
+        self.dynamo_accessor.insert_item('Carts', {'UserId': '123', 'Name': 'test1', 'EntityType': 'files'})
+        valid_result = self.dynamo_accessor.query('Carts', {'UserId': '123'})
         self.assertEqual(1, len(valid_result))
-        empty_result = self.dynamo_accessor.query('Carts', {'user_id': '124'})
+        empty_result = self.dynamo_accessor.query('Carts', {'UserId': '124'})
         self.assertEqual(0, len(empty_result))
+
+    @mock_dynamodb2
+    @mock_sts
+    def test_insert_and_get_single_item(self):
+        self.create_tables()
+        self.dynamo_accessor.insert_item('Carts', {'UserId': '1', 'Name': 'test1'})
+        valid_result = self.dynamo_accessor.get_item('Carts', {'UserId': '1', 'Name': 'test1'})
+        self.assertIsNotNone(valid_result)
+        empty_result = self.dynamo_accessor.get_item('Carts', {'UserId': '1', 'Name': 'test2'})
+        self.assertIsNone(empty_result)
 
     @mock_dynamodb2
     @mock_sts
     def test_insert_and_delete_single(self):
         self.create_tables()
-        self.dynamo_accessor.insert_item('Carts', {'user_id': '123', 'name': 'test1', 'entity_type': 'files'})
-        single_result = self.dynamo_accessor.query('Carts', {'user_id': '123'})
+        self.dynamo_accessor.insert_item('Carts', {'UserId': '123', 'Name': 'test1'})
+        single_result = self.dynamo_accessor.query('Carts', {'UserId': '123'})
         self.assertEqual(1, len(single_result))
-        self.dynamo_accessor.delete_item('Carts', {'user_id': '123', 'name': 'test1'})
-        empty_result = self.dynamo_accessor.query('Carts', {'user_id': '123'})
+        self.dynamo_accessor.delete_item('Carts', {'UserId': '123', 'Name': 'test1'})
+        empty_result = self.dynamo_accessor.query('Carts', {'UserId': '123'})
         self.assertEqual(0, len(empty_result))
+
+    @mock_dynamodb2
+    @mock_sts
+    def test_query_secondary_index(self):
+        self.create_tables()
+        self.dynamo_accessor.insert_item('Carts', {'UserId': '123', 'Name': 'test1', 'EntityType': 'files'})
+        self.dynamo_accessor.insert_item('Carts', {'UserId': '123', 'Name': 'test2', 'EntityType': 'files'})
+        self.dynamo_accessor.insert_item('Carts', {'UserId': '123', 'Name': 'test3', 'EntityType': 'not_files'})
+        valid_result = self.dynamo_accessor.query('Carts', {'EntityType': 'files'}, index_name='EntityTypeIndex')
+        self.assertEqual(2, len(valid_result))
+
+    # TODO: Test failure paths
