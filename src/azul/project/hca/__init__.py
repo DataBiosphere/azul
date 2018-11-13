cddd09ee89479caf0ac975dbeb0399dc1b01ea97
Hannes Schmidt <hannes@ucsc.edu>
2018-11-13 14:36:28 -0800

    Note project names in subscription whitelist for staging

--- src/azul/project/hca/__init__.py
+++ src/azul/project/hca/__init__.py
@@ -34,22 +34,22 @@ dss_subscription_query = {
                                     {
                                         "terms": {
                                             "files.project_json.provenance.document_id": [
-                                                # CBeta Release spreadsheet as of 10/18/2018
-                                                "2c4724a4-7252-409e-b008-ff5c127c7e89",
-                                                "08e7b6ba-5825-47e9-be2d-7978533c5f8c",
-                                                "019a935b-ea35-4d83-be75-e1a688179328",
-                                                "a5ae0428-476c-46d2-a9f2-aad955b149aa",
-                                                "adabd2bd-3968-4e77-b0df-f200f7351661",
-                                                "67bc798b-a34a-4104-8cab-cad648471f69",
-                                                "81b5f43d-3c20-4575-9efa-bfb0b070a6e3",
-                                                "519b58ef-6462-4ed3-8c0d-375b54f53c31",
-                                                "1f9a699a-262c-40a0-8b2c-7ba960ca388c",
-                                                "1f9a699a-262c-40a0-8b2c-7ba960ca388c",
-                                                "62aa3211-bf52-4873-9029-0bcc1d09e553",
-                                                "a71dee10-9a4d-4ea2-a6f3-ae7314112cf1",
-                                                "adb384b2-cd5e-4cf5-9205-5f066474005f",
-                                                "46c58e08-4518-4e45-acfe-bdab2434975d",
-                                                "3eaad325-3666-4b65-a4ed-e23ff71222c1",
+                                                # CBeta Release spreadsheet as of 11/13/2018
+                                                "2c4724a4-7252-409e-b008-ff5c127c7e89",  # treutlein
+                                                "08e7b6ba-5825-47e9-be2d-7978533c5f8c",  # pancreas6decades
+                                                "019a935b-ea35-4d83-be75-e1a688179328",  # neuron_diff
+                                                "a5ae0428-476c-46d2-a9f2-aad955b149aa",  # EMTAB5061
+                                                "adabd2bd-3968-4e77-b0df-f200f7351661",  # Regev-ICA
+                                                "67bc798b-a34a-4104-8cab-cad648471f69",  # Teichmann-mouse-melanoma
+                                                "81b5f43d-3c20-4575-9efa-bfb0b070a6e3",  # Meyer
+                                                "519b58ef-6462-4ed3-8c0d-375b54f53c31",  # EGEOD106540
+                                                "1f9a699a-262c-40a0-8b2c-7ba960ca388c",  # tabulamuris
+                                                "1f9a699a-262c-40a0-8b2c-7ba960ca388c",  # ido_amit
+                                                "62aa3211-bf52-4873-9029-0bcc1d09e553",  # humphreys
+                                                "a71dee10-9a4d-4ea2-a6f3-ae7314112cf1",  # peer
+                                                "adb384b2-cd5e-4cf5-9205-5f066474005f",  # basu
+                                                "46c58e08-4518-4e45-acfe-bdab2434975d",  # 10x-mouse-brain
+                                                "3eaad325-3666-4b65-a4ed-e23ff71222c1",  # rsatija
                                             ]
                                         }
                                     },
@@ -64,7 +64,7 @@ dss_subscription_query = {
                                                 {
                                                     "range": {
                                                         "manifest.version": {
-                                                            "gte": "2018-10-18"
+                                                            "gte": "2018-11-07"
                                                         }
                                                     }
                                                 }
