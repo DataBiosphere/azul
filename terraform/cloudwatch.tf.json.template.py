5f2719a1f5166551c8db45fd51b47ea37be26fed
Abraham <achave11@ucsc.edu>
2018-11-14 16:25:48 -0800

    Create CloudWatch metric resources with Terraform (fixes #332)
    * New terraform file for CloudWatch resources
    * Environment variable for enabling/disabling Terraform CloudWatch resources
    * Make target for destroying Terraform CloudWatch resources
    * Only non personal deployment should configure CloudWatch resources with Terraform

---
+++ terraform/cloudwatch.tf.json.template.py
@@ -0,0 +1,73 @@
+from azul.template import emit
+from azul import config
+from azul.deployment import aws
+
+if config.enable_cloudwatch_alarms:
+    emit({
+        "resource": [
+            {
+                "aws_cloudwatch_metric_alarm": {
+                    "CPUUtilization": {
+                        "alarm_name": config.es_domain+"-CPUUtilization",
+                        "actions_enabled": True,
+                        "comparison_operator": "GreaterThanOrEqualToThreshold",
+                        "evaluation_periods": "2",
+                        "metric_name": "CPUUtilization",
+                        "namespace": "AWS/ES",
+                        "period": "3600",
+                        "statistic": "Average",
+                        "threshold": "85",
+                        "alarm_description": "This metric monitors ES CPU utilization",
+                        "dimensions": {
+                            "ClientId": aws.account,
+                            "DomainName": config.es_domain
+                        },
+                        "insufficient_data_actions": []
+                    }
+                }
+            },
+            {
+                "aws_cloudwatch_metric_alarm": {
+                    "FreeStorageSpace": {
+                        "alarm_name": config.es_domain + "-FreeStorageSpace",
+                        "actions_enabled": True,
+                        "comparison_operator": "LessThanOrEqualToThreshold",
+                        "evaluation_periods": "1",
+                        "metric_name": "FreeStorageSpace",
+                        "namespace": "AWS/ES",
+                        "period": "300",
+                        "statistic": "Average",
+                        "threshold": "14000",
+                        "alarm_description": "This metric monitors ES Disk usage",
+                        "dimensions": {
+                            "ClientId": aws.account,
+                            "DomainName": config.es_domain
+                        },
+                        "insufficient_data_actions": []
+                    }
+                }
+            },
+            {
+                "aws_cloudwatch_metric_alarm": {
+                    "JVMMemoryPressure": {
+                        "alarm_name": config.es_domain + "-JVMMemoryPressure",
+                        "actions_enabled": True,
+                        "comparison_operator": "GreaterThanOrEqualToThreshold",
+                        "evaluation_periods": "1",
+                        "metric_name": "JVMMemoryPressure",
+                        "namespace": "AWS/ES",
+                        "period": "300",
+                        "statistic": "Minimum",
+                        "threshold": "65",
+                        "alarm_description": "This metric monitors memory pressure, should not exceed 65%",
+                        "dimensions": {
+                            "ClientId": aws.account,
+                            "DomainName": config.es_domain
+                        },
+                        "insufficient_data_actions": []
+                    }
+                }
+            }
+        ]
+    })
+
