8ac25659056cdbd56c8e92abf3119d82b6b42e38
Hannes Schmidt <hannes@ucsc.edu>
2018-11-16 20:16:25 -0800

    Streamline reindexer.py refactoring
    - Move delete functionality into module
    - Reduce duplication in handling of defaults

--- scripts/reindex.py
+++ scripts/reindex.py
@@ -10,51 +10,34 @@ import logging
 import sys
 from typing import List
 
-from azul import config
-from azul.es import ESClientFactory
-from azul.plugin import Plugin
 from azul.reindexer import Reindexer
 
 logger = logging.getLogger(__name__)
 
-plugin = Plugin.load()
-
-
-class Defaults:
-    dss_url = config.dss_endpoint
-    indexer_url = "https://" + config.api_lambda_domain('indexer') + "/"
-    es_query = plugin.dss_subscription_query()
-    num_workers = 16
-
+defaults = Reindexer()
 
 parser = argparse.ArgumentParser(description=__doc__)
 parser.add_argument('--dss-url',
-                    default=Defaults.dss_url,
+                    default=defaults.dss_url,
                     help='The URL of the DSS aka Blue Box REST API endpoint')
 parser.add_argument('--indexer-url',
-                    default=Defaults.indexer_url,
+                    default=defaults.indexer_url,
                     help="The URL of the indexer's notification endpoint to send bundles to")
 parser.add_argument('--es-query',
-                    default=Defaults.es_query,
+                    default=defaults.es_query,
                     type=json.loads,
                     help='The Elasticsearch query to use against DSS to enumerate the bundles to be indexed')
 parser.add_argument('--workers',
                     dest='num_workers',
-                    default=Defaults.num_workers,
+                    default=defaults.num_workers,
                     type=int,
                     help='The number of workers that will be sending bundles to the indexer concurrently')
 parser.add_argument('--sync',
                     dest='sync',
-                    default=None,
+                    default=False,
                     action='store_true',
                     help='Have the indexer lambda process the notification synchronously instead of queueing it for '
                          'asynchronous processing by a worker lambda.')
-parser.add_argument('--async',
-                    dest='sync',
-                    default=None,
-                    action='store_false',
-                    help='Have the indexer lambda queue the notification for asynchronous processing by a worker '
-                         'lambda instead of processing it directly.')
 parser.add_argument('--delete',
                     default=False,
                     action='store_true',
@@ -68,22 +51,16 @@ parser.add_argument('--dryrun', '--dry-run',
 def main(argv: List[str]):
     args = parser.parse_args(argv)
 
+    reindexer = Reindexer(indexer_url=args.indexer_url,
+                          dss_url=args.dss_url,
+                          es_query=args.es_query,
+                          sync=args.sync,
+                          num_workers=args.num_workers,
+                          dryrun=args.dryrun)
     if args.delete:
-        es_client = ESClientFactory.get()
-        indexer_cls = plugin.indexer_class()
-        indexer = indexer_cls()
-        for entity_type in indexer.entities():
-            for aggregate in False, True:
-                index_name = config.es_index_name(entity_type, aggregate=aggregate)
-                if es_client.indices.exists(index_name):
-                    if args.dryrun:
-                        logger.info("Would delete index '%s'", index_name)
-                    else:
-                        es_client.indices.delete(index=index_name)
+        reindexer.delete_all_indices()
 
-    Reindexer(indexer_url=args.indexer_url, dss_url=args.dss_url, es_query=args.es_query,
-              sync=args.sync, num_workers=args.num_workers,
-              dryrun=args.dryrun).reindex()
+    reindexer.reindex()
 
 
 if __name__ == "__main__":
