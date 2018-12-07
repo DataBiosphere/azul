9cf37f033a7561b92ba1578b664db25a0b575c6b
Juti Noppornpitak <jnopporn@shiroyuki.com>
2018-12-07 12:00:10 -0500

    Simplified the way FlushableBuffer cleaned up the remaining buffer.

--- test/service/test_buffer.py
+++ test/service/test_buffer.py
@@ -7,48 +7,54 @@ class FlushableBufferTest(TestCase):
     def test_not_flushed_because_of_no_data(self):
         test_min_size = 5
         mock_callback = Mock()
-        fb = FlushableBuffer(test_min_size, mock_callback)
-        fb.flush()
+        with FlushableBuffer(test_min_size, mock_callback) as fb:
+            fb.flush()  # The mock callback is not triggered.
+            mock_callback.assert_not_called()  # The mock callback is not triggered due to insufficient data.
+        # When the buffer is closed, the mock callback will not be triggered due to no remaining data.
         mock_callback.assert_not_called()
         self.assertEqual(0, fb.remaining_size)
 
     def test_not_flushed_because_of_insufficient_data(self):
         test_min_size = 5
         mock_callback = Mock()
-        fb = FlushableBuffer(test_min_size, mock_callback)
-        fb.write(b'?' * 4)
-        fb.flush()
-        mock_callback.assert_not_called()
-        self.assertEqual(4, fb.remaining_size)
+        with FlushableBuffer(test_min_size, mock_callback) as fb:
+            fb.write(b'?' * 4)
+            fb.flush()
+            mock_callback.assert_not_called()  # The mock callback is not triggered due to insufficient data.
+            self.assertEqual(4, fb.remaining_size)
+        mock_callback.assert_called_once()  # When the buffer is closed, the mock callback will be triggered.
+        self.assertEqual(0, fb.remaining_size)
 
     def test_force_flush_with_insufficient_data(self):
         test_min_size = 5
         mock_callback = Mock()
-        fb = FlushableBuffer(test_min_size, mock_callback)
-        fb.write(b'?' * 4)
-        fb.flush(check_limit=False)
-        mock_callback.assert_called_once()
+        with FlushableBuffer(test_min_size, mock_callback) as fb:
+            fb.write(b'?' * 4)
+            mock_callback.assert_not_called()  # The mock callback is not triggered due to insufficient data.
+        mock_callback.assert_called_once()  # The last call is automatically triggered when the buffer is closed.
         self.assertEqual(0, fb.remaining_size)
 
     def test_flush_with_sufficient_data(self):
         test_min_size = 5
         mock_callback = Mock()
-        fb = FlushableBuffer(test_min_size, mock_callback)
-        fb.write(b'?' * 5)
-        fb.flush()
-        mock_callback.assert_called_once()
+        with FlushableBuffer(test_min_size, mock_callback) as fb:
+            fb.write(b'?' * 5)
+            mock_callback.assert_not_called()  # The mock callback is not triggered due to insufficient data.
+        mock_callback.assert_called_once()  # The last call is automatically triggered when the buffer is closed.
         self.assertEqual(0, fb.remaining_size)
 
     def test_multiple_flush_ok(self):
         test_min_size = 5
         mock_callback = Mock()
-        fb = FlushableBuffer(test_min_size, mock_callback)
-        fb.write(b'?' * 5)
-        fb.write(b'?' * 15)
-        fb.flush()
-        fb.write(b'?' * 7)
-        fb.flush()
-        fb.write(b'?' * 4)
-        fb.flush(check_limit=False)
+        with FlushableBuffer(test_min_size, mock_callback) as fb:
+            fb.write(b'?' * 5)
+            fb.write(b'?' * 15)
+            fb.flush()
+            fb.write(b'?' * 7)
+            fb.flush()
+            self.assertEqual(2, mock_callback.call_count)
+            fb.write(b'?' * 4)
+            self.assertEqual(2, mock_callback.call_count)  # The callback is not triggered yet.
+        # At this point, the last call is automatically triggered when the buffer is closed.
         self.assertEqual(3, mock_callback.call_count)
         self.assertEqual(0, fb.remaining_size)
