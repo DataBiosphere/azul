5f7780136a3704377bbfcd092c121695eebeebd0
Benjamin Wu <benjaminwu124@gmail.com>
2018-12-11 18:59:59 -0500

    Add A record for public s3 bucket via route53

--- terraform/s3.tf.json.template.py
+++ terraform/s3.tf.json.template.py
@@ -25,6 +25,20 @@ emit({
                     }
                 }
             }
+        },
+        {
+            "aws_route53_record": {
+                "public_bucket_alias": {
+                    "zone_id": "${data.aws_route53_zone.azul.zone_id}",
+                    "name": config.s3_public_bucket_domain,
+                    "type": "A",
+                    "alias": {
+                        "name": "${aws_s3_bucket.public_bucket.website_endpoint}",
+                        "zone_id": "${aws_s3_bucket.public_bucket.hosted_zone_id}",
+                        "evaluate_target_health": True
+                    }
+                }
+            }
         }
     ]
 })
