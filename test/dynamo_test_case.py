daf65100e684a00df92fdb0ee8fd3c79b8819b43
Benjamin Wu <benjaminwu124@gmail.com>
2018-12-19 14:45:30 -0500

    Store list of containers in DockerContainerTestCase

--- test/dynamo_test_case.py
+++ test/dynamo_test_case.py
@@ -6,8 +6,6 @@ from docker_container_test_case import DockerContainerTestCase
 
 class DynamoTestCase(DockerContainerTestCase):
 
-    _dynamo_docker_container = None
-
     dynamo_accessor = None
 
     dynamo_url = None
@@ -15,19 +13,8 @@ class DynamoTestCase(DockerContainerTestCase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
-        api_container_port = '8000/tcp'
-
-        cls._dynamo_docker_container = cls._docker_client.containers.run(
-            'amazon/dynamodb-local',
-            detach=True,
-            auto_remove=True,
-            ports={api_container_port: ('127.0.0.1', None)})
-        cls.dynamo_url = f'http://{cls.get_container_address(cls._dynamo_docker_container, api_container_port)}'
+        container_host = cls.create_container('amazon/dynamodb-local', '8000/tcp')
+        cls.dynamo_url = f'http://{container_host}'
 
         with mock_sts():
             cls.dynamo_accessor = DynamoDataAccessor(cls.dynamo_url, 'us-east-1')
-
-    @classmethod
-    def tearDownClass(cls):
-        cls._dynamo_docker_container.kill()
-        super().tearDownClass()
