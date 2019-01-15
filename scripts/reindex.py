f2f52b05212a621c5e51c28a0afc01daf1563a3e
Hannes Schmidt <hannes@ucsc.edu>
2019-01-14 22:32:55 -0800

    Improve logging and `--help` output from reindex.py

--- scripts/reindex.py
+++ scripts/reindex.py
@@ -7,6 +7,7 @@ Command line utility to trigger indexing of bundles from DSS into Azul
 import argparse
 import json
 import logging
+import shutil
 import sys
 from typing import List
 
@@ -16,18 +17,30 @@ logger = logging.getLogger(__name__)
 
 defaults = Reindexer()
 
-parser = argparse.ArgumentParser(description=__doc__)
+
+class MyFormatter(argparse.ArgumentDefaultsHelpFormatter):
+    def __init__(self, prog) -> None:
+        super().__init__(prog,
+                         max_help_position=50,
+                         width=min(shutil.get_terminal_size((80, 25)).columns, 120))
+
+
+parser = argparse.ArgumentParser(description=__doc__, formatter_class=MyFormatter)
 parser.add_argument('--dss-url',
+                    metavar='URL',
                     default=defaults.dss_url,
                     help='The URL of the DSS aka Blue Box REST API endpoint')
 parser.add_argument('--indexer-url',
+                    metavar='URL',
                     default=defaults.indexer_url,
                     help="The URL of the indexer's notification endpoint to send bundles to")
 parser.add_argument('--es-query',
                     default=defaults.es_query,
+                    metavar='JSON',
                     type=json.loads,
                     help='The Elasticsearch query to use against DSS to enumerate the bundles to be indexed')
 parser.add_argument('--workers',
+                    metavar='NUM',
                     dest='num_workers',
                     default=defaults.num_workers,
                     type=int,
@@ -40,6 +53,7 @@ group.add_argument('--sync',
                    help='Have the indexer lambda process the notification synchronously instead of queueing it for '
                         'asynchronous processing by a worker lambda.')
 group.add_argument('--prefix',
+                    metavar='NUM',
                    default=0,
                    type=int,
                    help='The length of the bundle UUID prefix by which to partition the set of bundles that match the '
@@ -56,10 +70,19 @@ parser.add_argument('--dryrun', '--dry-run',
                     default=False,
                     action='store_true',
                     help='Just print what would be done, do not actually do it.')
+parser.add_argument('--verbose',
+                    default=False,
+                    action='store_true',
+                    help='Enable verbose logging')
 
 
 def main(argv: List[str]):
     args = parser.parse_args(argv)
+
+    level = logging.DEBUG if args.verbose else logging.INFO
+    logging.basicConfig(format="%(asctime)s %(levelname)-7s %(threadName)-7s: %(message)s", level=level)
+    logging.getLogger().setLevel(logging.INFO)
+    logging.getLogger('azul').setLevel(level)
 
     reindexer = Reindexer(indexer_url=args.indexer_url,
                           dss_url=args.dss_url,
@@ -75,5 +98,4 @@ def main(argv: List[str]):
 
 
 if __name__ == "__main__":
-    logging.basicConfig(format="%(asctime)s %(levelname)-7s %(threadName)-7s: %(message)s", level=logging.INFO)
     main(sys.argv[1:])
