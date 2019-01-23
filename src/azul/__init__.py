ebe359583e66b420ddb39ded9ef7b723cec5c21f
Daniel Sotirhos <dsotirho@ucsc.edu>
2019-01-23 12:54:45 -0800

    Move to Azul-specific bucket for Terraform state (#645)

--- src/azul/__init__.py
+++ src/azul/__init__.py
@@ -189,8 +189,8 @@ class Config:
         return self._term_from_env('AZUL_DEPLOYMENT_STAGE')
 
     @property
-    def terraform_backend_bucket_template(self) -> str:
-        return os.environ['AZUL_TERRAFORM_BACKEND_BUCKET_TEMPLATE']
+    def terraform_backend_bucket(self) -> str:
+        return os.environ['AZUL_TERRAFORM_BACKEND_BUCKET']
 
     @property
     def enable_cloudwatch_alarms(self) -> bool:
