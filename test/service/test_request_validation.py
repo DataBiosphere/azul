7dc5a7e77ea4f2b94543064a8e1e44e93d1db4bf
Michael Krause <michael.krause@ucsc.edu>
2019-01-23 14:32:43 -0800

    Closing file handle: put to last line

--- test/service/test_request_validation.py
+++ test/service/test_request_validation.py
@@ -204,7 +204,6 @@ class FacetNameValidationTest(WebServiceTestCase):
             zip_fname = os.path.basename(os.path.splitext(response.text)[0])
             fh = open(os.path.join(zip_dir, zip_fname, 'data', 'sample.tsv'))
             tsv_file = csv.DictReader(fh, delimiter='\t')
-            fh.close()
             # 2 because self.bundle has 2 files
             self.assertEqual(len(list(tsv_file)), 2,
                              'Wrong number of files were found.')
@@ -219,3 +218,4 @@ class FacetNameValidationTest(WebServiceTestCase):
                 ['drs_uri']
             self.assertEqual(expected_fieldnames, tsv_file.fieldnames,
                              'Manifest headers are not configured correctly')
+            fh.close()
