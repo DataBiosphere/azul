5250d34e4c570c6f200ae7430851c0dfead89b49
Michael Krause <michael.krause@ucsc.edu>
2019-01-23 10:09:51 -0800

    Added original (but modified) test_manifest from develop (manually)

--- test/service/test_request_validation.py
+++ test/service/test_request_validation.py
@@ -160,6 +160,26 @@ class FacetNameValidationTest(WebServiceTestCase):
             expected_field_order = [entity_field.strip() for entity_field in order_settings_file.readlines()]
             self.assertEqual(expected_field_order, actual_field_order, "Field order is not configured correctly")
 
+    @mock_sts
+    @mock_s3
+    def test_manifest(self):
+        # moto will mock the requests.get call so we can't hit localhost; add_passthru let's us hit the server
+        # see this GitHub issue and comment: https://github.com/spulec/moto/issues/1026#issuecomment-380054270
+        with ResponsesHelper() as helper:
+            helper.add_passthru(self.base_url)
+            storage_service = StorageService()
+            storage_service.create_bucket()
+
+            url = self.base_url + '/repository/files/export?filters={"file":{}}'
+            response = requests.get(url)
+            self.assertEqual(200, response.status_code, 'Unable to download manifest')
+            tsv_file = csv.DictReader(response.iter_lines(decode_unicode=True), delimiter='\t')
+            # 2 because self.bundle has 2 files
+            self.assertEqual(len(list(tsv_file)), 2, 'Wrong number of files were found.')
+            manifest_config = json.load(open('{}/request_config.json'.format(self.service_config_dir), 'r'))['manifest']
+            expected_fieldnames = list(manifest_config['bundles'].keys()) + list(manifest_config['contents.files'].keys())
+            self.assertEqual(expected_fieldnames, tsv_file.fieldnames, 'Manifest headers are not configured correctly')
+
     @mock_s3
     @mock_sts
     def test_bdbag_manifest(self):
