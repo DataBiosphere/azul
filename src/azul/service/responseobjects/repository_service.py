c3fe3fedd366d7958d323b7a564ece768572ad88
jesse <brennan@ucsc.edu>
2019-01-31 10:51:24 -0800

    Factor out getting single vs multiple items

--- src/azul/service/responseobjects/repository_service.py
+++ src/azul/service/responseobjects/repository_service.py
@@ -9,32 +9,28 @@ class RepositoryService(AbstractService):
         # FIXME: find a better way to get this func.
         self.file_url = url_func
 
-    def get_data(self, entity_type, pagination, filters=None, item_id=None):
-        filters = self.parse_filters(filters)
-        # FIXME: single item stuff should be abstracted away
-        if item_id is not None:
-            if entity_type == 'projects':
-                filters['file']['projectId'] = {"is": [item_id]}
-            else:
-                filters['file']['fileId'] = {"is": [item_id]}
-
-        # FIXME: which of these args are really optional??? I think all are necessary and should
-        # be passed that way.
-        # FIXME: transform_request may throw BadArgumentException. Catch that here??
+    def _get_data(self, entity_type, pagination, filters=None):
+        # FIXME: which of these args are really optional??? (looks like none of them)
         response = self.es_td.transform_request(filters=filters,
                                                 pagination=pagination,
                                                 post_filter=True,
                                                 entity_type=entity_type)
-
         if entity_type == 'files':
             for hit in response['hits']:
                 for file in hit['files']:
                     file['url'] = self.file_url(file['uuid'], version=file['version'], replica='aws')
+        return response
 
-        # FIXME: single item stuff should be abstracted away
-        if item_id is not None:
-            return response['hits'][0]
+    def _get_item(self, entity_type, item_id, pagination, filters):
+        if entity_type == 'projects':
+            filters['file']['projectId'] = {"is": [item_id]}
+        else:
+            filters['file']['fileId'] = {"is": [item_id]}
+        response = self._get_data(entity_type, pagination, filters)
+        return response['hits'][0]
 
+    def _get_items(self, entity_type, pagination, filters):
+        response = self._get_data(entity_type, pagination, filters)
         if entity_type == 'projects':
             # Filter out certain fields if getting *list* of projects
             for hit in response['hits']:
@@ -42,5 +38,10 @@ class RepositoryService(AbstractService):
                     project.pop('contributors')
                     project.pop('projectDescription')
                     project.pop('publications')
-
         return response
+
+    def get_data(self, entity_type, pagination, filters=None, item_id=None):
+        filters = self.parse_filters(filters)
+        if item_id is not None:
+            return self._get_item(entity_type, item_id, pagination, filters)
+        return self._get_items(entity_type, pagination, filters)
