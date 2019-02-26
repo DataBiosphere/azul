2c17ab9dc368584600ed3b7a8816d992ad3b4269
Michael Krause <michael.krause@ucsc.edu>
2019-02-26 13:59:02 -0800

    Export manifest in BDBag to Terra (#604)

--- test/service/test_request_validation.py
+++ test/service/test_request_validation.py
@@ -3,6 +3,8 @@ import json
 import logging
 import os
 import sys
+import re
+
 from tempfile import TemporaryDirectory
 from unittest import mock
 
@@ -10,10 +12,12 @@ from moto import mock_s3, mock_sts
 import requests
 
 import azul.changelog
+from azul import config
 from azul.service import service_config
 from azul.service.responseobjects.storage_service import StorageService
 from retorts import ResponsesHelper
 from service import WebServiceTestCase
+from zipfile import ZipFile
 
 log = logging.getLogger(__name__)
 
@@ -175,5 +179,57 @@ class FacetNameValidationTest(WebServiceTestCase):
             # 2 because self.bundle has 2 files
             self.assertEqual(len(list(tsv_file)), 2, 'Wrong number of files were found.')
             manifest_config = json.load(open('{}/request_config.json'.format(self.service_config_dir), 'r'))['manifest']
-            expected_fieldnames = list(manifest_config['bundles'].keys()) + list(manifest_config['contents.files'].keys())
+            expected_fieldnames = list(manifest_config['bundles'].keys()) + list(
+                manifest_config['contents.files'].keys())
             self.assertEqual(expected_fieldnames, tsv_file.fieldnames, 'Manifest headers are not configured correctly')
+
+    @mock_sts
+    @mock_s3
+    def test_bdbag_manifest(self):
+        """
+        As in test_manifest, moto will mock the requests.get call so we can't hit localhost; add_passthru let's us hit
+        the server (see GitHub issue and comment: https://github.com/spulec/moto/issues/1026#issuecomment-380054270)
+        """
+        logging.getLogger('test_request_validation').warning('test_manifest is invoked')
+        with ResponsesHelper() as helper, TemporaryDirectory() as zip_dir:
+            helper.add_passthru(self.base_url)
+            storage_service = StorageService()
+            storage_service.create_bucket()
+            url = self.base_url + '/repository/files/export?filters={"file":{}}&format=bdbag'
+            response = requests.get(url)
+            self.assertEqual(200, response.status_code, 'Unable to download manifest')
+            with ZipFile(response.text, 'r') as zip_fh:
+                zip_fh.extractall(zip_dir)
+            zip_fname = os.path.basename(os.path.splitext(response.text)[0])
+            with open(os.path.join(zip_dir, zip_fname, 'data', 'participant.tsv'), 'r') as fh:
+                observed = list(csv.reader(fh, delimiter='\t'))
+                expected = [['entity:participant_id'], ['7b07b9d0-cc0e-4098-9f64-f4a569f7d746']]
+                self.assertEqual(expected, observed, 'participant.tsv contains incorrect data')
+
+            expectations = [
+                ('entity:sample_id', 'DID_scRSq06'),
+                ('participant_id', '7b07b9d0-cc0e-4098-9f64-f4a569f7d746'),
+                ('cell_suspension_id', '412898c5-5b9b-4907-b07c-e9b89666e204'),
+                ('bundle_uuid', 'aaa96233-bf27-44c7-82df-b4dc15ad4d9d'),
+                ('bundle_version', '2018-11-02T113344.698028Z'),
+                ('file_content_type', 'application/gzip; dcp-type=data'),
+                ('file_name', 'SRR3562915_1.fastq.gz'),
+                ('file_sha256', '77337cb51b2e584b5ae1b99db6c163b988cbc5b894dda2f5d22424978c3bfc7a'),
+                ('file_size', '195142097'),
+                ('file_uuid', '7b07f99e-4a8a-4ad0-bd4f-db0d7a00c7bb'),
+                ('file_version', '2018-11-02T113344.698028Z'),
+                ('file_indexed', 'False'),
+                ('file_url', config.dss_endpoint + '/files/7b07f99e-4a8a-4ad0-bd4f-db0d7a00c7bb?'
+                                                   'version=2018-11-02T113344.698028Z&replica=gcp')
+            ]
+            expected_fieldnames, expected_row = map(list, zip(*expectations))
+            with open(os.path.join(zip_dir, zip_fname, 'data', 'sample.tsv'), 'r') as fh:
+                samples = iter(csv.reader(fh, delimiter='\t'))
+                row = next(samples)
+                self.assertEqual(expected_fieldnames, row, 'Manifest headers are not configured correctly')
+                row = next(samples)
+                self.assertEqual(row, expected_row, 'Row in sample.tsv incorrect.')
+                remaining_rows = list(samples)
+                self.assertTrue(all(len(row) == len(expected_row) for row in remaining_rows),
+                                'Incorrect number of entries in a row.')
+                self.assertEqual(len(remaining_rows), 1, 'Found incorrect number of files.')  # self.bundle has 2 files
