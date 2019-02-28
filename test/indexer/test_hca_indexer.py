5d9f0561fd79bf43be435242ae33f532a026103b
Daniel Sotirhos <dsotirho@ucsc.edu>
2019-02-27 17:34:26 -0800

    Change organ_part to an array (#699)

--- test/indexer/test_hca_indexer.py
+++ test/indexer/test_hca_indexer.py
@@ -325,11 +325,18 @@ class TestHCAIndexer(IndexerTestCase):
             if aggregate:
                 bundles = result_dict['_source']['bundles']
                 self.assertEqual(1, len(bundles))
+            specimens = contents['specimens']
             cell_suspensions = contents['cell_suspensions']
             if entity_type == 'files' and contents['files'][0]['file_format'] == 'pdf':
                 # The PDF files in that bundle aren't linked to a specimen
                 self.assertEqual(0, len(cell_suspensions))
             else:
+                for specimen in specimens:
+                    self.assertIn('bone marrow', specimen['organ_part'])
+                    self.assertIn('temporal lobe', specimen['organ_part'])
+                for cell_suspension in cell_suspensions:
+                    self.assertIn('bone marrow', cell_suspension['organ_part'])
+                    self.assertIn('temporal lobe', cell_suspension['organ_part'])
                 self.assertEqual(1 if aggregate else 384, len(cell_suspensions))
                 # 384 wells in total, four of them empty, the rest with a single cell
                 self.assertEqual(380, sum(cs['total_estimated_cells'] for cs in cell_suspensions))
@@ -439,7 +446,7 @@ class TestHCAIndexer(IndexerTestCase):
                 inner_specimens += 1
                 expect_list = aggregate and entity_type != 'specimens'
                 self.assertEqual(['Brain'] if expect_list else 'Brain', specimen['organ'])
-                self.assertEqual([None] if expect_list else None, specimen['organ_part'])
+                self.assertEqual([None], specimen['organ_part'])
 
         projects = 1
         specimens = 4
