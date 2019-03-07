4ff0139028d9aa4c04e3cd90b9bbcb30cdd7b7b4
Hannes Schmidt <hannes@ucsc.edu>
2019-03-06 22:33:29 -0800

    Pass `subscription_type` when subscribing to DSS (#771)
    https://github.com/HumanCellAtlas/data-store/pull/1822
    https://github.com/HumanCellAtlas/data-store/pull/1915
    https://github.com/HumanCellAtlas/data-store/issues/1957

--- src/azul/subscription.py
+++ src/azul/subscription.py
@@ -9,21 +9,22 @@ from azul.plugin import Plugin
 logger = logging.getLogger(__name__)
 
 
-def subscribe(dss_client, subscribe=True):
-    response = dss_client.get_subscriptions(replica='aws')
+def manage_subscriptions(dss_client, subscribe=True):
+    response = call_client(dss_client.get_subscriptions, replica='aws')
     current_subscriptions = freeze(response['subscriptions'])
 
     if subscribe:
         plugin = Plugin.load()
         base_url = config.indexer_endpoint()
         prefix = config.dss_query_prefix
-        new_subscriptions = [freeze(dict(replica='aws', es_query=query, callback_url=base_url + path))
+        new_subscriptions = [freeze(dict(replica='aws',
+                                         es_query=query,
+                                         callback_url=base_url + path))
                              for query, path in [(plugin.dss_subscription_query(prefix), '/'),
                                                  (plugin.dss_deletion_subscription_query(prefix), '/delete')]]
     else:
         new_subscriptions = []
 
-    matching_subscriptions = []
     for subscription in current_subscriptions:
         # Note the use of <= to allow for the fact that DSS returns subscriptions with additional attributes, more
         # than were originally supplied. If the subscription returned by DSS is a superset of the subscription we want
@@ -32,14 +33,24 @@ def subscribe(dss_client, subscribe=True):
                                       if new_subscription.items() <= subscription.items()), None)
         if matching_subscription:
             logging.info('Already subscribed: %r', thaw(subscription))
-            matching_subscriptions.append(matching_subscription)
+            new_subscriptions.remove(matching_subscription)
         else:
-            logging.info('Removing subscription: %r', thaw(subscription))
-            dss_client.delete_subscription(uuid=subscription['uuid'], replica=subscription['replica'])
+            subscription = thaw(subscription)
+            logging.info('Removing stale subscription: %r', subscription)
+            call_client(dss_client.delete_subscription,
+                        uuid=subscription['uuid'],
+                        replica=subscription['replica'])
 
     for subscription in new_subscriptions:
-        if subscription not in matching_subscriptions:
-            subscription = thaw(subscription)
-            response = dss_client.put_subscription(**subscription)
-            subscription['uuid'] = response['uuid']
-            logging.info('Registered subscription %r.', subscription)
+        subscription = thaw(subscription)
+        response = dss_client.put_subscription(**subscription)
+        subscription['uuid'] = response['uuid']
+        logging.info('Registered subscription %r.', subscription)
+
+
+def call_client(method, *args, **kwargs):
+    # Work around https://github.com/HumanCellAtlas/data-store/issues/1957
+    if 'subscription_type' in method.parameters:
+        kwargs['subscription_type'] = 'elasticsearch'
+    # noinspection PyArgumentList
+    return method(*args, **kwargs)
