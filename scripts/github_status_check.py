2d0fe29d6464acccbeacd87cc9104c15c0ad2ca0
Hannes Schmidt <hannes@ucsc.edu>
2019-03-08 19:50:16 -0800

    Gitlab posts status check on Github

---
+++ scripts/github_status_check.py
@@ -0,0 +1,15 @@
+import os
+import sys
+
+from github import Github
+from azul import config
+
+if __name__ == '__main__':
+    gh = Github(config.github_access_token)
+    repo = gh.get_repo(config.github_project)
+    commit = repo.get_commit(sha=os.environ['CI_COMMIT_SHA'])
+    status = sys.argv[1]
+    commit.create_status(state=status,
+                         target_url=os.environ['CI_PIPELINE_URL'],
+                         description=f'Gitlab build status is {status}',
+                         context='gitlab')
