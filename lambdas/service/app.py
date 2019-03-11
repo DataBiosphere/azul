96dfd2f9535f0ddcebc86f1403c406e3427153bf
Hannes Schmidt <hannes@ucsc.edu>
2019-03-11 00:36:34 -0700

    Fix: DSS proxy test fails on integration deployment

--- lambdas/service/app.py
+++ lambdas/service/app.py
@@ -664,7 +664,7 @@ def files_proxy(uuid):
             if file_name is None:
                 file_name = uuid
             bucket = location.netloc.partition('.')[0]
-            assert bucket == config.dss_checkout_bucket
+            assert bucket == config.dss_checkout_bucket, bucket
             location = s3.generate_presigned_url(ClientMethod=s3.get_object.__name__,
                                                  ExpiresIn=round(expires - time.time()),
                                                  Params={
