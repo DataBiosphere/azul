6d69de2cd29d2ed5e54e0cb7703ace9ac41a0869
Hannes Schmidt <hannes@ucsc.edu>
2019-03-12 19:24:35 -0700

    Refactor DRS support, fix DRS URLs (#774)

--- src/azul/drs.py
+++ src/azul/drs.py
@@ -1,9 +1,63 @@
-#! usr/bin/env python3
+from typing import Optional, Tuple
+from urllib.parse import urlunsplit, SplitResult, urlencode, urlsplit
+
+from azul import config
 
 
-def dos_object_url(file_id: str) -> str:
+def object_url(file_uuid: str,
+               file_version: Optional[str] = None,
+               base_url: Optional[str] = None) -> str:
     """
-    Return a DOS URL for an input file ID
+    The DRS URL for a given DSS file UUID and version. The return value will point at the bare-bones DRS data object
+    endpoint in the web service.
+
+    :param file_uuid: the DSS file UUID of the file
+
+    :param file_version: the DSS file version of the file
+
+    :param base_url: an optional service endpoint, e.g for local test servers. If absent, the service endpoint for the
+                     current deployment will be used.
     """
-    dos_endpoint = '/ga4gh/dos/v1/dataobjects'
-    return f'{dos_endpoint}/{file_id}'
+    scheme, netloc = _endpoint(base_url)
+    return urlunsplit(SplitResult(scheme='dos',
+                                  netloc=netloc,
+                                  path=file_uuid,
+                                  query=_url_query(file_version),
+                                  fragment=''))
+
+
+def http_object_url(file_uuid: str,
+                    file_version: Optional[str] = None,
+                    base_url: Optional[str] = None) -> str:
+    """
+    The HTTP(S) URL for a given DSS file UUID and version. The return value will point at the bare-bones DRS data
+    object endpoint in the web service.
+
+    :param file_uuid: the DSS file UUID of the file
+
+    :param file_version: the DSS file version of the file
+
+    :param base_url: an optional service endpoint, e.g for local test servers. If absent, the service endpoint for the
+                     current deployment will be used.
+    """
+    scheme, netloc = _endpoint(base_url)
+    return urlunsplit(SplitResult(scheme=scheme,
+                                  netloc=netloc,
+                                  path=drs_http_object_path(file_uuid),
+                                  query=_url_query(file_version),
+                                  fragment=''))
+
+
+def drs_http_object_path(file_uuid: str) -> str:
+    return f'/ga4gh/dos/v1/dataobjects/{file_uuid}'
+
+
+def _endpoint(base_url: Optional[str]) -> Tuple[str, str]:
+    if base_url is None:
+        base_url = config.service_endpoint()
+    base_url = urlsplit(base_url)
+    return base_url.scheme, base_url.netloc
+
+
+def _url_query(file_version: Optional[str]) -> str:
+    return urlencode({'version': file_version} if file_version else {})
