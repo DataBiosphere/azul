74aa9535af4030187710780dd0e77f51a264f565
Abraham <achave11@ucsc.edu>
2019-03-20 15:17:24 -0700

    Require auth for notifications (#96)
    * Update subscriptions to DSS with HMAC credentials
    * Modularized HMAC functionality for easy accessibility to Azul

--- lambdas/indexer/app.py
+++ lambdas/indexer/app.py
@@ -23,6 +23,7 @@ from more_itertools import chunked, partition
 from azul import config
 from azul.chalice import AzulChaliceApp
 from azul.health import Health
+from azul import hmac
 from azul.indexer import IndexWriter
 from azul.plugin import Plugin
 from azul.azulclient import AzulClient
@@ -77,6 +78,7 @@ def post_notification():
     """
     Receive a notification event and either queue it for asynchronous indexing or process it synchronously.
     """
+    hmac.verify(current_request=app.current_request)
     notification = app.current_request.json_body
     log.info("Received notification %r", notification)
     validate_request_syntax(notification)
@@ -134,6 +136,7 @@ def delete_notification():
     """
     Receive a deletion event and process it asynchronously
     """
+    hmac.verify(current_request=app.current_request)
     notification = app.current_request.json_body
     validate_request_syntax(notification)
     log.info("Received deletion notification %r", notification)
