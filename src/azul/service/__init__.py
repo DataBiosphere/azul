92e10d06c00b808a72c6ad3ed715418115c7cb3f
Hannes Schmidt <hannes@ucsc.edu>
2019-03-25 18:31:10 -0700

    Add BDBag support to new manifest endpoint (#827)

--- src/azul/service/__init__.py
+++ src/azul/service/__init__.py
@@ -10,7 +10,10 @@ logger = logging.getLogger(__name__)
 
 class AbstractService:
 
-    def parse_filters(self, filters: Optional[str]):
+    # FIXME: Convert back to instance method once #566 is solved
+    #  https://github.com/DataBiosphere/azul/issues/566
+    @classmethod
+    def parse_filters(cls, filters: Optional[str]):
         """
         Parses filters. Handles default cases where filters are None (not set) or {}
         :param filters: string of python interpretable data
@@ -24,7 +27,6 @@ class AbstractService:
             filters = ast.literal_eval(filters)
         except ValueError as e:
             logger.error('Malformed filters parameter: {}'.format(e))
-            # FIXME: should this be a new kind of exception??
             raise BadArgumentException('Malformed filters parameter')
         else:
             return default_filters if filters == {} else filters
