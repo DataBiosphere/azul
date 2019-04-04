DataBiosphere/azul

commit 1d76888a903c19af7259f541457155fd219bb030

Don't require new projects to be whitelisted in staging

src/azul/project/hca/__init__.py
@@ -66,21 +66,12 @@ class Plugin(azul.plugin.Plugin):
                                                 }
                                             },
                                             {
-                                                "bool": {
-                                                    "must": [
-                                                        {
-                                                            "prefix": {
-                                                                "files.project_json.project_core.project_short_name": "staging/"
-                                                            }
-                                                        },
-                                                        {
-                                                            "range": {
-                                                                "manifest.version": {
-                                                                    "gte": "2018-11-27"
-                                                                }
-                                                            }
+                                                {
+                                                    "range": {
+                                                        "manifest.version": {
+                                                            "gte": "2019-04-03"
                                                         }
-                                                    ]
+                                                    }
                                                 }
                                             }
                                         ]

commit 73d22568d69efce378cbc4085f96f2844d27a619

Fix: staging subscription query
Regression from 1d76888a903c19af7259f541457155fd219bb030

Fixes

```
Traceback (most recent call last):
  File "scripts/subscribe.py", line 38, in <module>
    main(sys.argv[1:])
  File "scripts/subscribe.py", line 32, in main
    subscription.manage_subscriptions(dss_client, subscribe=options.subscribe)
  File "/builds/azul/azul/src/azul/subscription.py", line 25, in manage_subscriptions
    for query, path in [(plugin.dss_subscription_query(prefix), '/'),
  File "/builds/azul/azul/src/azul/project/hca/__init__.py", line 80, in dss_subscription_query
    ] if config.dss_endpoint == "https://dss.staging.data.humancellatlas.org/v1" else [
TypeError: unhashable type: 'dict'
Makefile:15: recipe for target 'subscribe' failed
```

src/azul/project/hca/__init__.py
@@ -66,11 +66,9 @@ class Plugin(azul.plugin.Plugin):
                                                 }
                                             },
                                             {
-                                                {
-                                                    "range": {
-                                                        "manifest.version": {
-                                                            "gte": "2019-04-03"
-                                                        }
+                                                "range": {
+                                                    "manifest.version": {
+                                                        "gte": "2019-04-03"
                                                     }
                                                 }
                                             }