DataBiosphere/azul

commit 80edba8d0b2c32522237386fc9a6cac36d03a848

Add all metadata to manifest (#720)

src/azul/service/responseobjects/hca_response_v5.py
@@ -7,7 +7,7 @@ from tempfile import TemporaryDirectory
 from io import TextIOWrapper, StringIO
 from itertools import chain
 import logging
-from typing import MutableSet
+from typing import MutableSet, List
 
 from uuid import uuid4
 
@@ -32,6 +32,7 @@ from azul.service.responseobjects.utilities import json_pp
 from azul.json_freeze import freeze, thaw
 from azul.strings import to_camel_case
 from azul.transformer import SetAccumulator
+from azul.types import JSON
 
 logger = logging.getLogger(__name__)
 module_logger = logger  # FIXME: inline (https://github.com/DataBiosphere/azul/issues/419)
@@ -202,6 +203,24 @@ class ManifestResponse(AbstractResponse):
         m = self.manifest_entries[keyname]
         return [untranslated.get(es_name, "") for es_name in m.values()]
 
+    def _extract_fields(self, entities: List[JSON], column_mapping: JSON):
+        def validate(s: str) -> str:
+            assert '||' not in s
+            return s
+
+        cell_values = []
+        for field_name in column_mapping.values():
+            cell_value = []
+            for entity in entities:
+                field_value = entity[field_name]
+                if isinstance(field_value, list):
+                    cell_value += [validate(str(v)) for v in field_value if v is not None]
+                else:
+                    cell_value.append(validate(str(field_value)))
+            cell_values.append(" || ".join(sorted(set(cell_value))))
+
+        return cell_values
+
     def _push_content(self) -> str:
         """
         Push the content to S3 with multipart upload
@@ -215,9 +234,7 @@ class ManifestResponse(AbstractResponse):
             with FlushableBuffer(AWS_S3_DEFAULT_MINIMUM_PART_SIZE, multipart_upload.push) as buffer:
                 buffer_wrapper = TextIOWrapper(buffer, encoding="utf-8", write_through=True)
                 writer = csv.writer(buffer_wrapper, dialect='excel-tab')
-
-                writer.writerow(list(self.manifest_entries['bundles'].keys()) +
-                                list(self.manifest_entries['contents.files'].keys()))
+                writer.writerow(self.ordered_column_names)
                 for hit in self.es_search.scan():
                     self._iterate_hit_tsv(hit, writer)
 
@@ -252,8 +269,7 @@ class ManifestResponse(AbstractResponse):
 
         output = StringIO()
         writer = csv.writer(output, dialect='excel-tab')
-        writer.writerow(list(self.manifest_entries['bundles'].keys()) +
-                        list(self.manifest_entries['contents.files'].keys()))
+        writer.writerow(self.ordered_column_names)
         for hit in es_search.scan():
             self._iterate_hit_tsv(hit, writer)
 
@@ -311,14 +327,19 @@ class ManifestResponse(AbstractResponse):
 
     def _iterate_hit_tsv(self, es_search_hit, writer):
         hit_dict = es_search_hit.to_dict()
-        assert len(hit_dict['contents']['files']) == 1
-        file = hit_dict['contents']['files'][0]
-        file_fields = self._translate(file, 'contents.files')
 
-        for bundle in hit_dict['bundles']:
-            # FIXME: If a file is in multiple bundles, the manifest will list it twice. `hca dss download_manifest`
-            # would download the file twice (https://github.com/DataBiosphere/azul/issues/423).
-            writer.writerow(self._translate(bundle, 'bundles') + file_fields)
+        inner_entity_fields = []
+        for doc_path, column_mapping in self.manifest_entries.items():
+            doc_path = doc_path.split('.')
+            assert doc_path
+            entities = hit_dict
+            for key in doc_path[:-1]:
+                entities = entities.get(key, {})
+            entities = entities.get(doc_path[-1], [])
+
+            inner_entity_fields += self._extract_fields(entities, column_mapping)
+
+        writer.writerow(inner_entity_fields)
 
     def _iterate_hit_bdbag(self, es_search_hit, participants: MutableSet[str], sample_writer):
         hit_dict = es_search_hit.to_dict()
@@ -373,6 +394,9 @@ class ManifestResponse(AbstractResponse):
         self.mapping = mapping
         self.storage_service = StorageService()
         self.format = format
+
+        sources = list(self.manifest_entries.keys())
+        self.ordered_column_names = [field_name for source in sources for field_name in self.manifest_entries[source]]
 
 
 class EntryFetcher:

test/service/test_request_validation.py
@@ -168,6 +168,7 @@ class FacetNameValidationTest(WebServiceTestCase):
     @mock_sts
     @mock_s3
     def test_manifest(self):
+        self._index_canned_bundle(("f79257a7-dfc6-46d6-ae00-ba4b25313c10", "2018-09-14T133314.453337Z"))
         # moto will mock the requests.get call so we can't hit localhost; add_passthru let's us hit the server
         # see this GitHub issue and comment: https://github.com/spulec/moto/issues/1026#issuecomment-380054270
         with ResponsesHelper() as helper:
@@ -175,16 +176,70 @@ class FacetNameValidationTest(WebServiceTestCase):
             storage_service = StorageService()
             storage_service.create_bucket()
 
-            url = self.base_url + '/repository/files/export?filters={"file":{}}'
-            response = requests.get(url)
-            self.assertEqual(200, response.status_code, 'Unable to download manifest')
-            tsv_file = csv.DictReader(response.iter_lines(decode_unicode=True), delimiter='\t')
-            # 2 because self.bundle has 2 files
-            self.assertEqual(len(list(tsv_file)), 2, 'Wrong number of files were found.')
-            manifest_config = json.load(open('{}/request_config.json'.format(self.service_config_dir), 'r'))['manifest']
-            expected_fieldnames = list(manifest_config['bundles'].keys()) + list(
-                manifest_config['contents.files'].keys())
-            self.assertEqual(expected_fieldnames, tsv_file.fieldnames, 'Manifest headers are not configured correctly')
+            for single_part in False, True:
+                with self.subTest(is_single_part=single_part):
+                    with mock.patch.object(type(config), 'disable_multipart_manifests', single_part):
+                        url = self.base_url + '/repository/files/export?filters={"file":{}}'
+                        response = requests.get(url)
+                        self.assertEqual(200, response.status_code, 'Unable to download manifest')
+
+                        expected = [
+                            ('bundle_uuid', 'f79257a7-dfc6-46d6-ae00-ba4b25313c10',
+                                            'f79257a7-dfc6-46d6-ae00-ba4b25313c10'),
+                            ('bundle_version', '2018-09-14T133314.453337Z','2018-09-14T133314.453337Z'),
+                            ('file_content_type', 'application/pdf; dcp-type=data', 'application/gzip; dcp-type=data'),
+                            ('file_name', 'SmartSeq2_RTPCR_protocol.pdf', '22028_5#300_1.fastq.gz'),
+                            ('file_sha256', '2f6866c4ede92123f90dd15fb180fac56e33309b8fd3f4f52f263ed2f8af2f16',
+                                            '3125f2f86092798b85be93fbc66f4e733e9aec0929b558589c06929627115582'),
+                            ('file_size', '29230', '64718465'),
+                            ('file_uuid', '5f9b45af-9a26-4b16-a785-7f2d1053dd7c',
+                                          'f2b6c6f0-8d25-4aae-b255-1974cc110cfe'),
+                            ('file_version', '2018-09-14T123347.012715Z', '2018-09-14T123343.720332Z'),
+                            ('file_indexed', 'False', 'False'),
+                            ('file_format', 'pdf', 'fastq.gz'),
+                            ('total_estimated_cells', '', '9001'),
+                            ('instrument_manufacturer_model', '', 'Illumina HiSeq 2500'),
+                            ('library_construction_approach', '', 'Smart-seq2'),
+                            ('document_id', '67bc798b-a34a-4104-8cab-cad648471f69',
+                             '67bc798b-a34a-4104-8cab-cad648471f69'),
+                            ('institutions', 'DKFZ German Cancer Research Center || EMBL-EBI || University of Cambridge'
+                                             ' || University of Helsinki || Wellcome Trust Sanger Institute',
+                                             'DKFZ German Cancer Research Center || EMBL-EBI || University of Cambridge'
+                                             ' || University of Helsinki || Wellcome Trust Sanger Institute'),
+                            ('laboratory', 'Human Cell Atlas Data Coordination Platform || MRC Cancer Unit'
+                                           ' || Sarah Teichmann',
+                                           'Human Cell Atlas Data Coordination Platform || MRC Cancer Unit'
+                                           ' || Sarah Teichmann'),
+                            ('project_shortname', 'Mouse Melanoma', 'Mouse Melanoma'),
+                            ('project_title', 'Melanoma infiltration of stromal and immune cells',
+                                              'Melanoma infiltration of stromal and immune cells'),
+                            ('biological_sex', '', 'female'),
+                            ('specimen_id', '', '1209_T || 1210_T'),
+                            ('specimen_document_id', '', 'aaaaaaaa-7bab-44ba-a81d-3d8cb3873244'
+                                                     ' || b4e55fe1-7bab-44ba-a81d-3d8cb3873244'),
+                            ('disease', '', ''),
+                            ('donor_biomaterial_id', '', '1209'),
+                            ('donor_document_id', '', '89b50434-f831-4e15-a8c0-0d57e6baa94c'),
+                            ('genus_species', '', 'Mus musculus'),
+                            ('organ', '', 'brain || tumor'),
+                            ('organ_part', '', ''),
+                            ('organism_age', '', '6-12'),
+                            ('organism_age_unit', '', 'week'),
+                            ('preservation_method', '', '')
+                        ]
+
+                        expected_fieldnames, expected_pdf_row, expected_fastq_row = map(list, zip(*expected))
+                        tsv_file = csv.reader(response.iter_lines(decode_unicode=True), delimiter='\t')
+                        actual_fieldnames = next(tsv_file)
+                        rows = freeze(list(tsv_file))
+
+                        self.assertEqual(expected_fieldnames, actual_fieldnames,
+                                         'Manifest headers are not configured correctly')
+                        self.assertEqual(len(rows), 7, 'Wrong number of files were found.')
+                        self.assertIn(freeze(expected_pdf_row), rows, 'Expected pdf contains invalid values.')
+                        self.assertIn(freeze(expected_fastq_row), rows, 'Expected fastq contains invalid values.')
+                        self.assertTrue(all(len(row) == len(expected_pdf_row) for row in rows),
+                                        'Row sizes in manifest are not consistent.')
 
     @mock_sts
     @mock_s3

commit 76c9306e0d17f4d0a0a3cec2f7c1c1599b6295fc

One manifest row per occurrence of a file in a bundle (#955)
Regression from 80edba8d0b2c32522237386fc9a6cac36d03a848

src/azul/service/responseobjects/hca_response_v5.py
@@ -283,11 +283,13 @@ class ManifestResponse(AbstractResponse):
         writer.writeheader()
         for hit in self.es_search.scan():
             doc = hit.to_dict()
-            row = {}
-            for doc_path, column_mapping in self.manifest_entries.items():
-                entities = self._get_entities(doc_path, doc)
-                self._extract_fields(entities, column_mapping, row)
-            writer.writerow(row)
+            for bundle in list(doc['bundles']):
+                doc['bundles'] = [bundle]
+                row = {}
+                for doc_path, column_mapping in self.manifest_entries.items():
+                    entities = self._get_entities(doc_path, doc)
+                    self._extract_fields(entities, column_mapping, row)
+                writer.writerow(row)
 
     def _create_bdbag_archive(self) -> str:
         with TemporaryDirectory() as bag_path:

test/local_integration_test.py
@@ -24,7 +24,6 @@ from azul.decorators import memoized_property
 from azul.azulclient import AzulClient
 from azul.requests import requests_session
 from azul import drs
-from azul.service.responseobjects.hca_response_v5 import ManifestResponse
 
 logger = logging.getLogger(__name__)
 
@@ -130,29 +129,24 @@ class IntegrationTest(unittest.TestCase):
         return response.content
 
     def check_manifest(self, response: bytes):
-        self._check_manifest(BytesIO(response), 'bundle_uuid', allow_multiple_uuid=True)
+        self._check_manifest(BytesIO(response), 'bundle_uuid')
 
     def check_bdbag(self, response: bytes):
         with ZipFile(BytesIO(response)) as zip_fh:
             data_path = os.path.join(os.path.dirname(first(zip_fh.namelist())), 'data')
             file_path = os.path.join(data_path, 'samples.tsv')
             with zip_fh.open(file_path) as file:
-                self._check_manifest(file, 'entity:bundle_id', allow_multiple_uuid=False)
+                self._check_manifest(file, 'entity:bundle_id')
 
-    def _check_manifest(self, file: IO[bytes], uuid_field_name: str, allow_multiple_uuid: bool):
+    def _check_manifest(self, file: IO[bytes], uuid_field_name: str):
         text = TextIOWrapper(file)
         reader = csv.DictReader(text, delimiter='\t')
         rows = list(reader)
         logger.info(f'Manifest contains {len(rows)} rows.')
         self.assertGreater(len(rows), 0)
         self.assertIn(uuid_field_name, reader.fieldnames)
-        if allow_multiple_uuid:
-            bundle_uuids = rows[0][uuid_field_name].split(ManifestResponse.column_joiner)
-            self.assertGreater(len(bundle_uuids), 0)
-        else:
-            bundle_uuids = [rows[0][uuid_field_name]]
-        for bundle_uuid in bundle_uuids:
-            self.assertEqual(bundle_uuid, str(uuid.UUID(bundle_uuid)))
+        bundle_uuid = rows[0][uuid_field_name]
+        self.assertEqual(bundle_uuid, str(uuid.UUID(bundle_uuid)))
 
     def download_file_from_drs_response(self, response: bytes):
         json_data = json.loads(response)['data_object']