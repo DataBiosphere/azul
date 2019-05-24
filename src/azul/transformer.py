fe5c8126e94b393fd1894b43c2fc97c62af64a50
jesse <brennan@ucsc.edu>
2019-05-24 15:18:02 -0700

    Support duplicate notification (#947)
    Duplicate notifications are supported by changing how contributions are
    written to the index. First contributions are written with 'create'
    which will fail if these contributions already exist. They will be
    logged and then indexing tried again with update. This mean that the
    tally counts will now be accurate except in a few rare cases.
    
    If a notification has been indexed and aggregated and then a redundant
    notification is indexed, the tally can have a count of 0 (since we only
    update the tally count original contributions). For this reason I also
    added a test that ensures that aggregation happens if the tally is 0.

--- src/azul/transformer.py
+++ src/azul/transformer.py
@@ -97,7 +97,15 @@ class Document:
                    **cls._from_source(source))
         return self
 
-    def to_index(self, bulk=False) -> JSON:
+    def to_index(self, bulk=False, update=False) -> dict:
+        """
+        Build request parameters from the document for indexing
+
+        :param bulk: If bulk indexing
+        :param update: If creation failed and we need to try update. This will only affect docs with version_type None
+                       (AKA contributions)
+        :return: Request parameters for indexing
+        """
         delete = self.delete
         result = {
             '_index' if bulk else 'index': self.document_index,
@@ -106,8 +114,11 @@ class Document:
             '_id' if bulk else 'id': self.document_id
         }
         if self.version_type is None:
-            if bulk:
+            # TODO: FIXME: (jesse) why only for bulk???
+            if False and bulk:
                 result['_op_type'] = 'delete' if delete else 'index'
+            op_key = '_op_type' if bulk else 'op_type'
+            result[op_key] = 'delete' if delete else ('index' if update else 'create')
         else:
             if bulk:
                 result['_op_type'] = 'delete' if delete else ('create' if self.version is None else 'index')
