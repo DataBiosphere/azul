8fdaa603b1874c9e03a866dc1116b17cf90671fd
Hannes Schmidt <hannes@ucsc.edu>
2019-06-12 12:36:26 +0300

    REVERT ME: Avoid dots in entity ID column of Terra TSV (#1071)

--- src/azul/service/responseobjects/hca_response_v5.py
+++ src/azul/service/responseobjects/hca_response_v5.py
@@ -346,9 +346,9 @@ class ManifestResponse(AbstractResponse):
 
             # For each bundle containing the current file …
             for bundle in doc['bundles']:
-                bundle_fqid = bundle['uuid'], bundle['version']
+                bundle_fqid = bundle['uuid'], bundle['version'].replace('.', '_')
 
-                bundle_cells = {'entity:participant_id': '.'.join(bundle_fqid)}
+                bundle_cells = {'entity:participant_id': '_'.join(bundle_fqid)}
                 self._extract_fields([bundle], bundle_column_mapping, bundle_cells)
 
                 # Register the three extracted sets of fields as a group for this bundle and qualifier
