3b3e4c1dab402720c40b0f1a07b885d6832ff856
Abraham Chavez <achave11@ucsc.edu>
2019-07-04 01:30:03 -0700

    Remove excluded list and exclude whitelisted project (#1112)

--- src/azul/project/hca/__init__.py
+++ src/azul/project/hca/__init__.py
@@ -82,72 +82,7 @@ class Plugin(azul.plugin.Plugin):
                                             {
                                                 "terms": {
                                                     "files.project_json.provenance.document_id": [
-                                                        "1630e3dc-5501-4faf-9726-2e2c0b4da6d7",  # ???
-                                                        "fd1d163d-d6a7-41cd-b3bc-9d77ba9a36fe",  # peer
-                                                        "2a0faf83-e342-4b1c-bb9b-cf1d1147f3bb",  # treutlein
-                                                        "cf8439db-fcc9-44a8-b66f-8ffbf729bffa",  # meyer
-                                                        "6b9f514d-d738-403f-a9c2-62580bbe5c83",  # Q4_DEMO-…
-                                                        "311d013c-01e4-42c0-9c2d-25472afa9cbc",  # Q4_DEMO-…
-                                                        "d237ed6a-3a7f-4a91-b300-b070888a8542",  # DCP_Infrastructure_Test_
-                                                        "e6cc0b02-2125-4faa-9903-a9025a62efec",  # Q4_DEMO-…
-                                                        "e4dbcb98-0562-4071-8bea-5e8de5f3c147",  # Q4_DEMO-…
-                                                        "e79e9284-c337-4dfd-853d-66fa3facfbbd",  # 10x_prod_test_01_08_2019
-                                                        "560cd061-9165-4699-bc6e-8253e164c079",  # ss2_prod_test_01_08_2019
-                                                        "e83fda0e-6515-4f13-82cb-a5860ecfc2d4",  # prod/10x/2019-01-22T20:35:32Z
-                                                        "9a60e8c2-32ea-4586-bc1f-7ee58f462b07",  # prod/Smart-seq2/2019-01-22T20:35:33Z
-                                                        "71a6e049-4846-4c2a-8823-cc193c573efc",  # prod/Smart-seq2/2019-01-22T18:13:48Z
-                                                        "4b5a2268-507c-46e6-bab0-3efb30145e85",  # prod/10x/2019-01-22T18:44:02Z
-                                                        "364ebb73-652e-4d32-8938-1c922d0b2584",  # prod/Smart-seq2/2019-01-22T19:15:02Z
-                                                        "11f5d59b-0e2c-4f01-85ac-8d8dd3db53be",  # prod/Smart-seq2/2019-01-22T18:44:03Z
-                                                        "c1996526-6466-40ff-820f-dad4d63492ec",  # prod/10x/2019-01-29T22:06:07Z
-                                                        "c281dedc-e838-4464-bf51-1cc4efae3fb9",  # prod/10x/2019-01-29T23:14:07Z
-                                                        "40afcf6b-422a-47ba-ba7a-33678c949b5c",  # prod/10x/2019-01-30T05:22:07Z
-                                                        "71a6e049-4846-4c2a-8823-cc193c573efc",  # prod/Smart-seq2/2019-01-22T18:13:48Z
-                                                        "9a60e8c2-32ea-4586-bc1f-7ee58f462b07",  # prod/Smart-seq2/2019-01-22T20:35:33Z
-                                                        "0facfacd-5b0c-4228-8be5-37aa1f3a269d",  # prod/Smart-seq2/2019-01-29T22:06:07Z
-                                                        "76c209df-42bf-41dc-a5f5-3d27193ca7a6",  # prod/Smart-seq2/2019-01-29T23:14:07Z
-                                                        "bb409c34-bb87-4ed2-adaf-6d1ef10610b5",  # prod/Smart-seq2/2019-01-30T05:22:07Z
-                                                        "1a6b5e5d-914f-4dd6-8817-a1f9b7f364d5",  # prod/10x/2019-02-12T18:43:49Z
-                                                        "dd401943-1059-4b2d-b187-7a9e11822f95",  # prod/Smart-seq2/2019-02-12T18:43:49Z
-                                                        "209e6402-d854-49ea-815f-421dae5e3f4d",  # Tissue stability, https://github.com/HumanCellAtlas/data-store/issues/1976
-                                                        "6ac9d7b5-f86d-4c23-82a9-485a6642b278",  # Tissue Sensitivity, https://github.com/DataBiosphere/azul/issues/870
-                                                        "f396fa53-2a2d-4b8a-ad18-03bf4bd46833",  # Mouse Melanoma
-                                                        "f8880be0-210c-4aa3-9348-f5a423e07421",  # Old neuron_diff
-                                                        "0c7bbbce-3c70-4d6b-a443-1b92c1f205c8",  # BM_PC
-                                                        "e8642221-4c2c-4fd7-b926-a68bce363c88",  # Old pancreas6decades
-                                                        "5e6c0ede-5648-4b0e-83ce-4644c437b4c0",  # Old pancreas6decades
-                                                        "dadbdb31-5f69-485b-85f3-b244b74123f1",  # Old pancreas6decades
-                                                        "1a0f98b8-746a-489d-8af9-d5c657482aab",  # Old EMTAB5061
-                                                        "d96c2451-6e22-441f-a3e6-70fd0878bb1b",  # "cerebral organoid", superseded by 005d611a-14d5-4fbf-846e-571a1f874f70
-                                                        "5dfe932f-159d-4cab-8039-d32f22ffbbc2",  # Tissue Sensitivity "meyer", superseded by c4077b3c-5c98-4d26-a614-246d12c2e5d7
-                                                        "ccd8370a-84b8-464d-a87e-e688ac3e4f62",  # Mouse Melanoma, superseded by 8c3c290d-dfff-4553-8868-54ce45f4ba7f
-                                                        "34ec62a2-9643-430d-b41a-1e342bd615fc",  # kidney_biopsy_scRNA-seq "humphreys", superseded by 027c51c6-0719-469f-a7f5-640fe57cbece
-                                                        "c765e3f9-7cfc-4501-8832-79e5f7abd321",  # cardiomyocytes_basu "basu", superseded by a9c022b4-c771-4468-b769-cabcf9738de3
-                                                        "e1f2a0e4-1ec8-431e-a6df-c975b3a1131f",  # bone marrow "ido_amit", superseded by a29952d9-925e-40f4-8a1c-274f118f1f51
-                                                        "b6dc9b93-929a-45d0-beb2-5cf8e64872fe",  # neuron_diff, superseded by 2043c65a-1cf8-4828-a656-9e247d4e64f1
-                                                        "0ec2b05f-ddbe-4e5a-b30f-e81f4b1e330c",  # CD4+_lymphocytes "EGEOD106540", superseded by 90bd6933-40c0-48d4-8d76-778c103bf545
-                                                        "ff481f29-3d0b-4533-9de2-a760c61c162d",  # "10x-mouse-brain", superseded by 74b6d569-3b11-42ef-b6b1-a0454522b4a0
-                                                        "5f256182-5dfc-4070-8404-f6fa71d37c73",  # cell_hashing "rsatija", superseded by f81efc03-9f56-4354-aabb-6ce819c3d414
-                                                        "aabbec1a-1215-43e1-8e42-6489af25c12c",  # fetal-maternal interface, superseded by f83165c5-e2ea-4d15-a5cf-33f3550bffde
-                                                        "f7152c47-4ba7-4b29-9dbe-bf4dbc9faccb",  # prod/optimus/2019-05-18T12:01:24Z
-                                                        "ee777bd0-b70d-46ea-8488-3c635c0680a0",  # prod/optimus/2019-05-20T12:00:56Z
-                                                        "d90ca86f-6461-4db7-9e44-fd7c15f93947",  # prod/optimus/2019-05-19T12:01:25Z
-                                                        "4a6976fc-1add-4538-a410-4b8ecfd6d26d",  # prod/optimus/2019-05-28T19:09:31Z
-                                                        "ef2a1708-773c-4077-ae87-14c9c16fbdfe",  # prod/Smart-seq2/2019-05-18T12:01:25Z
-                                                        "6ccf02fe-7284-4e22-8f13-34cee293d003",  # prod/Smart-seq2/2019-05-19T12:01:25Z
-                                                        "24dce12d-c6d5-402d-b864-dbd4ac5bdf29",  # prod/Smart-seq2/2019-05-20T12:00:55Z
-                                                        "4129d036-67b9-4981-9d6d-3aaa235236d5",  # Another old neuron_diff
-                                                    ]
-                                                }
-                                            },
-                                            {
-                                                "terms": {
-                                                    "files.project_json.hca_ingest.document_id": [
-                                                        "bae45747-546a-4aed-9377-08e9115a8fb8",  # Q4_DEMO-…
-                                                        "7cb4940d-7c85-43d1-b2f5-1d99813e65df",  # Q4_DEMO-…
-                                                        "6ec8e247-2eb0-42d1-823f-75facd03988d",  # meyer
-                                                        "93f6a42f-1790-4af4-b5d1-8c436cb6feae",  # Teichmann-mouse-melanoma
-                                                        "6504d48c-1610-43aa-8cf8-214a960e110c",  # duplicate of Regev-ICA
+                                                        "179bf9e6-5b33-4c5b-ae26-96c7270976b8"  # 1m immune cells aka "Regev-ICA"
                                                     ]
                                                 }
                                             },
@@ -162,7 +97,6 @@ class Plugin(azul.plugin.Plugin):
                                                         {
                                                             "terms": {
                                                                 "files.project_json.provenance.document_id": [
-                                                                    "179bf9e6-5b33-4c5b-ae26-96c7270976b8",  # 1m immune cells aka "Regev-ICA"
                                                                     "29f53b7e-071b-44b5-998a-0ae70d0229a4",  # human hematopoietic profiling "peer"
                                                                 ]
                                                             }
