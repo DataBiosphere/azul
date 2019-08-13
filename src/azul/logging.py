3b9a500ca110da4530943e67b34def5fef94ac56
Hannes Schmidt <hannes@ucsc.edu>
2019-08-13 12:37:12 -0700

    Test logs list thread name and timestamps (#1222)

--- src/azul/logging.py
+++ src/azul/logging.py
@@ -11,12 +11,15 @@ def configure_app_logging(app: AzulChaliceApp, *loggers):
 
 def configure_script_logging(*loggers):
     assert len(logging.getLogger().handlers) == 0, 'Logging is already configured.'
-    logging.basicConfig(format="%(asctime)s %(levelname)-7s %(threadName)-7s: %(message)s")
-    _configure_log_levels(*loggers)
+    _configure_non_app_logging(loggers)
 
 
 def configure_test_logging(*loggers):
-    logging.basicConfig()
+    _configure_non_app_logging(loggers)
+
+
+def _configure_non_app_logging(loggers):
+    logging.basicConfig(format="%(asctime)s %(levelname)-7s %(threadName)-7s: %(message)s")
     _configure_log_levels(*loggers)
 
 
