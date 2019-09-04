58e671951c06bc9af8b09c9f52c1019d86a11d6a
Hannes Schmidt <hannes@ucsc.edu>
2019-09-03 21:04:29 -0700

    WIP

--- common.mk
+++ common.mk
@@ -16,22 +16,22 @@ ifneq ($(shell python -c "exec('try: import chalice\nexcept: print(False)\nelse:
 $(error Looks like some or all requirements is missing. Please run 'pip install -r requirements.dev.txt')
 endif
 
-ifneq ($(shell python -c "from chalice import chalice_version as v; \
-                          from pkg_resources import parse_version as p; \
-                          print(p(v) >= p('1.6.0'))"),True)
-$(error Looks like chalice is out of date. Please run 'pip install -Ur requirements.dev.txt')
-endif
+# ifneq ($(shell python -c "from chalice import chalice_version as v; \
+#                           from pkg_resources import parse_version as p; \
+#                           print(p(v) >= p('1.6.0'))"),True)
+# $(error Looks like chalice is out of date. Please run 'pip install -Ur requirements.dev.txt')
+# endif
 
 ifeq ($(shell which terraform),)
 $(warning Looks like TerraForm is not installed. This is ok as long as you're not trying to create a new deployment. \
           Deploying new lambdas is still possible with `make deploy` but `make terraform` will not work.)
 endif
 
-ifneq ($(shell python -c "import wheel as w; \
-                          from pkg_resources import parse_version as p; \
-                          print(p(w.__version__) >= p('0.32.3'))"),True)
-$(error Looks like the `wheel` package is outdated or missing. See README for instructions on how to fix this.)
-endif
+# ifneq ($(shell python -c "import wheel as w; \
+#                           from pkg_resources import parse_version as p; \
+#                           print(p(w.__version__) >= p('0.32.3'))"),True)
+# $(error Looks like the `wheel` package is outdated or missing. See README for instructions on how to fix this.)
+# endif
 
 check_branch:
 	python $(azul_home)/scripts/check_branch.py
