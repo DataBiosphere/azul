2f27fdabc9769779987ef9247eb6f8a2c0ffa679
jesse <brennan@ucsc.edu>
2019-09-18 20:04:19 -0700

    Ensure tearDown runs despite exceptions (#1298)

--- test/azul_test_case.py
+++ test/azul_test_case.py
@@ -3,6 +3,47 @@ from unittest import TestCase
 import boto3.session
 
 
+class AlwaysTearDownTestCase(TestCase):
+    """
+    AlwaysTearDownTestCase makes sure that tearDown / cleanup methods are always
+    run when they should be.
+
+    This means that
+
+    - if a KeyboardInterrupt is raised in a test, then tearDown, tearDownClass,
+      and tearDownModule will all still run
+    - if any exception is raised in a setUp, then tearDown, tearDownClass,
+      and tearDownModule will all still run
+
+    Caveats:
+
+    - All tearDown methods should pass even if their corresponding setUps don't
+      run at all, as in the case of a KeyboardInterrupt or other exception.
+    - If an exception is raised in setUpClass or setUpModule, the corresponding
+      tearDown will not be run.
+    """
+    def __init__(self, *args, **kwargs):
+        super().__init__(* args, **kwargs)
+        self.setUp = self._cleanup_wrapper(self.setUp, BaseException)
+
+    def run(self, result=None):
+        test_method = getattr(self, self._testMethodName)
+        wrapped_test = self._cleanup_wrapper(test_method, KeyboardInterrupt)
+        setattr(self, self._testMethodName, wrapped_test)
+        return super().run(result)
+
+    def _cleanup_wrapper(self, method, exception_cls):
+        def wrapped(*args, **kwargs):
+            try:
+                return method(*args, **kwargs)
+            except exception_cls:
+                self.tearDown()
+                self.doCleanups()
+                raise
+
+        return wrapped
+
+
 class AzulTestCase(TestCase):
 
     @classmethod
@@ -19,3 +60,88 @@ class AzulTestCase(TestCase):
     def tearDownClass(cls) -> None:
         boto3.session.Session.get_credentials = cls.get_credentials_orig
         super().tearDownClass()
+
+
+class Hidden:
+    # Keep this test case out of the main namespace
+
+    class TracingTestCase:
+        """A test case which traces its calls."""
+
+        def __init__(self, events):
+            super().__init__('test')
+            self.events = events
+
+        def setUp(self):
+            self.events.append('setUp')
+
+        def test(self):
+            self.events.append('test')
+
+        def tearDown(self):
+            self.events.append('tearDown')
+
+
+class TestAlwaysTearDownTestCase(TestCase):
+
+    def test_regular_execution_order(self):
+        expected = ['setUp', 'test', 'tearDown', 'setUp', 'test', 'tearDown']
+        for test_class, expected_events in [(TestCase, expected),
+                                            (AlwaysTearDownTestCase, expected)]:
+            with self.subTest(test_class=test_class, expected_events=expected_events):
+                events = []
+
+                class TC(Hidden.TracingTestCase, test_class):
+                    pass
+
+                TC(events).run()
+                TC(events).run()
+                self.assertEqual(events, expected_events)
+
+    def test_keyboard_interrupt_in_test(self):
+        expected = ['setUp', 'test']
+        for test_class, expected_events in [(TestCase, expected),
+                                            (AlwaysTearDownTestCase, expected + ['tearDown'])]:
+            with self.subTest(test_class=test_class, expected_events=expected_events):
+                events = []
+
+                class TC(Hidden.TracingTestCase, test_class):
+                    def test(self):
+                        super().test()
+                        raise KeyboardInterrupt()
+
+                with self.assertRaises(KeyboardInterrupt):
+                    TC(events).run()
+                self.assertEqual(events, expected_events)
+                self.assertEqual(events, expected_events)
+
+    def test_exception_in_setup(self):
+        expected = ['setUp']
+        for test_class, expected_events in [(TestCase, expected),
+                                            (AlwaysTearDownTestCase, expected + ['tearDown'])]:
+            with self.subTest(test_class=test_class, expected_events=expected_events):
+                events = []
+
+                class TC(Hidden.TracingTestCase, test_class):
+                    def setUp(self):
+                        super().setUp()
+                        raise RuntimeError('Exception in setUp')
+
+                TC(events).run()
+                self.assertEqual(events, expected_events)
+
+    def test_keyboard_interrupt_in_setup(self):
+        expected = ['setUp']
+        for test_class, expected_events in [(TestCase, expected),
+                                            (AlwaysTearDownTestCase, expected + ['tearDown'])]:
+            with self.subTest(test_class=test_class, expected_events=expected_events):
+                events = []
+
+                class TC(Hidden.TracingTestCase, test_class):
+                    def setUp(self):
+                        super().setUp()
+                        raise KeyboardInterrupt()
+
+                with self.assertRaises(KeyboardInterrupt):
+                    TC(events).run()
+                self.assertEqual(events, expected_events)
