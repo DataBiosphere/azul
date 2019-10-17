f50fe6fcfbb846ae7f6f886145868e2bbb8aea72
Hannes Schmidt <hannes@ucsc.edu>
2019-10-16 20:36:56 -0700

    Upgrade to Gitlab 12.3.5

--- terraform/gitlab/gitlab.tf.json.template.py
+++ terraform/gitlab/gitlab.tf.json.template.py
@@ -880,7 +880,7 @@ emit_tf({} if config.terraform_component != 'gitlab' else {
                                --volume /mnt/gitlab/config:/etc/gitlab \
                                --volume /mnt/gitlab/logs:/var/log/gitlab \
                                --volume /mnt/gitlab/data:/var/opt/gitlab \
-                               gitlab/gitlab-ce:11.8.3-ce.0
+                               gitlab/gitlab-ce:12.3.5-ce.0
                         docker run \
                                --detach \
                                --name gitlab-runner \
@@ -888,7 +888,7 @@ emit_tf({} if config.terraform_component != 'gitlab' else {
                                --volume /mnt/gitlab/runner/config:/etc/gitlab-runner \
                                --network gitlab-runner-net \
                                --env DOCKER_HOST=tcp://gitlab-dind:2375 \
-                               gitlab/gitlab-runner:v11.8.0
+                               gitlab/gitlab-runner:v12.3.0
                     """[1:]),  # trim newline char at the beginning as dedent() only removes indent common to all lines
                 "tags": {
                     "Name": "azul-gitlab"
