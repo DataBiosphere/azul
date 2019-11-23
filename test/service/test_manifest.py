d14378251f2c9e88e56f70d346edc7600e719558
Hannes Schmidt <hannes@ucsc.edu>
2019-11-23 00:25:27 -0800

    Consolidate AsyncManifestService tests in one module

--- test/service/test_manifest.py
+++ test/service/test_manifest.py
@@ -18,7 +18,6 @@ from urllib.parse import (
 )
 from zipfile import ZipFile
 
-from botocore.exceptions import ClientError
 from more_itertools import (
     first,
     one,
@@ -29,7 +28,6 @@ from moto import (
 )
 import requests
 
-from app_test_case import LocalAppTestCase
 from azul import config
 from azul.json_freeze import freeze
 from azul.logging import configure_test_logging
@@ -37,7 +35,6 @@ from azul.service import (
     hca_response_v5,
 )
 from azul.service.elasticsearch_service import ElasticsearchService
-from azul.service.step_function_helper import StateMachineError
 from azul.service.storage_service import StorageService
 from azul.types import JSON
 from azul_test_case import AzulTestCase
@@ -50,128 +47,6 @@ logger = logging.getLogger(__name__)
 # noinspection PyPep8Naming
 def setUpModule():
     configure_test_logging(logger)
-
-
-class TestAsyncManifestService(LocalAppTestCase):
-
-    @classmethod
-    def lambda_name(cls) -> str:
-        return 'service'
-
-    patch_step_function_helper = mock.patch(
-        'azul.service.async_manifest_service.AsyncManifestService.step_function_helper')
-
-    patch_current_request = mock.patch('lambdas.service.app.app.current_request')
-
-    @mock_sts
-    @patch_step_function_helper
-    @mock.patch('uuid.uuid4')
-    def test_manifest_endpoint_start_execution(self, mock_uuid, step_function_helper):
-        """
-        Calling start manifest generation without a token should start an
-        execution and return a response with Retry-After and Location in the
-        headers.
-        """
-        with ResponsesHelper() as helper:
-            helper.add_passthru(self.base_url)
-            for fetch in True, False:
-                with self.subTest(fetch=fetch):
-                    execution_name = '6c9dfa3f-e92e-11e8-9764-ada973595c11'
-                    mock_uuid.return_value = execution_name
-                    step_function_helper.describe_execution.return_value = {'status': 'RUNNING'}
-                    format_ = 'compact'
-                    filters = {'file': {'organ': {'is': ['lymph node']}}}
-                    params = {'filters': json.dumps(filters), 'format': format_}
-                    if fetch:
-                        response = requests.get(self.base_url + '/fetch/manifest/files',
-                                                params=params)
-                        response.raise_for_status()
-                        response = response.json()
-                    else:
-                        response = requests.get(self.base_url + '/manifest/files',
-                                                params=params,
-                                                allow_redirects=False)
-                    self.assertEqual(301, response['Status'] if fetch else response.status_code)
-                    self.assertIn('Retry-After', response if fetch else response.headers)
-                    self.assertIn('Location', response if fetch else response.headers)
-                    step_function_helper.start_execution.assert_called_once_with(config.manifest_state_machine_name,
-                                                                                 execution_name,
-                                                                                 execution_input=dict(format=format_,
-                                                                                                      filters=filters))
-                    step_function_helper.describe_execution.assert_called_once()
-                    step_function_helper.reset_mock()
-
-    @patch_step_function_helper
-    def test_manifest_endpoint_check_status(self, step_function_helper):
-        """
-        Calling start manifest generation with a token should check the status
-        without starting an execution.
-        """
-        params = {
-            'token': 'eyJleGVjdXRpb25faWQiOiAiN2M4OGNjMjktOTFjNi00NzEyLTg4MGYtZTQ3ODNlMmE0ZDllIn0='
-        }
-        step_function_helper.describe_execution.return_value = {'status': 'RUNNING'}
-        response = requests.get(self.base_url + '/fetch/manifest/files', params=params)
-        response.raise_for_status()
-        step_function_helper.start_execution.assert_not_called()
-        step_function_helper.describe_execution.assert_called_once()
-
-    @patch_step_function_helper
-    def test_manifest_endpoint_execution_not_found(self, step_function_helper):
-        """
-        Manifest status check should raise a BadRequestError (400 status code)
-        if execution cannot be found.
-        """
-        params = {
-            'token': 'eyJleGVjdXRpb25faWQiOiAiN2M4OGNjMjktOTFjNi00NzEyLTg4MGYtZTQ3ODNlMmE0ZDllIn0='
-        }
-        step_function_helper.describe_execution.side_effect = ClientError({
-            'Error': {
-                'Code': 'ExecutionDoesNotExist'
-            }
-        }, '')
-        response = requests.get(self.base_url + '/fetch/manifest/files', params=params)
-        self.assertEqual(response.status_code, 400)
-
-    @patch_step_function_helper
-    @patch_current_request
-    def test_manifest_endpoint_boto_error(self, _current_request, step_function_helper):
-        """
-        Manifest status check should reraise any ClientError that is not caused by ExecutionDoesNotExist
-        """
-        params = {
-            'token': 'eyJleGVjdXRpb25faWQiOiAiN2M4OGNjMjktOTFjNi00NzEyLTg4MGYtZTQ3ODNlMmE0ZDllIn0='
-        }
-        step_function_helper.describe_execution.side_effect = ClientError({
-            'Error': {
-                'Code': 'OtherError'
-            }
-        }, '')
-        response = requests.get(self.base_url + '/fetch/manifest/files', params=params)
-        self.assertEqual(response.status_code, 500)
-
-    @patch_step_function_helper
-    @patch_current_request
-    def test_manifest_endpoint_execution_error(self, _current_request, step_function_helper):
-        """
-        Manifest status check should return a generic error (500 status code)
-        if the execution errored.
-        """
-        params = {
-            'token': 'eyJleGVjdXRpb25faWQiOiAiN2M4OGNjMjktOTFjNi00NzEyLTg4MGYtZTQ3ODNlMmE0ZDllIn0='
-        }
-        step_function_helper.get_manifest_status.side_effect = StateMachineError
-        response = requests.get(self.base_url + '/fetch/manifest/files', params=params)
-        self.assertEqual(response.status_code, 500)
-
-    @patch_current_request
-    def test_manifest_endpoint_invalid_token(self, _current_request):
-        """
-        Manifest endpoint should raise a BadRequestError when given a token that cannot be decoded
-        """
-        params = {'token': 'Invalid base64'}
-        response = requests.get(self.base_url + '/fetch/manifest/files', params=params)
-        self.assertEqual(response.status_code, 400)
 
 
 class TestManifestEndpoints(WebServiceTestCase):
