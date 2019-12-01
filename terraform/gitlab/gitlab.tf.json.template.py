527b78dd8af80a431558641f78cbe671a0f564a9
Hannes Schmidt <hannes@ucsc.edu>
2019-11-30 20:34:43 -0800

    Update Gitlab to security release 12.3.8
    https://about.gitlab.com/blog/2019/11/27/security-release-gitlab-12-5-2-released/

--- terraform/gitlab/gitlab.tf.json.template.py
+++ terraform/gitlab/gitlab.tf.json.template.py
@@ -881,7 +881,7 @@ emit_tf({} if config.terraform_component != 'gitlab' else {
                                --volume /mnt/gitlab/config:/etc/gitlab \
                                --volume /mnt/gitlab/logs:/var/log/gitlab \
                                --volume /mnt/gitlab/data:/var/opt/gitlab \
-                               gitlab/gitlab-ce:12.3.5-ce.0
+                               gitlab/gitlab-ce:12.3.8-ce.0
                         docker run \
                                --detach \
                                --name gitlab-runner \
