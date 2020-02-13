DataBiosphere/azul

commit 4e9abbc1425b30d91d664902fd478709f7ced9d0

Update check_branch.py to handle `hca/*` branches (#1509)
This also will ensure that a branch is not deployed unless the expected
corresponding AWS account is selected / active.

src/azul/__init__.py
@@ -370,10 +370,11 @@ class Config:
         return os.environ['AZUL_DOMAIN_NAME']
 
     main_deployments_by_branch = {
-        'develop': 'dev',
-        'integration': 'integration',
-        'staging': 'staging',
-        'prod': 'prod'
+        'develop': (122796619775, 'dev'),
+        'hca/develop': (861229788715, 'dev'),
+        'hca/integration': (861229788715, 'integration'),
+        'hca/staging': (861229788715, 'staging'),
+        'hca/prod': (109067257620, 'prod')
     }
 
     @property

.gitallowed
@@ -8,3 +8,6 @@
 # This overrides one of the default patterns used by the AWS provider for
 # `git secrets. We don't mind exposing account IDs.
 AWS_ACCOUNT_ID=861229788715
+
+# This is the UCSC AWS Gateway account ID
+122796619775

scripts/check_branch.py
@@ -4,40 +4,57 @@ import sys
 import git
 
 from azul import config
+import azul.deployment
 
 """
-Ensure that the currently checked out branch matches the selected deployemnt
+Ensure that the currently checked out branch matches the selected deployment
 """
 
 
 def check_branch(branch, stage):
     """
-    >>> check_branch('dev', 'develop')
+    >>> from unittest.mock import patch
 
-    >>> check_branch('feature/foo', 'prod')
+    >>> with patch.object(azul.deployment, 'aws') as aws:
+    ...     aws.account = 123
+    ...     check_branch('develop', 'dev')
     Traceback (most recent call last):
     ...
-    RuntimeError: Non-protected branch 'feature/foo' can't be deployed to main deployment 'prod'
+    RuntimeError: Protected branch 'develop' should be deployed to AWS account '122796619775', not '123'
 
-    >>> check_branch('staging', 'hannes')
+    >>> with patch.object(azul.deployment, 'aws') as aws:
+    ...     aws.account = 122796619775
+    ...     check_branch('develop', 'dev')
+
+    >>> check_branch('issues/foo', 'prod')
     Traceback (most recent call last):
     ...
-    RuntimeError: Protected branch 'staging' should be deployed to 'staging', not 'hannes'
+    RuntimeError: Non-protected branch 'issues/foo' can't be deployed to main deployment 'prod'
 
-    >>> check_branch('staging', 'integration')
+    >>> check_branch('hca/staging', 'hannes.local')
     Traceback (most recent call last):
     ...
-    RuntimeError: Protected branch 'staging' should be deployed to 'staging', not 'integration'
+    RuntimeError: Protected branch 'hca/staging' should be deployed to 'staging', not 'hannes.local'
+
+    >>> check_branch('hca/staging', 'hca/integration')
+    Traceback (most recent call last):
+    ...
+    RuntimeError: Protected branch 'hca/staging' should be deployed to 'staging', not 'hca/integration'
     """
     stage_by_branch = config.main_deployments_by_branch
+    account = azul.deployment.aws.account
     try:
-        expected_stage = stage_by_branch[branch]
+        expected_account, expected_stage = stage_by_branch[branch]
     except KeyError:
-        if stage in stage_by_branch.values():
+        if stage in [s for _, s in stage_by_branch.values()]:
             raise RuntimeError(f"Non-protected branch '{branch}' can't be deployed to main deployment '{stage}'")
     else:
         if stage != expected_stage:
             raise RuntimeError(f"Protected branch '{branch}' should be deployed to '{expected_stage}', not '{stage}'")
+        elif account != expected_account:
+            raise RuntimeError(
+                f"Protected branch '{branch}' should be deployed to AWS account '{expected_account}', not '{account}'"
+            )
 
 
 def expected_stage(branch):

commit 64fc13370078fe9fde24eeec0abe4227307d9160

Fix: main deployments aren't detected correctly
Regression from 4e9abbc1425b30d91d664902fd478709f7ced9d0

src/azul/__init__.py
@@ -365,7 +365,9 @@ class Config:
 
     @property
     def is_main_deployment(self):
-        return self.deployment_stage in self.main_deployments_by_branch.values()
+        current_deployment = self.deployment_stage
+        return any(current_deployment == main_deployment
+                   for account, main_deployment in self.main_deployments_by_branch.values())
 
     @property
     def _git_status(self) -> Mapping[str, str]: