f84d7e4d40615ea9b420a2db91301d21910dbaaa
jesse <brennan@ucsc.edu>
2020-03-12 10:31:59 -0700

    [2/2] Use Terraform for deploying lambdas (#556)
    Make the actual changes for using Terraform for
    deploying the lambdas.

--- terraform/common.tf.json.template.py
+++ terraform/common.tf.json.template.py
@@ -34,5 +34,5 @@ emit_tf({
         "account_id": "${data.aws_caller_identity.current.account_id}",
         "region": "${data.aws_region.current.name}",
         "google_project": "${data.google_client_config.current.project}" if config.enable_gcp() else None
-    }
+    },
 })
