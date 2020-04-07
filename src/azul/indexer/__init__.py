551e81472d69f24358e25ac53be3b044701ea83c
Daniel Sotirhos <dsotirho@ucsc.edu>
2020-04-07 09:07:57 -0700

    [1/2] Reduce noisy indexer aggregation logs (#1670)

--- src/azul/indexer/__init__.py
+++ src/azul/indexer/__init__.py
@@ -303,14 +303,15 @@ class BaseIndexer(ABC):
             log.info('Reading %i expected contribution(s) using scan().', num_contributions)
             hits = scan(es_client, index=index, query=query, size=page_size, doc_type=Document.type)
         contributions = [Contribution.from_index(self.field_types(), hit) for hit in hits]
-
-        entity_key = attrgetter('entity')
-        log.info('Read %i contribution(s). Breakdown by entity: %s',
-                 len(contributions),
-                 {
-                     f'{entity.entity_type}/{entity.entity_id}': sum(1 for _ in contribution_group)
-                     for entity, contribution_group in groupby(sorted(contributions, key=entity_key), key=entity_key)
-                 })
+        log.info('Read %i contribution(s). ', len(contributions))
+        if log.isEnabledFor(logging.DEBUG):
+            entity_key = attrgetter('entity')
+            log.debug('Contribution entity counts: %s',
+                      {
+                          f'{entity.entity_type}/{entity.entity_id}': sum(1 for _ in contribution_group)
+                          for entity, contribution_group in
+                          groupby(sorted(contributions, key=entity_key), key=entity_key)
+                      })
         return contributions
 
     def _aggregate(self, contributions: List[Contribution]) -> List[Aggregate]:
@@ -334,6 +335,14 @@ class BaseIndexer(ABC):
                     assert entity == contribution.entity
                     contributions_by_entity[entity].append(contribution)
                     break
+        log.info('Selected %i contribution(s) to be aggregated.',
+                 sum(len(contributions) for contributions in contributions_by_entity.values()))
+        if log.isEnabledFor(logging.DEBUG):
+            log.debug('Contribution entity counts: %s',
+                      {
+                          f'{entity.entity_type}/{entity.entity_id}': len(contributions)
+                          for entity, contributions in sorted(contributions_by_entity.items())
+                      })
 
         # Create lookup for transformer by entity type
         transformers = {t.entity_type(): t for t in self.transformers() if isinstance(t, AggregatingTransformer)}
@@ -341,8 +350,6 @@ class BaseIndexer(ABC):
         # Aggregate contributions for the same entity
         aggregates = []
         for entity, contributions in contributions_by_entity.items():
-            log.info('Selected %i contributions to be aggregated for entity %s/%s.',
-                     len(contributions), entity.entity_type, entity.entity_id)
             transformer = transformers[entity.entity_type]
             contents = transformer.aggregate(contributions)
             bundles = [dict(uuid=c.bundle_uuid, version=c.bundle_version) for c in contributions]
