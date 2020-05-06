219e519330af48edf1237a104fdb628fc67de663
Noah Dove <nadove@ucsc.edu>
2020-05-06 03:17:54 -0700

    Register uncovered doctests, remove modules lacking doctests

--- test/test_doctests.py
+++ test/test_doctests.py
@@ -4,6 +4,7 @@ import unittest
 import azul
 import azul.azulclient
 import azul.collections
+import azul.doctests
 import azul.dss
 import azul.files
 import azul.json
@@ -13,6 +14,7 @@ from azul.modules import (
     load_app_module,
     load_module,
 )
+import azul.objects
 import azul.openapi
 import azul.openapi.schema
 import azul.openapi.params
@@ -35,10 +37,12 @@ def setupModule():
 def load_tests(_loader, tests, _ignore):
     tests.addTests(doctest.DocTestSuite(azul))
     tests.addTests(doctest.DocTestSuite(azul.collections))
+    tests.addTests(doctest.DocTestSuite(azul.doctests))
     tests.addTests(doctest.DocTestSuite(azul.dss))
     tests.addTests(doctest.DocTestSuite(azul.files))
     tests.addTests(doctest.DocTestSuite(azul.json))
     tests.addTests(doctest.DocTestSuite(azul.json_freeze))
+    tests.addTests(doctest.DocTestSuite(azul.objects))
     tests.addTests(doctest.DocTestSuite(azul.openapi))
     tests.addTests(doctest.DocTestSuite(azul.openapi.schema))
     tests.addTests(doctest.DocTestSuite(azul.openapi.params))
@@ -49,14 +53,13 @@ def load_tests(_loader, tests, _ignore):
     tests.addTests(doctest.DocTestSuite(azul.transformer))
     tests.addTests(doctest.DocTestSuite(azul.vendored.frozendict))
     tests.addTests(doctest.DocTestSuite(azul.azulclient))
-    tests.addTests(doctest.DocTestSuite(azul.service.elasticsearch_service))
     tests.addTests(doctest.DocTestSuite(retorts))
     tests.addTests(doctest.DocTestSuite(azul.project.hca.metadata_generator))
     tests.addTests(doctest.DocTestSuite(load_app_module('service')))
     root = azul.config.project_root
     tests.addTests(doctest.DocTestSuite(load_module(root + '/scripts/envhook.py', 'envhook')))
+    tests.addTests(doctest.DocTestSuite(load_module(root + '/scripts/export_environment.py', 'export_environment'))),
     tests.addTests(doctest.DocTestSuite(load_module(root + '/scripts/check_branch.py', 'check_branch')))
-    tests.addTests(doctest.DocTestSuite(load_module(root + '/scripts/copy_bundles.py', 'copy_bundles')))
     return tests
 
 
