bdca16eab6189b67fdb110821928e8eefc474569
Hannes Schmidt <hannes@ucsc.edu>
2020-05-13 15:25:24 -0700

    [4/4] Separate modules for transformation and aggregation
    Move aggregation code from azul.plugins.metadata.hca.transform to .aggregate

--- src/azul/plugins/metadata/hca/transform.py
+++ src/azul/plugins/metadata/hca/transform.py
@@ -8,9 +8,7 @@ from collections import (
     defaultdict,
 )
 import logging
-from operator import itemgetter
 from typing import (
-    Any,
     Iterable,
     List,
     Mapping,
@@ -28,30 +26,29 @@ from azul import (
     require,
 )
 from azul.collections import (
-    compose_keys,
     none_safe_key,
-    none_safe_tuple_key,
 )
 from azul.indexer.aggregate import (
-    Accumulator,
-    DistinctAccumulator,
-    FrequencySetAccumulator,
-    GroupingAggregator,
-    ListAccumulator,
-    SetAccumulator,
-    SetOfDictAccumulator,
     SimpleAggregator,
-    SingleValueAccumulator,
-    SumAccumulator,
-    UniqueValueCountAccumulator,
 )
-from azul.indexer.transform import Transformer
 from azul.indexer.document import (
     BundleUUID,
     BundleVersion,
     Contribution,
     EntityReference,
     FieldTypes,
+)
+from azul.indexer.transform import Transformer
+from azul.plugins.metadata.hca.aggregate import (
+    CellLineAggregator,
+    CellSuspensionAggregator,
+    DonorOrganismAggregator,
+    FileAggregator,
+    OrganoidAggregator,
+    ProjectAggregator,
+    ProtocolAggregator,
+    SampleAggregator,
+    SpecimenAggregator,
 )
 from azul.plugins.metadata.hca.metadata_generator import MetadataGenerator
 from azul.types import JSON
@@ -744,111 +741,3 @@ class BundleTransformer(BundleProjectTransformer):
             **super().field_types(),
             'metadata': None  # Exclude fields that came from MetadataGenerator() from translation
         }
-
-
-class FileAggregator(GroupingAggregator):
-
-    def _transform_entity(self, entity: JSON) -> JSON:
-        return dict(size=((entity['uuid'], entity['version']), entity['size']),
-                    file_format=entity['file_format'],
-                    count=((entity['uuid'], entity['version']), 1),
-                    content_description=entity['content_description'])
-
-    def _group_keys(self, entity) -> Iterable[Any]:
-        return entity['file_format']
-
-    def _get_accumulator(self, field) -> Optional[Accumulator]:
-        if field == 'file_format':
-            return SingleValueAccumulator()
-        elif field == 'content_description':
-            return SetAccumulator(max_size=100)
-        elif field in ('size', 'count'):
-            return DistinctAccumulator(SumAccumulator(0))
-        else:
-            return None
-
-
-class SampleAggregator(SimpleAggregator):
-
-    def _get_accumulator(self, field) -> Optional[Accumulator]:
-        return SetAccumulator(max_size=100)
-
-
-class SpecimenAggregator(SimpleAggregator):
-
-    def _get_accumulator(self, field) -> Optional[Accumulator]:
-        return SetAccumulator(max_size=100)
-
-
-class CellSuspensionAggregator(GroupingAggregator):
-
-    def _transform_entity(self, entity: JSON) -> JSON:
-        return {
-            **entity,
-            'total_estimated_cells': (entity['document_id'], entity['total_estimated_cells']),
-        }
-
-    def _group_keys(self, entity) -> Iterable[Any]:
-        return entity['organ']
-
-    def _get_accumulator(self, field) -> Optional[Accumulator]:
-        if field == 'total_estimated_cells':
-            return DistinctAccumulator(SumAccumulator(0))
-        else:
-            return SetAccumulator(max_size=100)
-
-
-class CellLineAggregator(SimpleAggregator):
-
-    def _get_accumulator(self, field) -> Optional[Accumulator]:
-        return SetAccumulator(max_size=100)
-
-
-class DonorOrganismAggregator(SimpleAggregator):
-
-    def _transform_entity(self, entity: JSON) -> JSON:
-        return {
-            **entity,
-            'donor_count': entity['biomaterial_id']
-        }
-
-    def _get_accumulator(self, field) -> Optional[Accumulator]:
-        if field == 'organism_age_range':
-            return SetOfDictAccumulator(max_size=100,
-                                        key=compose_keys(none_safe_tuple_key(none_last=True), itemgetter('lte', 'gte')))
-        elif field == 'donor_count':
-            return UniqueValueCountAccumulator()
-        else:
-            return SetAccumulator(max_size=100)
-
-
-class OrganoidAggregator(SimpleAggregator):
-
-    def _get_accumulator(self, field) -> Optional[Accumulator]:
-        return SetAccumulator(max_size=100)
-
-
-class ProjectAggregator(SimpleAggregator):
-
-    def _get_accumulator(self, field) -> Optional[Accumulator]:
-        if field == 'document_id':
-            return ListAccumulator(max_size=100)
-        elif field in ('project_description',
-                       'contact_names',
-                       'contributors',
-                       'publication_titles',
-                       'publications'):
-            return None
-        else:
-            return SetAccumulator(max_size=100)
-
-
-class ProtocolAggregator(SimpleAggregator):
-
-    def _get_accumulator(self, field) -> Optional[Accumulator]:
-        if field == 'document_id':
-            return None
-        elif field == 'assay_type':
-            return FrequencySetAccumulator(max_size=100)
-        else:
-            return SetAccumulator()
