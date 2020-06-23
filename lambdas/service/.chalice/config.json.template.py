ff6541cdecd2f0a5fd73ea7020d6e29223303909
jesse <brennan@ucsc.edu>
2020-06-22 17:43:59 -0700

    Use Terraform reference to set service lambda role ARN (#1911, #1775)

--- lambdas/service/.chalice/config.json.template.py
+++ lambdas/service/.chalice/config.json.template.py
@@ -1,5 +1,4 @@
 from azul import config
-from azul.deployment import aws
 from azul.template import emit
 
 suffix = '-' + config.deployment_stage
@@ -10,7 +9,7 @@ emit({
     "app_name": config.service_name[:-len(suffix)],  # Chalice appends stage name implicitly
     "api_gateway_stage": config.deployment_stage,
     "manage_iam_role": False,
-    "iam_role_arn": f"arn:aws:iam::{aws.account}:role/{config.service_name}",
+    "iam_role_arn": "${var.role_arn}",
     "environment_variables": config.lambda_env,
     "lambda_timeout": config.api_gateway_timeout + config.api_gateway_timeout_padding,
     "lambda_memory_size": 1024,
