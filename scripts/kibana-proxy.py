9132abcdd7004d38466135c522bd0e2c0aac7d6e
Natan Lao <natanlao@users.noreply.github.com>
2020-08-28 16:15:05 -0700

    [1/2] Use Docker API bindings in kibana_proxy.py (#1891)

--- scripts/kibana-proxy.py
+++ scripts/kibana-proxy.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-
 """
 Runs Kibana and aws-signing-proxy locally. The latter is used to sign requests by the former and forward them to an
 Amazon Elasticsearch instance. The default instance is the main ES instance for the current DSS stage.
@@ -21,21 +20,13 @@ Then run
 and open `http://localhost:5601` in your browser while leaving the script running. Hitting Ctrl-C terminates it and
 its child processes.
 """
-
-from itertools import (
-    chain,
-)
 import logging
 import os
-import shlex
-import signal
 import sys
 import time
 
 import boto3
-from more_itertools import (
-    flatten,
-)
+import docker
 
 from azul.logging import (
     configure_script_logging,
@@ -48,7 +39,7 @@ class KibanaProxy:
 
     def __init__(self, options) -> None:
         self.options = options
-        self.pids = {}
+        self.containers = []
 
     def run(self):
         # aws-signing-proxy doesn't support credentials
@@ -56,38 +47,51 @@ class KibanaProxy:
         kibana_port = self.options.kibana_port
         cerebro_port = self.options.cerebro_port or kibana_port + 1
         proxy_port = self.options.proxy_port or kibana_port + 2
-        container_name = f'aws-signing-proxy-{kibana_port}'
+        client = docker.from_env()
         try:
-            self.spawn('docker', 'run', '--rm', '-t',
-                       '--name', container_name,
-                       *flatten(
-                           ('-p', f'127.0.0.1:{port}:{port}')
-                           for port in (kibana_port, cerebro_port, proxy_port)
-                       ),
-                       '-e', f'AWS_ACCESS_KEY_ID={creds.access_key}',
-                       '-e', f'AWS_SECRET_ACCESS_KEY={creds.secret_key}',
-                       '-e', f'AWS_SESSION_TOKEN={creds.token}',
-                       '-e', 'AWS_REGION',
-                       'cllunsford/aws-signing-proxy',
-                       '-target', self.dss_end_point,
-                       '-port', str(proxy_port),
-                       AWS_REGION=os.environ['AWS_DEFAULT_REGION'])
+            proxy = client.containers.run('cllunsford/aws-signing-proxy',
+                                          auto_remove=True,
+                                          command=['-target', self.dss_end_point, '-port', str(proxy_port)],
+                                          detach=True,
+                                          environment={
+                                              'AWS_ACCESS_KEY_ID': creds.access_key,
+                                              'AWS_SECRET_ACCESS_KEY': creds.secret_key,
+                                              'AWS_SESSION_TOKEN': creds.token,
+                                              'AWS_REGION': os.environ['AWS_DEFAULT_REGION']
+                                          },
+                                          ports={port: port for port in (kibana_port, cerebro_port, proxy_port)},
+                                          tty=True)
+            self.containers.append(proxy)
             time.sleep(3)
-            self.spawn('docker', 'run', '--rm', '-t',
-                       '--network', f'container:{container_name}',
-                       '-e', f'ELASTICSEARCH_HOSTS=http://localhost:{proxy_port}',
-                       '-e', f'SERVER_PORT={kibana_port}',
-                       'docker.elastic.co/kibana/kibana-oss:6.8.0')
-            self.spawn('docker', 'run', '--rm', '-t',
-                       '--network', f'container:{container_name}',
-                       'lmenezes/cerebro:0.8.5',  # 0.9.1 does not work against ES 6.8.0
-                       f'-Dhttp.port={cerebro_port}')
+            kibana = client.containers.run('docker.elastic.co/kibana/kibana-oss:6.8.0',
+                                           auto_remove=True,
+                                           detach=True,
+                                           environment={
+                                               'ELASTICSEARCH_HOSTS': f'http://localhost:{proxy_port}',
+                                               'SERVER_PORT': kibana_port
+                                           },
+                                           network_mode=f'container:{proxy.name}',
+                                           tty=True)
+            self.containers.append(kibana)
+            # 0.9.1 does not work against ES 6.8.0
+            cerebro = client.containers.run('lmenezes/cerebro:0.8.5',
+                                            auto_remove=True,
+                                            command=[f'-Dhttp.port={cerebro_port}'],
+                                            detach=True,
+                                            network_mode=f'container:{proxy.name}',
+                                            tty=True)
+            self.containers.append(cerebro)
             time.sleep(10)
-            print(f'Now open Kibana at http://127.0.0.1:{kibana_port}/')
-            print(f'Now open Cerebro at http://127.0.0.1:{cerebro_port}/ and paste http://localhost:{proxy_port}')
-            self.wait()
+            print(f'Now open Kibana at http://127.0.0.1:{kibana_port}/ and open Cerebro '
+                  f'at http://127.0.0.1:{cerebro_port}/#/overview?host=http://localhost:'
+                  f'{proxy_port} (or paste in http://localhost:{proxy_port})')
+            for container in self.containers:
+                container.wait()
+        except KeyboardInterrupt:
+            pass
         finally:
-            self.kill()
+            for container in self.containers:
+                container.kill()
 
     @property
     def dss_end_point(self):
@@ -95,26 +99,6 @@ class KibanaProxy:
         es = boto3.client('es')
         domain = es.describe_elasticsearch_domain(DomainName=self.options.domain)
         return 'https://' + domain['DomainStatus']['Endpoint']
-
-    def spawn(self, *args: str, **env: str):
-        logged_command = ' '.join(chain(
-            (k + '=' + shlex.quote(v) for k, v in env.items()),
-            map(shlex.quote, args)))
-        log.info('Running %s', logged_command)
-        pid = os.spawnvpe(os.P_NOWAIT, args[0], list(args), env={**os.environ, **env})
-        self.pids[pid] = logged_command
-
-    def wait(self):
-        while self.pids:
-            pid, status = os.waitpid(-1, 0)
-            args = self.pids.pop(pid)
-            raise Exception(f'Exited: {args}')
-
-    def kill(self):
-        for pid, args in self.pids.items():
-            log.info('Terminating: %s', args)
-            os.kill(pid, signal.SIGINT)
-            os.waitpid(pid, 0)
 
 
 def main(argv):
