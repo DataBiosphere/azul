7be649c7e246664076300284f6e5ba01f3e7b494
Hannes Schmidt <hannes@ucsc.edu>
2020-08-31 18:07:19 -0700

    Fix AZUL_TDR_TARGET in example deployment (#2181)

--- scripts/register_sam.py
+++ scripts/register_sam.py
@@ -22,8 +22,15 @@ def main():
     tdr = TDRClient()
     tdr.register_with_sam()
     tdr.verify_authorization()
-    plugin = Plugin(TDRSource.parse(config.tdr_source(config.default_catalog)))
-    plugin.verify_authorization()
+
+    tdr_catalogs = (
+        catalog
+        for catalog, plugins in config.catalogs.items()
+        if plugins['repository'] == 'tdr'
+    )
+    for source in set(map(config.tdr_source, tdr_catalogs)):
+        plugin = Plugin(TDRSource.parse(source))
+        plugin.verify_authorization()
 
 
 if __name__ == '__main__':
