5e452b91d808f51943717921c192893333e6e89d
Hannes Schmidt <hannes@ucsc.edu>
2020-09-03 11:22:45 -0700

    [2/3] Add unit test coverage of IndexController (#1589)

--- test/indexer/test_indexer_controller.py
+++ test/indexer/test_indexer_controller.py
@@ -1,15 +1,14 @@
 import json
 import logging
-from typing import (
-    List,
-    Sequence,
-)
 
 import boto3
 from chalice.app import (
     SQSRecord,
 )
 import mock
+from more_itertools import (
+    one,
+)
 from moto import (
     mock_sqs,
     mock_sts,
@@ -25,20 +24,13 @@ from azul.azulclient import (
 from azul.indexer import (
     BundleFQID,
 )
-from azul.indexer.document import (
-    Contribution,
-)
 from azul.indexer.index_controller import (
     IndexController,
-)
-from azul.indexer.index_service import (
-    CataloguedTallies,
 )
 from azul.logging import (
     configure_test_logging,
 )
 from indexer import (
-    ForcedRefreshIndexService,
     IndexerTestCase,
 )
 
@@ -50,9 +42,6 @@ def setUpModule():
     configure_test_logging(log)
 
 
-Contributions = Sequence[Contribution]
-
-
 @mock_sts
 @mock_sqs
 class TestIndexController(IndexerTestCase):
@@ -62,6 +51,8 @@ class TestIndexController(IndexerTestCase):
         self.index_service.create_indices(self.catalog)
         self.client = AzulClient()
         self.controller = IndexController()
+        # noinspection PyPropertyAccess
+        self.controller.index_service = self.index_service
         self.queue_manager = queues.Queues(delete=True)
 
     def tearDown(self):
@@ -81,7 +72,7 @@ class TestIndexController(IndexerTestCase):
         }
         return SQSRecord(event_dict=event_dict, context='controller_test')
 
-    def test_contribution_action_invalid(self):
+    def test_invalid_notification(self):
         event = [
             self._mock_sqs_record(action='banana',
                                   notification='slug',
@@ -89,24 +80,26 @@ class TestIndexController(IndexerTestCase):
         ]
         self.assertRaises(AssertionError, self.controller.contribute, event)
 
-    def test_contribution_action_reindex(self):
+    def test_remote_reindex(self):
         event = [self._mock_sqs_record(action='reindex', prefix='ff')]
-        with mock.patch('azul.azulclient.AzulClient.do_remote_reindex') as mock_reindex:
+        with mock.patch.object(AzulClient, 'do_remote_reindex') as mock_reindex:
             mock_reindex.return_value = True
             self.controller.contribute(event)
             self.assertEqual(1, mock_reindex.call_count)
 
     def test_contribute_and_aggregate(self):
         """
-        Index two bundles that make contributions to the same project.
-        Inspect that the contributions match the tallies that are returned to
-        SQS. During aggregation only the project entity is deferred due to
+        Contribution and aggregation of two bundles
+
+        Index two bundles that make contributions to the same project. Inspect
+        that the contributions match the tallies that are returned to SQS.
+        During aggregation only the project entity is deferred due to
         multiple contributions.
         """
         self._create_mock_queues()
         event = []
         bundles = []
-        contributions_per_bundle: List[Contributions] = []
+        expected_entities = set()
         bundle_fqids = [
             BundleFQID('56a338fe-7554-4b5d-96a2-7df127a7640b', '2018-03-28T151023.074974Z'),
             BundleFQID('b2216048-7eaa-45f4-8077-5a3fb4204953', '2018-03-29T104041.822717Z')
@@ -118,57 +111,50 @@ class TestIndexController(IndexerTestCase):
                                                notification=notification))
             bundle = self._load_canned_bundle(bundle_fqid)
             bundles.append(bundle)
-            contributions_per_bundle.append(self.index_service.transform(self.catalog, bundle=bundle, delete=False))
-        repository_patch = mock.patch('azul.indexer.index_controller.IndexController.repository_plugin')
-        service_patch = mock.patch('azul.indexer.index_controller.IndexController.index_service',
-                                   new_callable=mock.PropertyMock)
-        with repository_patch as mock_repository, service_patch as mock_index_service:
-            mock_index_service.return_value = ForcedRefreshIndexService()
-            mock_repository.return_value = mock.MagicMock()
-            mock_repository.return_value.fetch_bundle.side_effect = bundles
-            # Test contributions from two bundles with from the same project
-            tallies = self._assert_notification_contributions(event, contributions_per_bundle)
+            # Invoke the service once to produce a set of expected entities so
+            # we don't need to hard-code them. Keep in mind that this test is
+            # not covering the service, only the controller.
+            contributions = self.index_service.transform(self.catalog, bundle, delete=False)
+            expected_entities.update(
+                (c.entity.entity_id, c.entity.entity_type)
+                for c in contributions
+            )
+
+        mock_plugin = mock.MagicMock()
+        mock_plugin.fetch_bundle.side_effect = bundles
+        with mock.patch.object(IndexController,
+                               'repository_plugin',
+                               return_value=mock_plugin):
+            # Test contribution
+            self.controller.contribute(event)
+            tallies = [
+                json.loads(m.body)
+                for m in self.queue_manager.read_messages(self.controller._tallies_queue())
+            ]
+            entities_from_tallies = {
+                (t['entity_id'], t['entity_type'])
+                for t in tallies
+            }
+            self.assertSetEqual(expected_entities, entities_from_tallies)
+            self.assertListEqual([mock.call(f) for f in bundle_fqids], mock_plugin.fetch_bundle.mock_calls)
+
             # Test aggregation for tallies, inspect for deferred tallies
-            project_tally = self._assert_aggregation_creates_deferral(tallies)
-            # Test aggregation for deferred tallies
-            self._assert_aggregation_handles_deferral(project_tally)
+            event = [self._mock_sqs_record(**t) for t in tallies]
+            self.controller.aggregate(event)
+            messages = self.queue_manager.read_messages(self.controller._tallies_queue())
 
-    def _assert_notification_contributions(self,
-                                           event: Sequence[SQSRecord],
-                                           contributions_per_bundle: Sequence[Contributions]):
-        self.controller.contribute(event)
-        tallies = [
-            json.loads(m.body)
-            for m in self.queue_manager.read_messages(self.controller._tallies_queue())
-        ]
-        entities_from_tallies = {(t['entity_id'], t['entity_type']) for t in tallies}
-        entities_from_contributions = {
-            (c.entity.entity_id, c.entity.entity_type)
-            for cs in contributions_per_bundle for c in cs
-        }
-        self.assertSetEqual(entities_from_tallies, entities_from_contributions)
-        return tallies
+            # Check that aggregation of project entity was deferred
+            project_tally = json.loads(one(messages).body)
+            expected_tally = {
+                'catalog': 'test',
+                'entity_type': 'projects',
+                'entity_id': '93f6a42f-1790-4af4-b5d1-8c436cb6feae',
+                'num_contributions': 2
+            }
+            self.assertDictEqual(project_tally, expected_tally)
 
-    def _assert_aggregation_creates_deferral(self, tallies: Sequence[CataloguedTallies]):
-        event = []
-        for tally in tallies:
-            event.append(self._mock_sqs_record(**tally))
-        self.controller.aggregate(event)
-        messages = self.queue_manager.read_messages(self.controller._tallies_queue())
-        # Check that the deferral for the project entity was returned to the tallies queue.
-        self.assertEqual(1, len(messages))
-        project_tally = json.loads(messages[0].body)
-        expected_tally = {
-            'catalog': 'test',
-            'entity_type': 'projects',
-            'entity_id': '93f6a42f-1790-4af4-b5d1-8c436cb6feae',
-            'num_contributions': 2
-        }
-        self.assertDictEqual(project_tally, expected_tally)
-        return project_tally
-
-    def _assert_aggregation_handles_deferral(self, deffered_tally: CataloguedTallies):
-        event = [self._mock_sqs_record(**deffered_tally)]
-        self.controller.aggregate(event)
-        messages = self.queue_manager.read_messages(self.controller._tallies_queue())
-        self.assertEqual(0, len(messages), )
+            # Test aggregation of deferred project entity
+            event = [self._mock_sqs_record(**project_tally)]
+            self.controller.aggregate(event)
+            messages = self.queue_manager.read_messages(self.controller._tallies_queue())
+            self.assertEqual(0, len(messages))
