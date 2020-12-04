f3e520429d6109619be96a9731d7fbf90085f3a0
Hannes Schmidt <hannes@ucsc.edu>
2020-12-03 16:06:41 -0800

    [r 2/2] Correctly identify dangling supplementary files (#2540)

--- src/azul/plugins/repository/tdr/__init__.py
+++ src/azul/plugins/repository/tdr/__init__.py
@@ -220,11 +220,16 @@ class Plugin(RepositoryPlugin):
             log.info('Retrieved links content, %i top-level links', len(links))
             project = EntityReference(entity_type='project',
                                       entity_id=links_json['project_id'])
-            subgraph_entities, inputs, outputs, supp_files = self._parse_links(links, project)
+            (
+                subgraph_entities,
+                inputs,
+                outputs,
+                supplementary_files
+            ) = self._parse_links(links, project)
             for entity in subgraph_entities:
                 entities[entity.entity_type].add(entity.entity_id)
 
-            dangling_inputs = self._dangling_inputs(inputs, outputs | supp_files)
+            dangling_inputs = self._dangling_inputs(inputs, outputs | supplementary_files)
             if dangling_inputs:
                 if log.isEnabledFor(logging.DEBUG):
                     log.debug('Subgraph %r has dangling inputs: %r', subgraph, dangling_inputs)
@@ -296,12 +301,12 @@ class Plugin(RepositoryPlugin):
         :return: A tuple of (1) a set of all entities found in the links, (2)
                  the subset of those entities that occur as inputs, (3)
                  those that occur as outputs, (4) those that occur as
-                 supplementary files
+                 supplementary files.
         """
         entities = set()
         inputs = set()
         outputs = set()
-        supp_files = set()
+        supplementary_files = set()
         entities.add(project)
         for link in links:
             link_type = link['link_type']
@@ -311,8 +316,8 @@ class Plugin(RepositoryPlugin):
                 entities.add(process)
                 for category in ('input', 'output', 'protocol'):
                     for entity in cast(JSONs, link[category + 's']):
-                        entity = EntityReference(entity_id=entity[category + '_id'],
-                                                 entity_type=entity[category + '_type'])
+                        entity = EntityReference(entity_type=entity[category + '_type'],
+                                                 entity_id=entity[category + '_id'])
                         entities.add(entity)
                         if category == 'input':
                             inputs.add(entity)
@@ -325,15 +330,14 @@ class Plugin(RepositoryPlugin):
                 require(associate == project,
                         'Supplementary file must be associated with the current project',
                         project, associate)
-                for supplementary_file in cast(JSONs, link['files']):
-                    supp_file_ref = EntityReference(entity_type='supplementary_file',
-                                                    entity_id=supplementary_file['file_id'])
-                    entities.add(supp_file_ref)
-                    supp_files.add(supp_file_ref)
-
+                for entity in cast(JSONs, link['files']):
+                    entity = EntityReference(entity_type='supplementary_file',
+                                             entity_id=entity['file_id'])
+                    entities.add(entity)
+                    supplementary_files.add(entity)
             else:
                 raise RequirementError('Unexpected link_type', link_type)
-        return entities, inputs, outputs, supp_files
+        return entities, inputs, outputs, supplementary_files
 
     def _dangling_inputs(self, inputs: Entities, outputs: Entities) -> Entities:
         return {
