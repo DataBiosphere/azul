5dfc35f61cd5a7a588780223ec8fcc5f52d1c983
Hannes Schmidt <hannes@ucsc.edu>
2020-12-21 17:07:02 -0800

    [2/2] Make indexer tests insensitive to AZUL_INDEXER_CONCURRENCY (#2107)

--- test/indexer/test_hca_indexer.py
+++ test/indexer/test_hca_indexer.py
@@ -119,15 +119,14 @@ def setUpModule():
 class TestHCAIndexer(IndexerTestCase):
 
     def _get_all_hits(self):
+        # Without `preserve_order`, hits are sorted by `_doc`, which is fastest
+        # but causes the `sort` field in hits to vary unpredictably, based on
+        # the number of shards, for example, but also under what appear to be
+        # unrelated code changes. This makes asserting test results verbatim
+        # impossible. Thus we set `preserve_order` to True.
         hits = list(scan(client=self.es_client,
                          index=','.join(self.index_service.index_names(self.catalog)),
-                         doc_type="doc",
-                         # Without preserve_order, hits are sorted based on
-                         # "_doc", which is fastest. Unfortunately, hits are
-                         # also contaminated with a "sort" field that varies
-                         # unpredictably based on the number of shards. This
-                         # makes asserting test results verbatim impossible.
-                         # Thus we set preserve_order to True.
+                         doc_type='doc',
                          preserve_order=True))
         for hit in hits:
             self._verify_sorted_lists(hit)
