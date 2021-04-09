5515201c32de55274ae44590eac118e98eca8799
Hannes Schmidt <hannes@ucsc.edu>
2021-04-08 19:39:40 -0700

    [1/2] Refactor async manifest code (#2949)
    The actual refactoring

--- src/azul/service/manifest_controller.py
+++ src/azul/service/manifest_controller.py
@@ -3,12 +3,12 @@ from typing import (
 )
 
 import attr
-from botocore.exceptions import (
-    ClientError,
-)
 from chalice import (
     BadRequestError,
     Response,
+)
+from furl import (
+    furl,
 )
 
 from azul import (
@@ -21,8 +21,11 @@ from azul.service import (
 )
 from azul.service.async_manifest_service import (
     AsyncManifestService,
+    InvalidTokenError,
+    Token,
 )
 from azul.service.manifest_service import (
+    Manifest,
     ManifestFormat,
     ManifestService,
 )
@@ -48,11 +51,11 @@ class ManifestController(Controller):
     def service(self) -> ManifestService:
         return ManifestService(StorageService())
 
-    def get_manifest(self, event: JSON) -> JSON:
-        manifest = self.service.get_manifest(format_=ManifestFormat(event['format']),
-                                             catalog=event['catalog'],
-                                             filters=event['filters'],
-                                             object_key=event['object_key'])
+    def get_manifest(self, input: JSON) -> JSON:
+        manifest = self.service.get_manifest(format_=ManifestFormat(input['format_']),
+                                             catalog=input['catalog'],
+                                             filters=input['filters'],
+                                             object_key=input['object_key'])
         return manifest.to_json()
 
     def get_manifest_async(self,
@@ -61,11 +64,47 @@ class ManifestController(Controller):
                            catalog: CatalogName,
                            query_params: Mapping[str, str],
                            fetch: bool):
-        wait_time, manifest = self.__file_manifest(self_url, catalog, query_params)
-        if fetch:
+
+        token = query_params.get('token')
+        if token is None:
+            service = self.service
+            format_ = ManifestFormat(query_params['format'])
+            filters = service.parse_filters(query_params['filters'])
+            object_key, manifest = service.get_cached_manifest(format_=format_,
+                                                               catalog=catalog,
+                                                               filters=filters)
+            if manifest is None:
+                assert object_key is not None
+                input = dict(format_=format_.value,
+                             catalog=catalog,
+                             filters=filters,
+                             object_key=object_key)
+                token = self.async_service.start_generation(input)
+        else:
+            try:
+                token = Token.decode(token)
+                token_or_manifest = self.async_service.inspect_generation(token)
+            except InvalidTokenError as e:
+                raise BadRequestError(e.args) from e
+            else:
+                if isinstance(token_or_manifest, Token):
+                    token, manifest = token_or_manifest, None
+                elif isinstance(token_or_manifest, Manifest):
+                    token, manifest = None, token_or_manifest
+                else:
+                    assert False, token_or_manifest
+
+        if manifest is None:
+            location = furl(self_url, args={'token': token.encode()})
             body = {
-                'Status': 301 if wait_time else 302,
-                'Location': manifest.location,
+                'Status': 301,
+                'Location': location.url,
+                'Retry-After': token.wait_time
+            }
+        else:
+            body = {
+                'Status': 302,
+                'Location': manifest.location
             }
             try:
                 command_line = manifest.properties['command_line']
@@ -73,41 +112,9 @@ class ManifestController(Controller):
                 pass
             else:
                 body['CommandLine'] = command_line
-            if wait_time:  # Only return Retry-After if manifest is not ready
-                body['Retry-After'] = wait_time
+
+        if fetch:
             return Response(body=body)
         else:
-            return Response(body='',
-                            headers={
-                                'Retry-After': str(wait_time),
-                                'Location': manifest.location
-                            },
-                            status_code=301 if wait_time else 302)
-
-    def __file_manifest(self, self_url: str, catalog: CatalogName, query_params):
-        format_ = ManifestFormat(query_params['format'])
-        service = self.service
-        filters = service.parse_filters(query_params['filters'])
-
-        object_key = None
-        token = query_params.get('token')
-        if token is None:
-            object_key, manifest = service.get_cached_manifest(format_=format_,
-                                                               catalog=catalog,
-                                                               filters=filters)
-            if manifest is not None:
-                return 0, manifest
-        try:
-            return self.async_service.start_or_inspect_manifest_generation(
-                self_url,
-                format_=format_,
-                catalog=catalog,
-                filters=filters,
-                token=token,
-                object_key=object_key)
-        except ClientError as e:
-            if e.response['Error']['Code'] == 'ExecutionDoesNotExist':
-                raise BadRequestError('Invalid token given')
-            raise
-        except ValueError as e:
-            raise BadRequestError(e.args)
+            headers = {k: str(body[k]) for k in body.keys() & {'Location', 'Retry-After'}}
+            return Response(body='', status_code=body['Status'], headers=headers)
