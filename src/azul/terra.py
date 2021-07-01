8432ace042c8fbcaca630e008cd8ad4f02b65892
jesse <brennan@ucsc.edu>
2021-07-01 13:10:08 -0700

    Fix: `make sam` fails with ambiguous response from TDR (#3192)

--- src/azul/terra.py
+++ src/azul/terra.py
@@ -268,7 +268,13 @@ class TDRClient(SAMClient):
         response = self._request('GET', endpoint, fields=params)
         if response.status == 200:
             response = json.loads(response.data)
-            total = response['total']
+            try:
+                # FIXME: Once filteredTotal is deployed in Terra prod, we can
+                #        remove this try/except block.
+                #        https://github.com/DataBiosphere/azul/issues/3195
+                total = response['filteredTotal']
+            except KeyError:
+                total = response['total']
             if total == 0:
                 raise self._insufficient_access(resource)
             elif total == 1:
