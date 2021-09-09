5259d3d95c3c3dd777fb07be732e534d81653cac
Noah Aviel Dove <nadove@ucsc.edu>
2021-09-09 03:43:31 -0700

    Summary aggregations use consistent names

--- src/azul/service/index_query_service.py
+++ src/azul/service/index_query_service.py
@@ -161,7 +161,7 @@ class IndexQueryService(ElasticsearchService):
             ],
             'cell_suspensions': [
                 'totalCellCount',
-                'cellCountSummaries',
+                'cellCountSummaries'
             ]
         }
 
