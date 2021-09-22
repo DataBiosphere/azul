eb170a9de67475497ce880d798a6a3ed7e8604b2
Noah Aviel Dove <nadove@ucsc.edu>
2021-09-22 12:59:51 -0700

    Refactor scripts/post_deploy.py

--- scripts/post_deploy.py
+++ scripts/post_deploy.py
@@ -20,15 +20,16 @@ from azul.terra import (
 
 log = logging.getLogger(__name__)
 
+tdr = TDRClient.with_service_account_credentials()
+public_tdr = TDRClient.with_public_service_account_credentials()
 
-def main():
-    configure_script_logging(log)
-    tdr = TDRClient.with_service_account_credentials()
+
+def register_with_sam():
     tdr.register_with_sam()
-
-    public_tdr = TDRClient.with_public_service_account_credentials()
     public_tdr.register_with_sam()
 
+
+def verify_sources():
     tdr_catalogs = {
         catalog.name
         for catalog in config.catalogs.values()
@@ -37,20 +38,33 @@ def main():
     assert tdr_catalogs, tdr_catalogs
     for source in set(chain(*map(config.tdr_sources, tdr_catalogs))):
         source = TDRSourceSpec.parse(source)
-        api_project = tdr.lookup_source_project(source)
-        require(api_project == source.project,
-                'Actual Google project of TDR source differs from configured '
-                'one',
-                api_project, source)
-        tdr.check_api_access(source)
-        tdr.check_bigquery_access(source)
+        verify_source(source)
 
+
+def verify_source(source: TDRSourceSpec):
+    api_project = tdr.lookup_source_project(source)
+    require(api_project == source.project,
+            'Actual Google project of TDR source differs from configured '
+            'one',
+            api_project, source)
+    tdr.check_api_access(source)
+    tdr.check_bigquery_access(source)
+
+
+def verify_source_access():
     public_snapshots = set(public_tdr.snapshot_names_by_id())
     all_snapshots = set(tdr.snapshot_names_by_id())
     diff = public_snapshots - all_snapshots
     require(not diff,
             'The public service account can access snapshots that the indexer '
             'service account cannot', diff)
+
+
+def main():
+    configure_script_logging(log)
+    register_with_sam()
+    verify_sources()
+    verify_source_access()
 
 
 if __name__ == '__main__':
