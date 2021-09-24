db83767dd712502c9461e5ca596fb5bf15fb2b4e
Abraham Chavez <achave11@ucsc.edu>
2021-09-24 10:14:29 -0700

    Fix: Simultaneous requests for same manifest kick off multiple redundant generations (#3271)

--- src/azul/service/async_manifest_service.py
+++ src/azul/service/async_manifest_service.py
@@ -6,9 +6,9 @@ from enum import (
 import json
 import logging
 from typing import (
+    Tuple,
     Union,
 )
-import uuid
 
 import attr
 from botocore.exceptions import (
@@ -36,6 +36,8 @@ class Status(Enum, metaclass=CaseInsensitiveEnumMeta):
     running = auto()
     succeeded = auto()
     failed = auto()
+    timed_out = auto()
+    aborted = auto()
 
 
 class InvalidTokenError(Exception):
@@ -76,30 +78,46 @@ class AsyncManifestService(AbstractService):
     """
     helper = StepFunctionHelper()
 
+    step_function_execution_retries = 3
+
     def __init__(self, state_machine_name):
         self.state_machine_name = state_machine_name
 
-    def start_generation(self, input: JSON) -> Token:
-        execution_id = str(uuid.uuid4())
-        self.helper.start_execution(self.state_machine_name,
-                                    execution_id,
-                                    execution_input=input)
-        return Token(execution_id=execution_id,
-                     request_index=0,
-                     wait_time=self._get_next_wait_time(0))
+    def start_generation(self, input: JSON, execution_id: str) -> Token:
+
+        for retry_index in range(self.step_function_execution_retries):
+            token = Token(execution_id=f'{execution_id}_{retry_index}',
+                          request_index=0,
+                          wait_time=self._get_next_wait_time(0))
+            try:
+                self.helper.start_execution(self.state_machine_name,
+                                            token.execution_id,
+                                            execution_input=input)
+            except ClientError as e:
+                if e.response['Error']['Code'] == 'ExecutionAlreadyExists':
+                    if retry_index + 1 == self.step_function_execution_retries:
+                        raise StateMachineError('Maximum manifest generation retries reached.',
+                                                token.execution_id)
+                    output, status = self._inspect_generation(token)
+                    if status not in Status:
+                        raise StateMachineError(status, output)
+                    if status is Status.running:
+                        return token.advance(wait_time=self._get_next_wait_time(token.request_index))
+                    else:
+                        # If a manifest expires before the name of the Step
+                        # Function execution that created it can be reused
+                        # (90 days), StartExecution will fail with
+                        # ExecutionAlreadyExists. In that case we need to retry
+                        # even a successful execution, so that it can recreate
+                        # the manifest.
+                        continue
+                else:
+                    raise
+            else:
+                return token
 
     def inspect_generation(self, token) -> Union[Token, JSON]:
-        try:
-            name = self.state_machine_name
-            execution = self.helper.describe_execution(state_machine_name=name,
-                                                       execution_name=token.execution_id)
-        except ClientError as e:
-            if e.response['Error']['Code'] == 'ExecutionDoesNotExist':
-                raise InvalidTokenError from e
-            else:
-                raise
-        output = execution.get('output')
-        status = Status[execution['status']]
+        output, status = self._inspect_generation(token)
         if status is Status.succeeded:
             # Because describe_execution is eventually consistent, output may
             # not yet be present
@@ -111,6 +129,20 @@ class AsyncManifestService(AbstractService):
             return token.advance(wait_time=self._get_next_wait_time(token.request_index))
         else:
             raise StateMachineError(status, output)
+
+    def _inspect_generation(self, token: Token) -> Tuple[str, Status]:
+        try:
+            name = self.state_machine_name
+            execution = self.helper.describe_execution(state_machine_name=name,
+                                                       execution_name=token.execution_id)
+        except ClientError as e:
+            if e.response['Error']['Code'] == 'ExecutionDoesNotExist':
+                raise InvalidTokenError from e
+            else:
+                raise
+        output = execution.get('output')
+        status = Status[execution['status']]
+        return output, status
 
     def _get_next_wait_time(self, request_index: int) -> int:
         wait_times = [1, 1, 4, 6, 10]
