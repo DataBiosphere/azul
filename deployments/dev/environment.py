DataBiosphere/azul

commit c96cf6ae7130228a3aeac0327c8c724ee0abbc1d

Remove lungmap catalogs from dev and sandbox

deployments/dev/environment.py
@@ -158,12 +158,6 @@ def env() -> Mapping[str, Optional[str]]:
             'tdr:datarepo-dev-e8e0a59a:snapshot/hca_dev_f8aa201c4ff145a4890e840d63459ca2__20210901_20210903:',
             'tdr:datarepo-dev-96d8e08c:snapshot/hca_dev_faeedcb0e0464be7b1ad80a3eeabb066__20210831_20210903:'
         ]),
-        **{
-            f'AZUL_TDR_{catalog.upper()}_SOURCES': ','.join([
-                'tdr:broad-jade-dev-data:snapshot/lungmap_dev_20210412__20210414:',
-            ])
-            for catalog in ('lungmap', 'it3lungmap')
-        },
         'AZUL_TDR_SOURCE_LOCATION': 'us-central1',
         'AZUL_TDR_SERVICE_URL': 'https://jade.datarepo-dev.broadinstitute.org',
         'AZUL_SAM_SERVICE_URL': 'https://sam.dsde-dev.broadinstitute.org',

deployments/sandbox/environment.py
@@ -76,12 +76,6 @@ def env() -> Mapping[str, Optional[str]]:
             'tdr:datarepo-dev-dbc582d9:snapshot/hca_dev_dbcd4b1d31bd4eb594e150e8706fa192__20210827_20210902:4',
             'tdr:datarepo-dev-10f0610a:snapshot/hca_dev_f81efc039f564354aabb6ce819c3d414__20210827_20210903:4'
         ]),
-        **{
-            f'AZUL_TDR_{catalog.upper()}_SOURCES': ','.join([
-                'tdr:broad-jade-dev-data:snapshot/lungmap_dev_20210412__20210414:/0',
-            ])
-            for catalog in ('lungmap', 'it3lungmap')
-        },
         'AZUL_TDR_SOURCE_LOCATION': 'us-central1',
         'AZUL_TDR_SERVICE_URL': 'https://jade.datarepo-dev.broadinstitute.org',
         'AZUL_SAM_SERVICE_URL': 'https://sam.dsde-dev.broadinstitute.org',

commit 63c3baaf40349b8224a3b0b2bc56d54872696470

Really remove lungmap catalogs (HumanCellAtlas/dcp2#17)
Regression from c96cf6ae

deployments/dev/environment.py
@@ -41,9 +41,7 @@ def env() -> Mapping[str, Optional[str]]:
                                     repository=dict(name='tdr')))
             for name, atlas, internal in [
                 ('dcp2', 'hca', False),
-                ('lungmap', 'lungmap', False),
                 ('it2', 'hca', True),
-                ('it3lungmap', 'lungmap', True)
             ]
         }),
 

deployments/sandbox/environment.py
@@ -51,9 +51,7 @@ def env() -> Mapping[str, Optional[str]]:
                                     repository=dict(name='tdr')))
             for name, atlas, internal in [
                 ('dcp2', 'hca', False),
-                ('lungmap', 'lungmap', False),
                 ('it2', 'hca', True),
-                ('it3lungmap', 'lungmap', True)
             ]
         }),
 