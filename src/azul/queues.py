DataBiosphere/azul

commit ed20d0a71b7fb7e6e87f604e8a4b2c45ca2b24cd

Fix brittle coupling with lambda names in manage_queues.py (#1848)

src/azul/queues.py
@@ -43,6 +43,9 @@ from azul.files import (
 from azul.lambdas import (
     Lambdas,
 )
+from azul.modules import (
+    load_app_module,
+)
 
 logger = logging.getLogger(__name__)
 
@@ -371,6 +374,17 @@ class Queues:
         """
         Enable or disable the readers and writers of the given queues
         """
+        indexer = load_app_module('indexer')
+        functions_by_queue = {
+            handler.queue: [config.indexer_function_name(handler.name)]
+            for handler in indexer.app.handler_map.values()
+            if hasattr(handler, 'queue')
+        }
+        # Since the indexer lambda writes to the notifications queue, we must
+        # deactivate it also.
+        notifications_queue = config.notifications_queue_name()
+        functions_by_queue[notifications_queue].append(config.indexer_name)
+
         with ThreadPoolExecutor(max_workers=len(queues)) as tpe:
             futures = []
 
@@ -378,18 +392,13 @@ class Queues:
                 futures.append(tpe.submit(f, *args, **kwargs))
 
             for queue_name, queue in queues.items():
-                if queue_name == config.notifications_queue_name():
-                    submit(self._manage_lambda, config.indexer_name, enable)
-                    submit(self._manage_sqs_push, config.indexer_name + '-contribute', queue, enable)
-                elif queue_name == config.notifications_queue_name(retry=True):
-                    submit(self._manage_sqs_push, config.indexer_name + '-contribute_retry', queue, enable)
-                elif queue_name == config.tallies_queue_name():
-                    submit(self._manage_sqs_push, config.indexer_name + '-aggregate', queue, enable)
-                elif queue_name == config.tallies_queue_name(retry=True):
-                    # FIXME: Brittle coupling between the string literal below and
-                    #        the handler function name in app.py
-                    #        https://github.com/DataBiosphere/azul/issues/1848
-                    submit(self._manage_sqs_push, config.indexer_name + '-aggregate_retry', queue, enable)
+                try:
+                    functions = functions_by_queue[queue_name]
+                except KeyError:
+                    assert queue_name in config.fail_queue_names
+                else:
+                    for function in functions:
+                        submit(self._manage_lambda, function, enable)
             self._handle_futures(futures)
             futures = [tpe.submit(self._wait_for_queue_idle, queue) for queue in queues.values()]
             self._handle_futures(futures)

lambdas/service/app.py
@@ -351,7 +351,8 @@ class ServiceApp(AzulChaliceApp):
                  **params: str) -> str:
         file_uuid = urllib.parse.quote(file_uuid, safe='')
         view_function = fetch_repository_files if fetch else repository_files
-        url = self.self_url(endpoint_path=view_function.path.format(file_uuid=file_uuid))
+        path = one(view_function.path)
+        url = self.self_url(endpoint_path=path.format(file_uuid=file_uuid))
         params = urllib.parse.urlencode(dict(params, catalog=catalog))
         return f'{url}?{params}'
 
@@ -1601,7 +1602,7 @@ def repository_files(file_uuid: str) -> Response:
     'responses': {
         '200': {
             'description': format_description(f'''
-                Emulates the response code and headers of {repository_files.path}
+                Emulates the response code and headers of {one(repository_files.path)}
                 while bypassing the default user agent behavior. Note that the
                 status code of a successful response will be 200 while the
                 `Status` field of its body will be 302.

src/azul/chalice.py
@@ -76,9 +76,6 @@ class AzulChaliceApp(Chalice):
 
             def decorator(view_func):
                 self._register_spec(path, path_spec, method_spec, methods)
-                # Stash the URL path a view function is bound to as an attribute of
-                # the function itself.
-                view_func.path = path
                 return chalice_decorator(view_func)
 
             return decorator
@@ -171,6 +168,8 @@ class AzulChaliceApp(Chalice):
         else:
             log.info('Returning %i response. To log headers and body, set AZUL_DEBUG to 1.', response.status_code)
 
+    absent = object()
+
     def _register_handler(self,
                           handler_type,
                           name,
@@ -182,11 +181,25 @@ class AzulChaliceApp(Chalice):
                                   wrapped_handler, kwargs, options)
         # Our handlers reference the name of the corresponding Lambda function
         # which allows the handler to be the single source of truth when
-        # configuring Terraform, etc.
-        if hasattr(wrapped_handler, 'name'):
-            assert wrapped_handler.name == name
-        else:
-            wrapped_handler.name = name
+        # configuring Terraform, etc. We store other parameters used to
+        # configure the handler for the same reason.
+        for attribute, new_value, is_additive in [
+            ('name', name, False),
+            ('queue', kwargs.get('queue', self.absent), False),
+            ('path', kwargs.get('path', self.absent), True)
+        ]:
+            if new_value is not self.absent:
+                try:
+                    old_value = getattr(wrapped_handler, attribute)
+                except AttributeError:
+                    if is_additive:
+                        new_value = [new_value]
+                    setattr(wrapped_handler, attribute, new_value)
+                else:
+                    if is_additive:
+                        old_value.append(new_value)
+                    else:
+                        assert old_value == new_value
 
     # Some type annotations to help with auto-complete
     lambda_context: LambdaContext

commit 11d49dc6cf009ee52d33587a49cdecd00f38504c

Fix: Cancelling a reindex takes 2+ hours (#3304)
Regression from ed20d0a71b7fb7e6e87f604e8a4b2c45ca2b24cd

src/azul/queues.py
@@ -385,14 +385,10 @@ class Queues:
         """
         indexer = load_app_module('indexer')
         functions_by_queue = {
-            handler.queue: [config.indexer_function_name(handler.name)]
+            handler.queue: config.indexer_function_name(handler.name)
             for handler in indexer.app.handler_map.values()
             if hasattr(handler, 'queue')
         }
-        # Since the indexer lambda writes to the notifications queue, we must
-        # deactivate it also.
-        notifications_queue = config.notifications_queue_name()
-        functions_by_queue[notifications_queue].append(config.indexer_name)
 
         with ThreadPoolExecutor(max_workers=len(queues)) as tpe:
             futures = []
@@ -402,12 +398,14 @@ class Queues:
 
             for queue_name, queue in queues.items():
                 try:
-                    functions = functions_by_queue[queue_name]
+                    function = functions_by_queue[queue_name]
                 except KeyError:
                     assert queue_name in config.fail_queue_names
                 else:
-                    for function in functions:
-                        submit(self._manage_lambda, function, enable)
+                    if queue_name == config.notifications_queue_name():
+                        # Prevent new notifications from being added
+                        submit(self._manage_lambda, config.indexer_name, enable)
+                    submit(self._manage_sqs_push, function, queue, enable)
             self._handle_futures(futures)
             futures = [tpe.submit(self._wait_for_queue_idle, queue) for queue in queues.values()]
             self._handle_futures(futures)