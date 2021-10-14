bc26ab8cfa92e27c6b5c496a882c6ec09e1f1501
Hannes Schmidt <hannes@ucsc.edu>
2021-10-13 17:32:09 -0700

    [r] Remove support for full manifest (#3527, #2693)

--- test/indexer/test_hca_indexer.py
+++ test/indexer/test_hca_indexer.py
@@ -9,9 +9,6 @@ from concurrent.futures import (
     ThreadPoolExecutor,
 )
 import copy
-from copy import (
-    deepcopy,
-)
 from itertools import (
     chain,
 )
@@ -86,9 +83,6 @@ from azul.logging import (
 )
 from azul.plugins import (
     MetadataPlugin,
-)
-from azul.plugins.metadata.hca.full_metadata import (
-    FullMetadata,
 )
 from azul.plugins.repository.dss import (
     DSSBundle,
@@ -165,21 +159,6 @@ class TestHCAIndexer(IndexerTestCase):
         expected_hits = self._load_canned_result(self.old_bundle)
         hits = self._get_all_hits()
         self.assertElasticsearchResultsEqual(expected_hits, hits)
-
-    def test_full_metadata_with_cgm(self):
-        """
-        Reproduce https://github.com/DataBiosphere/azul/issues/2440.
-
-        Generating the full manifest for a contributor-generated matrix bundle
-        caused an extraneous `null` to be stored in the index.
-        """
-        bundle_fqid = self.bundle_fqid(uuid='4f2fc365-9f97-51ca-bbfe-fe30cefc333d',
-                                       version='2020-10-26T09:37:17.517006Z')
-        self._index_canned_bundle(bundle_fqid)
-        for hit in self._get_all_hits():
-            entity_type, _ = self._parse_index_name(hit)
-            if entity_type == 'bundles':
-                self.assertNotIn(None, hit['_source']['contents']['metadata'], hit)
 
     def test_deletion(self):
         """
@@ -1765,24 +1744,6 @@ class TestHCAIndexer(IndexerTestCase):
             for file in contents['files']:
                 self.assertEqual(file['content_description'], ['RNA sequence'])
 
-    def test_metadata_generator(self):
-        bundle_fqid = self.bundle_fqid(uuid='587d74b4-1075-4bbf-b96a-4d1ede0481b2',
-                                       version='2018-10-10T022343.182000Z')
-        bundle = self._load_canned_bundle(bundle_fqid)
-        full_metadata = FullMetadata()
-        full_metadata.add_bundle(bundle)
-        metadata_rows = full_metadata.dump()
-        expected_metadata_contributions = 20
-        self.assertEqual(expected_metadata_contributions, len(metadata_rows))
-        for metadata_row in metadata_rows:
-            self.assertEqual(bundle.uuid, metadata_row['bundle_uuid'])
-            self.assertEqual(bundle.version, metadata_row['bundle_version'])
-            if metadata_row['file_format'] == 'matrix':
-                expected_file_name = '377f2f5a-4a45-4c62-8fb0-db9ef33f5cf0.zarr/'
-                self.assertEqual(expected_file_name, metadata_row['file_name'])
-            else:
-                self.assertIn(metadata_row['file_format'], {'txt', 'csv', 'fastq.gz', 'results', 'bam', 'bai'})
-
     def test_related_files_field_exclusion(self):
         bundle_fqid = self.bundle_fqid(uuid='587d74b4-1075-4bbf-b96a-4d1ede0481b2',
                                        version='2018-10-10T022343.182000Z')
@@ -1815,95 +1776,6 @@ class TestHCAIndexer(IndexerTestCase):
                                          }
                                      })
         self.assertEqual(0, hits["hits"]["total"])
-
-    def test_metadata_field_exclusion(self):
-        self._index_canned_bundle(self.old_bundle)
-        bundles_index = config.es_index_name(catalog=self.catalog,
-                                             entity_type='bundles',
-                                             aggregate=False)
-
-        # Ensure that a metadata row exists …
-        hits = self._get_all_hits()
-        bundles_hit = one(hit['_source'] for hit in hits if hit['_index'] == bundles_index)
-        expected_metadata_hits = 2
-        self.assertEqual(expected_metadata_hits, len(bundles_hit['contents']['metadata']))
-        for metadata_row in bundles_hit['contents']['metadata']:
-            actual_fqid = self.bundle_fqid(uuid=metadata_row['bundle_uuid'],
-                                           version=metadata_row['bundle_version'])
-            self.assertEqual(self.old_bundle, actual_fqid)
-
-        # … but that it can't be used for queries
-        hits = self.es_client.search(index=bundles_index,
-                                     body={
-                                         "query": {
-                                             "match": {
-                                                 "contents.metadata.bundle_uuid": self.old_bundle.uuid
-                                             }
-                                         }
-                                     })
-        self.assertEqual(0, hits["hits"]["total"])
-
-        # Check that the dynamic mapping has the metadata field disabled
-        mapping = self.es_client.indices.get_mapping(index=bundles_index)
-        contents = mapping[bundles_index]['mappings']['doc']['properties']['contents']
-        self.assertFalse(contents['properties']['metadata']['enabled'])
-
-        # Ensure we can find the bundle UUID outside of `metadata`.
-        hits = self.es_client.search(index=bundles_index,
-                                     body={
-                                         "query": {
-                                             "match": {
-                                                 "bundle_uuid": self.old_bundle.uuid
-                                             }
-                                         }
-                                     })
-        self.assertEqual(1, hits["hits"]["total"])
-
-    def test_dynamic_mappings_off(self):
-        """
-        Prove that dynamic mappings in Elasticsearch are disabled.
-
-        Index a bundle in which a field contains a value that would match one of
-        the patterns used by the Elasticsearch dynamic mapper to infer the field
-        type. Then index a bundle where that same field has a value that would
-        violate the type inferred by the mapper. If the dynamic mapper were
-        enabled, indexing the second bundle would fail.
-        """
-        singlecell_bundle = self._load_canned_bundle(self.new_bundle)
-        multicell_fqid = self.bundle_fqid(uuid='e0ae8cfa-2b51-4419-9cde-34df44c6458a',
-                                          version='2018-12-05T230917.591044Z')
-        multicell_bundle = self._load_canned_bundle(multicell_fqid)
-
-        f0, f1 = (f'cell_line_{i}.json' for i in [0, 1])
-        d0, d1, d2 = '2014-10-24', '2014-11-09', '2015-01-09'
-
-        singlecell_bundle.metadata_files[f0] = deepcopy(multicell_bundle.metadata_files[f0])
-
-        singlecell_bundle.metadata_files[f0]['provenance'] = {
-            'document_id': 'e7e0f358-a681-4412-8888-318234f90ca9',
-            'submission_date': '2019-05-15T09:36:02.702Z',
-            'update_date': '2019-05-15T09:36:11.640Z'
-        }
-        singlecell_bundle.metadata_files[f0]['date_established'] = d0
-        multicell_bundle.metadata_files[f0]['date_established'] = d1
-        multicell_bundle.metadata_files[f1]['date_established'] = d2
-
-        bundles_index = config.es_index_name(catalog=self.catalog,
-                                             entity_type='bundles',
-                                             aggregate=False)
-        seen_docs = []
-        for bundle, expected_date in [
-            (singlecell_bundle, d0),
-            (multicell_bundle, f'{d1}||{d2}')
-        ]:
-            self._index_bundle(bundle)
-            hits = self._get_all_hits()
-            docs = [hit['_source']
-                    for hit in hits
-                    if hit['_index'] == bundles_index and hit['_source'] not in seen_docs]
-            seen_docs.extend(docs)
-            doc = one(docs)
-            self.assertEqual(expected_date, doc['contents']['metadata'][0]['cell_line.date_established'])
 
 
 # FIXME: move to separate class
