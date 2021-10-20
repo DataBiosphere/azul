DataBiosphere/azul

commit 8c61677f3f9f4ae2b796a3295e282398c84ff84c

[u] Define sources within catalog JSON (#3114)

environment.py
@@ -39,6 +39,10 @@ def env() -> Mapping[str, Optional[str]]:
         #           plugin_type: {'name'=plugin_package},
         #           ...
         #       }
+        #       'sources': [
+        #                   source,
+        #                   ...
+        #       ]
         #   }
         # }
         #
@@ -372,54 +376,6 @@ def env() -> Mapping[str, Optional[str]]:
         # the best I could come up with is the profile name.
         #
         'TF_DATA_DIR': '{project_root}/deployments/.active/.terraform.{AWS_PROFILE}',
-
-        # Identifies the canned staging area to index.
-        #
-        # The syntax in EBNF is:
-        #
-        # sources = source (',', source )* ;
-        #
-        # source = GitHub URL,
-        #          ':', [ UUID prefix [ '/', Partition prefix length ] ] ;
-        #
-        # Example:
-        #
-        # https://github.com/HumanCellAtlas/schema-test-data/tree/de355ca/tests:
-        #
-        # The GitHub URL must have the syntax
-        #
-        # 'https://github.com/', <owner>, '/', <name>, '/tree/', <ref>, ['/', <path>] ;
-        #
-        # `ref` can be a branch, tag, or commit SHA. If `ref` contains special
-        # characters like `/`, '?` or `#` they must be URL-encoded.
-        #
-        'azul_canned_sources': None,
-
-        # Identifies the Terra Data Repository datasets or snapshots to index.
-        #
-        # The syntax in EBNF is:
-        #
-        # sources = source (',', source )* ;
-        #
-        # source = 'tdr:', Google Cloud project name,
-        #          ':', ( 'dataset' | 'snapshot' ),
-        #          '/', TDR dataset or snapshot name,
-        #          ':', [ UUID prefix [ '/', Partition prefix length ] ] ;
-        #
-        # Example:
-        #
-        # tdr:broad-jade-dev-data:dataset/hca_mvp:2/1,tdr:broad-jade-dev-data:snapshot/hca_mvp:2
-        #
-        # To configure a source specific to a particular catalog, include the
-        # catalog name in all upper case in the variable name. For example, to
-        # configure a source specific to a catalog named `bar0`, use
-        #
-        # AZUL_TDR_BAR0_SOURCES
-        #
-        # If defined, the catalog-specific variable will be used, otherwise this
-        # variable will be used.
-        #
-        'AZUL_TDR_SOURCES': None,
 
         # BigQuery dataset location of the TDR snapshots the deployment is
         # configured to index. All configured snapshots must reside in the same

deployments/prod/environment.py
@@ -40,48 +40,47 @@ def env() -> Mapping[str, Optional[str]]:
         'AZUL_S3_BUCKET': 'edu-ucsc-gi-azul-dcp2-prod-storage-{AZUL_DEPLOYMENT_STAGE}',
 
         'AZUL_CATALOGS': json.dumps({
-            **{
-                f'{name}{rel}': dict(atlas='hca',
-                                     internal=internal,
-                                     plugins=dict(metadata=dict(name='hca'),
-                                                  repository=dict(name='tdr')))
-                for rel in (9, 10, 1)
-                for name, internal in (('dcp', False), ('it', True))
-            },
-            **{
-                name: dict(atlas='lungmap',
-                           internal=internal,
-                           plugins=dict(metadata=dict(name='hca'),
-                                        repository=dict(name='tdr')))
-                for name, internal in (('lungmap', False), ('it0lungmap', True))
-            }
+            f'{name}': dict(atlas=atlas,
+                            internal=bool(i),
+                            plugins=dict(metadata=dict(name='hca'),
+                                         repository=dict(name='tdr')),
+                            sources=sources)
+            for atlas, names, sources in [
+                (
+                    'hca',
+                    ['dcp9', 'it9'],
+                    [
+                        'tdr:tdr-fp-546ade29:snapshot/hca_prod_20201120_dcp2___20210910_dcp9:',
+                    ]
+                ),
+                (
+                    'hca',
+                    ['dcp10', 'it10'],
+                    [
+                        'tdr:tdr-fp-43194825:snapshot/hca_prod_20201120_dcp2___20211004_dcp10:'
+                    ]
+                ),
+                (
+                    'hca',
+                    ['dcp1', 'it1'],
+                    [
+                        'tdr:broad-datarepo-terra-prod-hca2:snapshot/hca_prod_20201118_dcp1___20201209:',
+                    ]
+                ),
+                (
+                    'lungmap',
+                    ['lungmap', 'it0lungmap'],
+                    [
+                        'tdr:tdr-fp-a02eee6b:snapshot/hca_prod_1bdcecde16be420888f478cd2133d11d__20211013_20211013_lungmap:',
+                        'tdr:tdr-fp-9f58860b:snapshot/hca_prod_00f056f273ff43ac97ff69ca10e38c89__20211004_lungmap_20211005:',
+                        'tdr:tdr-fp-ad968fd8:snapshot/hca_prod_2620497955a349b28d2b53e0bdfcb176__20211012_lungmap_20211013_lungmap:'
+                    ]
+                )
+            ] for i, name in enumerate(names)
         }),
 
         'AZUL_PARTITION_PREFIX_LENGTH': '2',
 
-        'AZUL_TDR_SOURCES': ','.join([
-            'tdr:broad-datarepo-terra-prod-hca2:snapshot/hca_prod_20201118_dcp1___20201209:',
-        ]),
-        **{
-            f'AZUL_TDR_{catalog.upper()}_SOURCES': ','.join([
-                'tdr:tdr-fp-546ade29:snapshot/hca_prod_20201120_dcp2___20210910_dcp9:',
-            ])
-            for catalog in ('dcp9', 'it9')
-        },
-        **{
-            f'AZUL_TDR_{catalog.upper()}_SOURCES': ','.join([
-                'tdr:tdr-fp-43194825:snapshot/hca_prod_20201120_dcp2___20211004_dcp10:'
-            ])
-            for catalog in ('dcp10', 'it10')
-        },
-        **{
-            f'AZUL_TDR_{catalog.upper()}_SOURCES': ','.join([
-                'tdr:tdr-fp-a02eee6b:snapshot/hca_prod_1bdcecde16be420888f478cd2133d11d__20211013_20211013_lungmap:',
-                'tdr:tdr-fp-9f58860b:snapshot/hca_prod_00f056f273ff43ac97ff69ca10e38c89__20211004_lungmap_20211005:',
-                'tdr:tdr-fp-ad968fd8:snapshot/hca_prod_2620497955a349b28d2b53e0bdfcb176__20211012_lungmap_20211013_lungmap:'
-            ])
-            for catalog in ('lungmap', 'it0lungmap')
-        },
         'AZUL_TDR_SOURCE_LOCATION': 'US',
         'AZUL_TDR_SERVICE_URL': 'https://jade-terra.datarepo-prod.broadinstitute.org',
         'AZUL_SAM_SERVICE_URL': 'https://sam.dsde-prod.broadinstitute.org',

scripts/generate_openapi_document.py
@@ -22,7 +22,8 @@ def main():
                                atlas='hca',
                                internal=False,
                                plugins=dict(metadata=config.Catalog.Plugin(name='hca'),
-                                            repository=config.Catalog.Plugin(name='tdr')))
+                                            repository=config.Catalog.Plugin(name='tdr')),
+                               sources=set())
     }
 
     # To create a normalized OpenAPI document, we patch any

scripts/post_deploy.py
@@ -36,7 +36,7 @@ def verify_sources():
         if catalog.plugins[RepositoryPlugin.type_name()].name == 'tdr'
     }
     assert tdr_catalogs, tdr_catalogs
-    for source in set(chain(*map(config.tdr_sources, tdr_catalogs))):
+    for source in set(chain(*map(config.sources, tdr_catalogs))):
         source = TDRSourceSpec.parse(source)
         verify_source(source)
 

src/azul/__init__.py
@@ -189,19 +189,8 @@ class Config:
     def dss_endpoint(self) -> Optional[str]:
         return self.environ.get('AZUL_DSS_ENDPOINT')
 
-    def canned_sources(self, catalog: CatalogName) -> AbstractSet[str]:
-        try:
-            sources = self.environ[f'azul_canned_{catalog.lower()}_sources']
-        except KeyError:
-            sources = self.environ['azul_canned_sources']
-        return frozenset(sources.split(','))
-
-    def tdr_sources(self, catalog: CatalogName) -> AbstractSet[str]:
-        try:
-            sources = self.environ[f'AZUL_TDR_{catalog.upper()}_SOURCES']
-        except KeyError:
-            sources = self.environ['AZUL_TDR_SOURCES']
-        return frozenset(sources.split(','))
+    def sources(self, catalog: CatalogName) -> AbstractSet[str]:
+        return config.catalogs[catalog].sources
 
     @property
     def tdr_source_location(self) -> str:
@@ -529,6 +518,7 @@ class Config:
         atlas: str
         internal: bool
         plugins: Mapping[str, Plugin]
+        sources: set
 
         _it_catalog_re: ClassVar[re.Pattern] = re.compile(r'it[\d]+')
 
@@ -560,7 +550,8 @@ class Config:
             return cls(name=name,
                        atlas=spec['atlas'],
                        internal=spec['internal'],
-                       plugins=plugins)
+                       plugins=plugins,
+                       sources=set(spec['sources']))
 
     @cached_property
     def catalogs(self) -> Mapping[CatalogName, Catalog]:

src/azul/plugins/repository/canned/__init__.py
@@ -79,7 +79,7 @@ class Plugin(RepositoryPlugin[SimpleSourceSpec, CannedSourceRef]):
         return cls(
             frozenset(
                 SimpleSourceSpec.parse(name).effective
-                for name in config.canned_sources(catalog)
+                for name in config.sources(catalog)
             )
         )
 

commit b749ea876b02a4a84379b6c145fa3a2f2cc41751

Fix: EBNF for sources was deleted (#3562)
Regression from: 8c61677f

environment.py
@@ -56,6 +56,32 @@ def env() -> Mapping[str, Optional[str]]:
         #
         # The first catalog listed is the default catalog.
         #
+        # A source represents a TDR dataset, TDR snapshot, or canned staging
+        # area to index. Each source is a string matching the following EBNF grammar:
+        #
+        # source = TDR source | canned source ;
+        #
+        # TDR source = 'tdr:', Google Cloud project name,
+        #              ':', ( 'dataset' | 'snapshot' ),
+        #              '/', TDR dataset or snapshot name,
+        #              ':', [ UUID prefix [ '/', Partition prefix length ] ] ;
+        #
+        # canned source = 'https://github.com',
+        #                 '/', owner,
+        #                 '/', repo,
+        #                 '/tree/', ref,
+        #                 ['/', path] ;
+        #                 ':', [ UUID prefix [ '/', Partition prefix length ] ] ;
+        #
+        # `ref` can be a branch, tag, or commit SHA. If `ref` contains special
+        # characters like `/`, '?` or `#` they must be URL-encoded.
+        #
+        # Examples:
+        #
+        # tdr:broad-jade-dev-data:snapshot/hca_mvp:2
+        # tdr:broad-jade-dev-data:dataset/hca_mvp:2/1
+        # https://github.com/HumanCellAtlas/schema-test-data/tree/de355ca/tests:2
+        #
         'AZUL_CATALOGS': None,
 
         # The Account ID number for AWS