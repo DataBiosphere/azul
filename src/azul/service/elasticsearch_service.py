DataBiosphere/azul

commit 16d2d75996a1856a13dd6ffca56dba4daa9c24f5

Generalize index field translation

src/azul/service/elasticsearch_service.py
@@ -45,9 +45,6 @@ from azul import (
 from azul.es import (
     ESClientFactory,
 )
-from azul.indexer.document import (
-    Document,
-)
 from azul.indexer.document_service import (
     DocumentService,
 )
@@ -126,7 +123,10 @@ class ElasticsearchService(DocumentService, AbstractService):
             assert isinstance(value, dict)
             assert isinstance(one(value.values()), list)
             field_type = self.field_type(catalog, tuple(key.split('.')))
-            value = {key: [Document.translate_field(v, field_type) for v in val] for key, val in value.items()}
+            value = {
+                key: [field_type.to_index(v) for v in val]
+                for key, val in value.items()
+            }
             translated_filters[key] = value
         return translated_filters
 
@@ -146,7 +146,7 @@ class ElasticsearchService(DocumentService, AbstractService):
             if relation == 'is':
                 query = Q('terms', **{facet + '.keyword': value})
                 field_type = self.field_type(catalog, tuple(facet.split('.')))
-                translated_none = Document.translate_field(None, field_type)
+                translated_none = field_type.to_index(None)
                 if translated_none in value:
                     # Note that at this point None values in filters have already
                     # been translated eg. {'is': ['~null']} and if the filter has a
@@ -244,7 +244,7 @@ class ElasticsearchService(DocumentService, AbstractService):
             if isinstance(agg, FieldBucketData):
                 field_type = self.field_type(catalog, tuple(agg.meta['path']))
                 for bucket in agg:
-                    bucket['key'] = Document.translate_field(bucket['key'], field_type, forward=False)
+                    bucket['key'] = field_type.from_index(bucket['key'])
                     translate(bucket)
             elif isinstance(agg, BucketData):
                 for attr in dir(agg):

src/azul/indexer/document.py
@@ -25,9 +25,6 @@ from typing import (
 )
 
 import attr
-from humancellatlas.data.metadata import (
-    api,
-)
 
 from azul import (
     CatalogName,
@@ -41,6 +38,7 @@ from azul.types import (
     AnyJSON,
     AnyMutableJSON,
     JSON,
+    JSONs,
 )
 
 EntityID = str
@@ -205,7 +203,91 @@ class AggregateCoordinates(DocumentCoordinates[CataloguedEntityReference]):
         return f'aggregate for {self.entity}'
 
 
-FieldType = Union[type, None]
+N = TypeVar('N')
+
+T = TypeVar('T', bound=AnyJSON)
+
+
+class FieldType(Generic[N, T], metaclass=ABCMeta):
+    shadowed: bool = False
+
+    @abstractmethod
+    def to_index(self, value: N) -> T:
+        raise NotImplementedError()
+
+    @abstractmethod
+    def from_index(self, value: T) -> N:
+        raise NotImplementedError()
+
+
+class PassThrough(Generic[T], FieldType[T, T]):
+
+    def to_index(self, value: T) -> T:
+        return value
+
+    def from_index(self, value: T) -> T:
+        return value
+
+
+pass_thru_str: PassThrough[str] = PassThrough()
+pass_thru_int: PassThrough[int] = PassThrough()
+pass_thru_bool: PassThrough[bool] = PassThrough()
+pass_thru_json: PassThrough[JSON] = PassThrough()
+pass_thru_jsons: PassThrough[JSONs] = PassThrough()
+
+
+class NullableString(FieldType[Optional[str], str]):
+    # Note that the replacement values for `None` used for each data type
+    # ensure that `None` values are placed at the end of a sorted list.
+    null_string = '~null'
+
+    def to_index(self, value: Optional[str]) -> str:
+        return self.null_string if value is None else value
+
+    def from_index(self, value: str) -> Optional[str]:
+        return None if value == self.null_string else value
+
+
+null_str = NullableString()
+
+Number = Union[float, int]
+
+N_ = TypeVar('N_', bound=Number)
+
+
+class NullableNumber(Generic[N_], FieldType[Optional[N_], Number]):
+    shadowed = True
+    # Maximum int that can be represented as a 64-bit int and double IEEE
+    # floating point number. This prevents loss when converting between the two.
+    null_int = sys.maxsize - 1023
+    assert null_int == int(float(null_int))
+
+    def to_index(self, value: Optional[N_]) -> Number:
+        return self.null_int if value is None else value
+
+    def from_index(self, value: Number) -> Optional[N_]:
+        return None if value == self.null_int else value
+
+
+null_int: NullableNumber[int] = NullableNumber()
+
+null_float: NullableNumber[float] = NullableNumber()
+
+
+class NullableBool(NullableNumber[bool]):
+    shadowed = False
+
+    def to_index(self, value: Optional[bool]) -> Number:
+        value = {False: 0, True: 1, None: None}[value]
+        return super().to_index(value)
+
+    def from_index(self, value: Number) -> Optional[bool]:
+        value = super().from_index(value)
+        return {0: False, 1: True, None: None}[value]
+
+
+null_bool: NullableBool = NullableBool()
+
 FieldTypes4 = Union[Mapping[str, FieldType], FieldType]
 FieldTypes3 = Union[Mapping[str, FieldTypes4], FieldType]
 FieldTypes2 = Union[Mapping[str, FieldTypes3], FieldType]
@@ -246,60 +328,10 @@ class Document(Generic[C]):
     @classmethod
     def field_types(cls, field_types: FieldTypes) -> FieldTypes:
         return {
-            'entity_id': str,
-            'entity_version': str,
+            'entity_id': null_str,
+            'entity_version': null_str,
             'contents': field_types
         }
-
-    @classmethod
-    def translate_field(cls, value: AnyJSON, field_type: FieldType, forward: bool = True) -> Any:
-        """
-        Translate a single value for insert into or after fetching from Elasticsearch.
-
-        :param value: A value to translate
-        :param field_type: The type of the field value
-        :param forward: If we should translate forward or backward (aka un-translate)
-        :return: The translated value
-        """
-        # Note that the replacement values for `None` used for each data type
-        # ensure that `None` values are placed at the end of a sorted list.
-        null_string = '~null'
-        # Maximum int that can be represented as a 64-bit int and double IEEE
-        # floating point number. This prevents loss when converting between the two.
-        null_int = sys.maxsize - 1023
-        assert null_int == int(float(null_int))
-        bool_translation_forward = {False: 0, True: 1, None: null_int}
-        bool_translation_backward = {0: False, 1: True, null_int: None}
-
-        if field_type is bool:
-            if forward:
-                return bool_translation_forward[value]
-            else:
-                return bool_translation_backward[value]
-        elif field_type is int or field_type is float:
-            if forward:
-                if value is None:
-                    return null_int
-            else:
-                if value == null_int:
-                    return None
-            return value
-        elif field_type is str:
-            if forward:
-                if value is None:
-                    return null_string
-            else:
-                if value == null_string:
-                    return None
-            return value
-        elif field_type is dict:
-            return value
-        elif field_type is api.UUID4:
-            return value
-        elif field_type is None:
-            return value
-        else:
-            raise ValueError(f'Unknown field_type value {field_type}')
 
     @classmethod
     def translate_fields(cls,
@@ -329,39 +361,51 @@ class Document(Generic[C]):
         :return: A copy of the original document with values translated
                  according to their type.
         """
-        if field_types is None:
-            return doc
-        elif isinstance(doc, dict):
-            new_dict = {}
-            for key, val in doc.items():
-                # Shadow copy fields should only be present during a reverse translation and we skip over to remove them
-                if key.endswith('_'):
-                    assert not forward
-                else:
-                    try:
-                        field_type = field_types[key]
-                    except KeyError:
-                        raise KeyError(f'Key {key} not defined in field_types')
-                    except TypeError:
-                        raise TypeError(f'Key {key} not defined in field_types')
-                    new_dict[key] = cls.translate_fields(val, field_type, forward=forward, path=(*path, key))
-                    if forward and field_type in (int, float):
-                        # Add a non-translated shadow copy of this field's numeric value for sum aggregations
-                        new_dict[key + '_'] = val
-            return new_dict
-        elif isinstance(doc, list):
-            # Translate an empty list to a list containing a single None value
-            # (and then further translate that None value according to the field
-            # type), but do so only at the field level (to avoid case of
-            # contents['organoids'] == []).
-            if doc or isinstance(field_types, dict):
+        if isinstance(field_types, dict):
+            if isinstance(doc, dict):
+                new_doc = {}
+                for key, val in doc.items():
+                    if key.endswith('_'):
+                        # Shadow copy fields should only be present during a reverse
+                        # translation and we skip over to remove them.
+                        assert not forward, path
+                    else:
+                        try:
+                            field_type = field_types[key]
+                        except KeyError:
+                            raise KeyError(f'Key {key!r} not defined in field_types')
+                        except TypeError:
+                            raise TypeError(f'Key {key!r} not defined in field_types')
+                        new_doc[key] = cls.translate_fields(val, field_type, forward=forward, path=(*path, key))
+                        if forward and isinstance(field_type, FieldType) and field_type.shadowed:
+                            # Add a non-translated shadow copy of this field's
+                            # numeric value for sum aggregations
+                            new_doc[key + '_'] = val
+                return new_doc
+            elif isinstance(doc, list):
                 return [cls.translate_fields(val, field_types, forward=forward, path=path) for val in doc]
             else:
-                assert len(doc) == 0 and isinstance(doc, list) and isinstance(field_types, type)
-                assert forward, path
-                return cls.translate_fields([None], field_types, forward=forward, path=path)
+                assert False, (path, type(doc))
+        elif isinstance(field_types, FieldType):
+            field_type = field_types
+            if forward:
+                if isinstance(doc, list):
+                    if not doc:
+                        # Translate an empty list to a list containing a single
+                        # None value (and then further translate that None value
+                        # according to the field type) so ES doesn't discard it.
+                        doc = [None]
+                    return [field_type.to_index(value) for value in doc]
+                else:
+                    return field_type.to_index(doc)
+            else:
+                if isinstance(doc, list):
+                    assert doc, path  # ES discards empty lists
+                    return [field_type.from_index(value) for value in doc]
+                else:
+                    return field_type.from_index(doc)
         else:
-            return cls.translate_field(doc, field_types, forward=forward)
+            assert False, (path, type(field_types))
 
     def to_source(self) -> JSON:
         return dict(entity_id=self.coordinates.entity.entity_id,
@@ -480,9 +524,10 @@ class Contribution(Document[ContributionCoordinates[E]]):
     def field_types(cls, field_types: FieldTypes) -> FieldTypes:
         return {
             **super().field_types(field_types),
-            'bundle_uuid': None,  # No translation needed on this str field, field will never be None
-            'bundle_version': None,  # No translation needed on this str field, field will never be None
-            'bundle_deleted': None,  # No translation needed on this bool field, field will never be None
+            # These pass-through fields will never be None
+            'bundle_uuid': pass_thru_str,
+            'bundle_version': pass_thru_str,
+            'bundle_deleted': pass_thru_bool
         }
 
     @classmethod
@@ -512,8 +557,8 @@ class Aggregate(Document[AggregateCoordinates]):
     def field_types(cls, field_types: FieldTypes) -> FieldTypes:
         return {
             **super().field_types(field_types),
-            'num_contributions': None,  # Exclude count value field from translations, field will never be None
-            'bundles': None  # Exclude bundle uuid and version values from translations
+            'num_contributions': pass_thru_int,
+            'bundles': pass_thru_jsons
         }
 
     @classmethod

src/azul/plugins/metadata/hca/transform.py
@@ -42,6 +42,12 @@ from azul.indexer.document import (
     ContributionCoordinates,
     EntityReference,
     FieldTypes,
+    PassThrough,
+    null_bool,
+    null_int,
+    null_str,
+    pass_thru_int,
+    pass_thru_json,
 )
 from azul.indexer.transform import (
     Transformer,
@@ -70,6 +76,8 @@ log = logging.getLogger(__name__)
 Sample = Union[api.CellLine, api.Organoid, api.SpecimenFromOrganism]
 sample_types = api.CellLine, api.Organoid, api.SpecimenFromOrganism
 assert Sample.__args__ == sample_types  # since we can't use * in generic types
+
+pass_thru_uuid4: PassThrough[api.UUID4] = PassThrough()
 
 
 class BaseTransformer(Transformer, metaclass=ABCMeta):
@@ -137,12 +145,12 @@ class BaseTransformer(Transformer, metaclass=ABCMeta):
     @classmethod
     def _contact_types(cls) -> FieldTypes:
         return {
-            'contact_name': str,
-            'corresponding_contributor': bool,
-            'email': str,
-            'institution': str,
-            'laboratory': str,
-            'project_role': str
+            'contact_name': null_str,
+            'corresponding_contributor': null_bool,
+            'email': null_str,
+            'institution': null_str,
+            'laboratory': null_str,
+            'project_role': null_str
         }
 
     def _contact(self, p: api.ProjectContact):
@@ -159,8 +167,8 @@ class BaseTransformer(Transformer, metaclass=ABCMeta):
     @classmethod
     def _publication_types(cls) -> FieldTypes:
         return {
-            'publication_title': str,
-            'publication_url': str
+            'publication_title': null_str,
+            'publication_url': null_str
         }
 
     def _publication(self, p: api.ProjectPublication):
@@ -173,22 +181,22 @@ class BaseTransformer(Transformer, metaclass=ABCMeta):
     @classmethod
     def _project_types(cls) -> FieldTypes:
         return {
-            'project_title': str,
-            'project_description': str,
-            'project_short_name': str,
-            'laboratory': str,
-            'institutions': str,
-            'contact_names': str,
+            'project_title': null_str,
+            'project_description': null_str,
+            'project_short_name': null_str,
+            'laboratory': null_str,
+            'institutions': null_str,
+            'contact_names': null_str,
             'contributors': cls._contact_types(),
-            'document_id': str,
-            'publication_titles': str,
+            'document_id': null_str,
+            'publication_titles': null_str,
             'publications': cls._publication_types(),
-            'insdc_project_accessions': str,
-            'geo_series_accessions': str,
-            'array_express_accessions': str,
-            'insdc_study_accessions': str,
-            'supplementary_links': str,
-            '_type': str
+            'insdc_project_accessions': null_str,
+            'geo_series_accessions': null_str,
+            'array_express_accessions': null_str,
+            'insdc_study_accessions': null_str,
+            'supplementary_links': null_str,
+            '_type': null_str
         }
 
     def _project(self, project: api.Project) -> MutableJSON:
@@ -237,16 +245,16 @@ class BaseTransformer(Transformer, metaclass=ABCMeta):
     @classmethod
     def _specimen_types(cls) -> FieldTypes:
         return {
-            'has_input_biomaterial': str,
-            '_source': str,
-            'document_id': str,
-            'biomaterial_id': str,
-            'disease': str,
-            'organ': str,
-            'organ_part': str,
-            'storage_method': str,
-            'preservation_method': str,
-            '_type': str
+            'has_input_biomaterial': null_str,
+            '_source': null_str,
+            'document_id': null_str,
+            'biomaterial_id': null_str,
+            'disease': null_str,
+            'organ': null_str,
+            'organ_part': null_str,
+            'storage_method': null_str,
+            'preservation_method': null_str,
+            '_type': null_str
         }
 
     def _specimen(self, specimen: api.SpecimenFromOrganism) -> MutableJSON:
@@ -266,12 +274,12 @@ class BaseTransformer(Transformer, metaclass=ABCMeta):
     @classmethod
     def _cell_suspension_types(cls) -> FieldTypes:
         return {
-            'document_id': str,
-            'biomaterial_id': str,
-            'total_estimated_cells': int,
-            'selected_cell_type': str,
-            'organ': str,
-            'organ_part': str
+            'document_id': null_str,
+            'biomaterial_id': null_str,
+            'total_estimated_cells': null_int,
+            'selected_cell_type': null_str,
+            'organ': null_str,
+            'organ_part': null_str
         }
 
     def _cell_suspension(self, cell_suspension: api.CellSuspension) -> MutableJSON:
@@ -304,10 +312,10 @@ class BaseTransformer(Transformer, metaclass=ABCMeta):
     @classmethod
     def _cell_line_types(cls) -> FieldTypes:
         return {
-            'document_id': str,
-            'biomaterial_id': str,
-            'cell_line_type': str,
-            'model_organ': str
+            'document_id': null_str,
+            'biomaterial_id': null_str,
+            'cell_line_type': null_str,
+            'model_organ': null_str
         }
 
     def _cell_line(self, cell_line: api.CellLine) -> MutableJSON:
@@ -322,15 +330,17 @@ class BaseTransformer(Transformer, metaclass=ABCMeta):
     @classmethod
     def _donor_types(cls) -> FieldTypes:
         return {
-            'document_id': str,
-            'biomaterial_id': str,
-            'biological_sex': str,
-            'genus_species': str,
-            'diseases': str,
-            'organism_age': str,
-            'organism_age_unit': str,
-            'organism_age_range': None,  # Exclude ranged values from translation, prevents problem due to shadow copies
-            'donor_count': None  # Exclude this field added by DonorOrganismAggregator from translation
+            'document_id': null_str,
+            'biomaterial_id': null_str,
+            'biological_sex': null_str,
+            'genus_species': null_str,
+            'diseases': null_str,
+            'organism_age': null_str,
+            'organism_age_unit': null_str,
+            # Prevent problem due to shadow copies on numeric ranges
+            'organism_age_range': pass_thru_json,
+            # Pass through field added by DonorOrganismAggregator
+            'donor_count': pass_thru_int
         }
 
     def _donor(self, donor: api.DonorOrganism) -> MutableJSON:
@@ -356,10 +366,10 @@ class BaseTransformer(Transformer, metaclass=ABCMeta):
     @classmethod
     def _organoid_types(cls) -> FieldTypes:
         return {
-            'document_id': str,
-            'biomaterial_id': str,
-            'model_organ': str,
-            'model_organ_part': str
+            'document_id': null_str,
+            'biomaterial_id': null_str,
+            'model_organ': null_str,
+            'model_organ_part': null_str
         }
 
     def _organoid(self, organoid: api.Organoid) -> MutableJSON:
@@ -373,22 +383,23 @@ class BaseTransformer(Transformer, metaclass=ABCMeta):
     @classmethod
     def _file_types(cls) -> FieldTypes:
         return {
-            'content-type': str,
-            'indexed': bool,
-            'name': str,
-            'crc32c': str,
-            'sha256': str,
-            'size': int,
-            'count': None,  # Exclude this field added by FileAggregator from translation, field will never be None
-            'uuid': api.UUID4,
-            'version': str,
-            'document_id': str,
-            'file_format': str,
-            'content_description': str,
-            '_type': str,
+            'content-type': null_str,
+            'indexed': null_bool,
+            'name': null_str,
+            'crc32c': null_str,
+            'sha256': null_str,
+            'size': null_int,
+            # Pass through field added by FileAggregator, will never be None
+            'count': pass_thru_int,
+            'uuid': pass_thru_uuid4,
+            'version': null_str,
+            'document_id': null_str,
+            'file_format': null_str,
+            'content_description': null_str,
+            '_type': null_str,
             'related_files': cls._related_file_types(),
-            'read_index': str,
-            'lane_index': int
+            'read_index': null_str,
+            'lane_index': null_int
         }
 
     def _file(self, file: api.File, related_files: Iterable[api.File] = ()) -> MutableJSON:
@@ -419,12 +430,12 @@ class BaseTransformer(Transformer, metaclass=ABCMeta):
     @classmethod
     def _related_file_types(cls) -> FieldTypes:
         return {
-            'name': str,
-            'crc32c': str,
-            'sha256': str,
-            'size': int,
-            'uuid': api.UUID4,
-            'version': str,
+            'name': null_str,
+            'crc32c': null_str,
+            'sha256': null_str,
+            'size': null_int,
+            'uuid': pass_thru_uuid4,
+            'version': null_str,
         }
 
     def _related_file(self, file: api.File) -> MutableJSON:
@@ -440,12 +451,13 @@ class BaseTransformer(Transformer, metaclass=ABCMeta):
     @classmethod
     def _protocol_types(cls) -> FieldTypes:
         return {
-            'document_id': str,
-            'library_construction_approach': str,
-            'instrument_manufacturer_model': str,
-            'paired_end': bool,
-            'workflow': str,
-            'assay_type': None  # Exclude counter dict used to produce a FrequencySetAccumulator from translation
+            'document_id': null_str,
+            'library_construction_approach': null_str,
+            'instrument_manufacturer_model': null_str,
+            'paired_end': null_bool,
+            'workflow': null_str,
+            # Pass through counter used to produce a FrequencySetAccumulator
+            'assay_type': pass_thru_json
         }
 
     def _protocol(self, protocol: api.Protocol) -> MutableJSON:
@@ -469,7 +481,7 @@ class BaseTransformer(Transformer, metaclass=ABCMeta):
     @classmethod
     def _sequencing_process_types(cls) -> FieldTypes:
         return {
-            'document_id': str,
+            'document_id': null_str,
         }
 
     def _sequencing_process(self, process: api.Process) -> MutableJSON:
@@ -480,8 +492,8 @@ class BaseTransformer(Transformer, metaclass=ABCMeta):
     @classmethod
     def _sample_types(cls) -> FieldTypes:
         return {
-            'entity_type': str,
-            'effective_organ': str,
+            'entity_type': null_str,
+            'effective_organ': null_str,
             **cls._cell_line_types(),
             **cls._organoid_types(),
             **cls._specimen_types()
@@ -801,5 +813,5 @@ class BundleTransformer(BundleProjectTransformer):
     def field_types(cls) -> FieldTypes:
         return {
             **super().field_types(),
-            'metadata': None  # Exclude full metadata from translation
+            'metadata': pass_thru_json  # Exclude full metadata from translation
         }

test/indexer/test_hca_indexer.py
@@ -70,8 +70,10 @@ from azul.indexer.document import (
     CataloguedEntityReference,
     Contribution,
     ContributionCoordinates,
-    Document,
     EntityReference,
+    null_bool,
+    null_int,
+    null_str,
 )
 from azul.indexer.index_service import (
     IndexWriter,
@@ -126,11 +128,11 @@ class TestHCAIndexer(IndexerTestCase):
     old_bundle = BundleFQID('aaa96233-bf27-44c7-82df-b4dc15ad4d9d', '2018-11-02T113344.698028Z')
     new_bundle = BundleFQID('aaa96233-bf27-44c7-82df-b4dc15ad4d9d', '2018-11-04T113344.698028Z')
 
-    translated_str_null = Document.translate_field(None, str)
-    translated_int_null = Document.translate_field(None, int)
-    translated_bool_null = Document.translate_field(None, bool)
-    translated_bool_true = Document.translate_field(True, bool)
-    translated_bool_false = Document.translate_field(False, bool)
+    translated_str_null = null_str.to_index(None)
+    translated_int_null = null_int.to_index(None)
+    translated_bool_null = null_bool.to_index(None)
+    translated_bool_true = null_bool.to_index(True)
+    translated_bool_false = null_bool.to_index(False)
 
     def test_indexing(self):
         """

test/service/test_response.py
@@ -27,7 +27,7 @@ from azul.indexer import (
     BundleFQID,
 )
 from azul.indexer.document import (
-    Document,
+    null_str,
 )
 from azul.indexer.index_service import (
     IndexService,
@@ -1262,7 +1262,7 @@ class TestResponse(WebServiceTestCase):
 
         for facet in facets.values():
             for term in facet['terms']:
-                self.assertNotEqual(term['term'], Document.translate_field(None, str))
+                self.assertNotEqual(term['term'], null_str.to_index(None))
 
     def test_sample(self):
         """

commit 098ff20b5efd216cf087e7bcbbf441dfa1ed792a

Fix: Translate filters only works with 'is' operator filters
Regression from 16d2d75996a1856a13dd6ffca56dba4daa9c24f5

src/azul/service/elasticsearch_service.py
@@ -122,18 +122,18 @@ class ElasticsearchService(DocumentService, AbstractService):
         :return: Returns translated filters with 'es_keys'
         """
         translated_filters = {}
-        for key, value in filters.items():
-            # Replace the key in the filter with the name within ElasticSearch
-            key = field_mapping[key]
-            # Replace the value in the filter with the value translated for None values
-            assert isinstance(value, dict)
-            assert isinstance(one(value.values()), list)
-            field_type = self.field_type(catalog, tuple(key.split('.')))
-            value = {
-                key: [field_type.to_index(v) for v in val]
-                for key, val in value.items()
+        for field, filter in filters.items():
+            field = field_mapping[field]
+            operator, values = one(filter.items())
+            field_type = self.field_type(catalog, tuple(field.split('.')))
+            to_index = field_type.to_index
+            filter = {
+                operator:
+                    [(to_index(start), to_index(end)) for start, end in values]
+                    if operator in ('contains', 'within', 'intersects') else
+                    list(map(to_index, values))
             }
-            translated_filters[key] = value
+            translated_filters[field] = filter
         return translated_filters
 
     def _create_query(self, catalog: CatalogName, filters):