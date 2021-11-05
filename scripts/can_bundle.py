DataBiosphere/azul

commit fc1a0df98e0aca59e5a4642964f57aa58cb7a023

Fix: catalog source string not sufficient for can_bundle.py script (#3436)

scripts/can_bundle.py
@@ -68,15 +68,21 @@ def fetch_bundle(source: str, bundle_uuid: str, bundle_version: str) -> Bundle:
     for catalog in config.catalogs:
         plugin = plugin_for(catalog)
         sources = set(map(str, plugin.sources))
-        if source in sources:
-            source = plugin.resolve_source(source)
-            fqid = SourcedBundleFQID(source=source,
-                                     uuid=bundle_uuid,
-                                     version=bundle_version)
-            bundle = plugin.fetch_bundle(fqid)
-            logger.info('Fetched bundle %r version %r from catalog %r.',
-                        fqid.uuid, fqid.version, catalog)
-            return bundle
+        try:
+            parsed_source = plugin.resolve_source(source)
+        except Exception:
+            pass
+        else:
+            for configured_source in sources:
+                configured_source = plugin.resolve_source(configured_source)
+                if configured_source.spec.contains(parsed_source.spec):
+                    fqid = SourcedBundleFQID(source=configured_source,
+                                             uuid=bundle_uuid,
+                                             version=bundle_version)
+                    bundle = plugin.fetch_bundle(fqid)
+                    logger.info('Fetched bundle %r version %r from catalog %r.',
+                                fqid.uuid, fqid.version, catalog)
+                    return bundle
     raise ValueError('No repository using this source')
 
 

src/azul/indexer/__init__.py
@@ -200,6 +200,25 @@ class SourceSpec(ABC, Generic[SOURCE_SPEC]):
     def effective(self) -> SOURCE_SPEC:
         return attr.evolve(self, prefix=self.prefix.effective)
 
+    def contains(self, other: 'SourceSpec') -> bool:
+        """
+        >>> p = SimpleSourceSpec.parse
+
+        >>> p('foo:4').contains(p('foo:42'))
+        True
+
+        >>> p('foo:42').contains(p('foo:4'))
+        False
+
+        >>> p('foo:42').contains(p('foo:42'))
+        True
+
+        >>> p('foo:1').contains(p('foo:2'))
+        False
+        """
+        assert isinstance(other, SourceSpec), (self, other)
+        return other.prefix.common.startswith(self.prefix.common)
+
 
 @attr.s(frozen=True, auto_attribs=True, kw_only=True)
 class SimpleSourceSpec(SourceSpec['SimpleSourceSpec']):
@@ -254,6 +273,22 @@ class SimpleSourceSpec(SourceSpec['SimpleSourceSpec']):
         True
         """
         return f'{self.name}:{self.prefix}'
+
+    def contains(self, other: 'SourceSpec') -> bool:
+        """
+        >>> p = SimpleSourceSpec.parse
+
+        >>> p('foo:').contains(p('foo:'))
+        True
+
+        >>> p('foo:').contains(p('bar:'))
+        False
+        """
+        return (
+            isinstance(other, SimpleSourceSpec)
+            and super().contains(other)
+            and self.name == other.name
+        )
 
 
 SOURCE_REF = TypeVar('SOURCE_REF', bound='SourceRef')

src/azul/terra.py
@@ -198,6 +198,30 @@ class TDRSourceSpec(SourceSpec):
     def qualify_table(self, table_name: str) -> str:
         return '.'.join((self.project, self.bq_name, table_name))
 
+    def contains(self, other: 'SourceSpec') -> bool:
+        """
+        >>> p = TDRSourceSpec.parse
+
+        >>> p('tdr:foo:snapshot/bar:').contains(p('tdr:foo:snapshot/bar:'))
+        True
+
+        >>> p('tdr:foo:snapshot/bar:').contains(p('tdr:bar:snapshot/bar:'))
+        False
+
+        >>> p('tdr:foo:snapshot/bar:').contains(p('tdr:foo:dataset/bar:'))
+        False
+
+        >>> p('tdr:foo:snapshot/bar:').contains(p('tdr:foo:snapshot/baz:'))
+        False
+        """
+        return (
+            isinstance(other, TDRSourceSpec)
+            and super().contains(other)
+            and self.is_snapshot == other.is_snapshot
+            and self.project == other.project
+            and self.name == other.name
+        )
+
 
 class CredentialsProvider(ABC):
 

commit 10d0ec6ae5be04b9202f622cdee338b7349b39c0

Fix: Use parsed_source.contains in scripts/can_bundle.py (#3436)
Regression from: fc1a0df9

scripts/can_bundle.py
@@ -75,7 +75,7 @@ def fetch_bundle(source: str, bundle_uuid: str, bundle_version: str) -> Bundle:
         else:
             for configured_source in sources:
                 configured_source = plugin.resolve_source(configured_source)
-                if configured_source.spec.contains(parsed_source.spec):
+                if parsed_source.spec.contains(configured_source.spec):
                     fqid = SourcedBundleFQID(source=configured_source,
                                              uuid=bundle_uuid,
                                              version=bundle_version)