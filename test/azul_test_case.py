8f7bd59e8bcc2bdc55a335462a0f37c0330f3b08
amar jandu <ajandu@ucsc.edu>
2022-02-17 14:14:53 -0800

    [r] Remove `hits.projects.{foo}Accessions` from response (#3334)

--- test/azul_test_case.py
+++ test/azul_test_case.py
@@ -97,10 +97,6 @@ class AzulTestCase(TestCase):
                     "'collections.abc' is deprecated since Python 3.3, and in 3.9 "
                     "it will stop working"
                 ),
-
-                # FIXME: Remove with fix for #3334
-                #        https://github.com/DataBiosphere/azul/issues/3334
-                RE('.*Use Project.accessions instead.')
             },
             UserWarning: {
                 'https://github.com/DataBiosphere/azul/issues/2114',
