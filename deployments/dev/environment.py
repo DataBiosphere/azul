0440303c578a4cadc88da631345c030368975285
Noah Aviel Dove <nadove@ucsc.edu>
2022-03-31 19:27:35 -0700

    [r] Add catalogs for LungMAP to dev

--- deployments/dev/environment.py
+++ deployments/dev/environment.py
@@ -149,6 +149,17 @@ dcp2_sources = mkdict([
     mksrc('datarepo-dev-96d8e08c', 'hca_dev_faeedcb0e0464be7b1ad80a3eeabb066__20210831_20210903', 62),
 ])
 
+lungmap_sources = mkdict([
+    mksrc('datarepo-dev-5d9526e0', 'lungmap_dev_1bdcecde16be420888f478cd2133d11d__20220401_20220404', 1),
+    mksrc('datarepo-dev-8de6d66b', 'lungmap_dev_2620497955a349b28d2b53e0bdfcb176__20220404_20220404', 1)
+])
+
+lm2_sources = dict(**lungmap_sources, **mkdict([
+    mksrc('datarepo-dev-b47b6759', 'lungmap_dev_00f056f273ff43ac97ff69ca10e38c89__20220404_20220404_lm2', 1),
+    mksrc('datarepo-dev-2e9ef7fd', 'lungmap_dev_20037472ea1d4ddb9cd356a11a6f0f76__20220401_20220404_lm2', 1),
+    mksrc('datarepo-dev-2ebe1e7d', 'lungmap_dev_f899709cae2c4bb988f0131142e6c7ec__20220401_20220404_lm2', 1)
+]))
+
 
 def env() -> Mapping[str, Optional[str]]:
     """
@@ -180,11 +191,16 @@ def env() -> Mapping[str, Optional[str]]:
         'AZUL_DEPLOYMENT_STAGE': 'dev',
 
         'AZUL_CATALOGS': json.dumps({
-            f'dcp2{suffix}': dict(atlas='hca',
-                                  internal=internal,
-                                  plugins=dict(metadata=dict(name='hca'),
-                                               repository=dict(name='tdr')),
-                                  sources=list(filter(None, dcp2_sources.values())))
+            f'{catalog}{suffix}': dict(atlas=atlas,
+                                       internal=internal,
+                                       plugins=dict(metadata=dict(name='hca'),
+                                                    repository=dict(name='tdr')),
+                                       sources=list(filter(None, sources.values())))
+            for atlas, catalog, sources in [
+                ('hca', 'dcp2', dcp2_sources),
+                ('lungmap', 'lungmap', lungmap_sources),
+                ('lungmap', 'lm2', lm2_sources)
+            ]
             for suffix, internal in [
                 ('', False),
                 ('-it', True)
