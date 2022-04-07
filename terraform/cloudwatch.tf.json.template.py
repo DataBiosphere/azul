1acd5957ce70f4336e662329c538aca717007920
Noah Dove <nadove@ucsc.edu>
2022-04-07 15:41:22 -0700

    Create CloudWatch metric filter for number of 5xx errors (#4009)

--- terraform/cloudwatch.tf.json.template.py
+++ terraform/cloudwatch.tf.json.template.py
@@ -1,5 +1,27 @@
+from azul import (
+    config,
+)
 from azul.terraform import (
     emit_tf,
 )
 
-emit_tf(None)
+emit_tf(None if config.disable_monitoring else {
+    "resource": [
+        {
+            "aws_cloudwatch_log_metric_filter": {
+                f"azul-{lambda_}-5xx": {
+                    "name": config.qualified_resource_name(lambda_ + '_5xx'),
+                    "pattern": "{ $.status = 5* }",
+                    "log_group_name": "${aws_cloudwatch_log_group.%s.name}" % lambda_,
+                    "metric_transformation": {
+                        "name": config.qualified_resource_name(lambda_ + '_5xx'),
+                        "namespace": config.qualified_resource_name('metrics'),
+                        "value": 1,
+                        "unit": "Count"
+                    }
+                }
+            }
+        }
+        for lambda_ in config.lambda_names()
+    ]
+})
