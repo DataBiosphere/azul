da44c07f65f5da0a6e672ab1c1604d762fd85332
Noah Dove <nadove@ucsc.edu>
2022-04-13 13:45:05 -0700

    Work around IT failure due to TDR response timeout (#4083)

--- test/integration_test.py
+++ test/integration_test.py
@@ -211,8 +211,11 @@ class IntegrationTestCase(AzulTestCase, metaclass=ABCMeta):
         self.assertFalse(tdr.is_registered(),
                          f'The "unregistered" service account ({email!r}) has '
                          f'been registered')
-        # The unregistered service account should not have access to any sources
-        self.assertRaises(RequirementError, tdr.snapshot_names_by_id)
+        # FIXME: Re-enable once TDR timeout issues are resolved
+        #        https://github.com/DataBiosphere/azul/issues/4092
+        if False:
+            # The unregistered service account should not have access to any sources
+            self.assertRaises(RequirementError, tdr.snapshot_names_by_id)
         return tdr
 
     @cached_property
@@ -994,7 +997,10 @@ class IndexingIntegrationTest(IntegrationTestCase, AlwaysTearDownTestCase):
         with self._public_service_account_credentials:
             public_source_ids = list_source_ids()
         with self._unregistered_service_account_credentials:
-            self.assertEqual(public_source_ids, list_source_ids())
+            # FIXME: Re-enable once TDR timeout issues are resolved
+            #        https://github.com/DataBiosphere/azul/issues/4092
+            if False:
+                self.assertEqual(public_source_ids, list_source_ids())
         with self._authorization_context(TDRClient.for_registered_user(OAuth2('foo'))):
             self.assertEqual(401, self._get_url_unchecked(url).status)
         self.assertEqual(set(), list_source_ids() & managed_access_source_ids)
