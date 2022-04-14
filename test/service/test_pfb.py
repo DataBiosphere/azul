a3fcf1eb95efe30919004e0afa1aebe8553a1232
Hannes Schmidt <hannes@ucsc.edu>
2022-04-13 22:24:55 -0700

    Remove unnecessary cast

--- test/service/test_pfb.py
+++ test/service/test_pfb.py
@@ -21,7 +21,7 @@ class TestPFB(AzulUnitTestCase):
     def test_pfb_schema(self):
         field_types = FileTransformer.field_types()
         schema = avro_pfb.pfb_schema_from_field_types(field_types)
-        fastavro.parse_schema(cast(dict, schema))
+        fastavro.parse_schema(schema)
         # Parsing successfully proves our schema is valid
         with self.assertRaises(KeyError):
             fastavro.parse_schema({'this': 'is not', 'an': 'avro schema'})
