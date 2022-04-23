aaadf2691e5220cee73459f82430410beca678ee
Hannes Schmidt <hannes@ucsc.edu>
2022-04-22 18:33:33 -0700

    Eliminate step_function_helper.py

--- src/azul/service/async_manifest_service.py
+++ src/azul/service/async_manifest_service.py
@@ -11,9 +11,8 @@ from botocore.exceptions import (
     ClientError,
 )
 
-from azul.service.step_function_helper import (
-    StateMachineError,
-    StepFunctionHelper,
+from azul.deployment import (
+    aws,
 )
 from azul.types import (
     JSON,
@@ -54,28 +53,33 @@ class Token:
                            request_index=self.request_index + 1)
 
 
+class StateMachineError(RuntimeError):
+
+    def __init__(self, *args) -> None:
+        super().__init__('Failed to generate manifest', *args)
+
+
 class AsyncManifestService:
     """
     Starting and checking the status of manifest generation jobs.
     """
-    step_function_helper = StepFunctionHelper()
 
     def __init__(self, state_machine_name):
         self.state_machine_name = state_machine_name
 
     def start_generation(self, input: JSON) -> Token:
         execution_id = str(uuid.uuid4())
-        self.step_function_helper.start_execution(self.state_machine_name,
-                                                  execution_id,
-                                                  execution_input=input)
+        self._start_execution(self.state_machine_name,
+                              execution_id,
+                              execution_input=input)
         return Token(execution_id=execution_id,
                      request_index=0,
                      wait_time=self._get_next_wait_time(0))
 
     def inspect_generation(self, token) -> Union[Token, JSON]:
         try:
-            execution = self.step_function_helper.describe_execution(state_machine_name=self.state_machine_name,
-                                                                     execution_name=token.execution_id)
+            execution = self._describe_execution(state_machine_name=self.state_machine_name,
+                                                 execution_name=token.execution_id)
         except ClientError as e:
             if e.response['Error']['Code'] == 'ExecutionDoesNotExist':
                 raise InvalidTokenError from e
@@ -101,3 +105,26 @@ class AsyncManifestService:
             return wait_times[request_index]
         except IndexError:
             return wait_times[-1]
+
+    def state_machine_arn(self, state_machine_name):
+        return f'arn:aws:states:{aws.region_name}:{aws.account}:stateMachine:{state_machine_name}'
+
+    def execution_arn(self, state_machine_name, execution_name):
+        return f'arn:aws:states:{aws.region_name}:{aws.account}:execution:{state_machine_name}:{execution_name}'
+
+    @property
+    def _sfn(self):
+        return aws.stepfunctions
+
+    def _start_execution(self, state_machine_name, execution_name, execution_input):
+        execution_params = {
+            'stateMachineArn': self.state_machine_arn(state_machine_name),
+            'name': execution_name,
+            'input': json.dumps(execution_input)
+        }
+        execution_response = self._sfn.start_execution(**execution_params)
+        assert self.execution_arn(state_machine_name, execution_name) == execution_response['executionArn']
+
+    def _describe_execution(self, state_machine_name, execution_name):
+        return self._sfn.describe_execution(
+            executionArn=self.execution_arn(state_machine_name, execution_name))
