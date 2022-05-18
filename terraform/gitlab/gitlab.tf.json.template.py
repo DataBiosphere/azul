14cbd7c2badd14018c3c9a875e60e79fb1b6cfdc
Abraham Chavez <achave11@ucsc.edu>
2022-05-18 10:57:50 -0700

    Update GitLab to 14.10.2 (#4014)

--- terraform/gitlab/gitlab.tf.json.template.py
+++ terraform/gitlab/gitlab.tf.json.template.py
@@ -1243,7 +1243,7 @@ emit_tf({} if config.terraform_component != 'gitlab' else {
                                --volume /mnt/gitlab/config:/etc/gitlab \
                                --volume /mnt/gitlab/logs:/var/log/gitlab \
                                --volume /mnt/gitlab/data:/var/opt/gitlab \
-                               gitlab/gitlab-ce:14.10.1-ce.0
+                               gitlab/gitlab-ce:14.10.2-ce.0
                         docker run \
                                --detach \
                                --name gitlab-runner \
