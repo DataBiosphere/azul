bdf7861d19bd7d13e1bd6a3b8665380813e6acef
Hannes Schmidt <hannes@ucsc.edu>
2022-05-27 20:20:50 -0700

    [H] Fix: IT in prod fails due to ConnectionResetError (#4186, #4067)
    2abee4ff2e76b24fb67d02ac4a2b34450ab2ffa7

--- src/azul/terra.py
+++ src/azul/terra.py
@@ -328,13 +328,14 @@ class TerraClient(OAuth2Client):
         log.debug('_request(%r, %s, headers=%r, timeout=%r, body=%r)',
                   method, url, headers, timeout, body)
         try:
+            retry = urllib3.Retry(total=None, connect=2, read=2, redirect=0, status=0, other=2)
             response = self._http_client.request(method,
                                                  str(url),
                                                  headers=headers,
                                                  # FIXME: Service should return 503 response when Terra client times out
                                                  #        https://github.com/DataBiosphere/azul/issues/3968
                                                  timeout=timeout,
-                                                 retries=False,
+                                                 retries=retry,
                                                  body=body)
         except TimeoutError:
             raise TerraTimeoutException(url, timeout)
