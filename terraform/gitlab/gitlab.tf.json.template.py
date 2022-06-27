0165b3fd0ce58f82d6babeceebf8ad1e7ee20ca8
Abraham Chavez <achave11@ucsc.edu>
2022-06-27 10:25:47 -0700

    Update GitLab to 15.1.0 (#4014)

--- terraform/gitlab/gitlab.tf.json.template.py
+++ terraform/gitlab/gitlab.tf.json.template.py
@@ -1320,7 +1320,7 @@ emit_tf({} if config.terraform_component != 'gitlab' else {
                                --volume /mnt/gitlab/config:/etc/gitlab \
                                --volume /mnt/gitlab/logs:/var/log/gitlab \
                                --volume /mnt/gitlab/data:/var/opt/gitlab \
-                               gitlab/gitlab-ce:15.0.3-ce.0
+                               gitlab/gitlab-ce:15.1.0-ce.0
                         docker run \
                                --detach \
                                --name gitlab-runner \
@@ -1328,7 +1328,7 @@ emit_tf({} if config.terraform_component != 'gitlab' else {
                                --volume /mnt/gitlab/runner/config:/etc/gitlab-runner \
                                --network gitlab-runner-net \
                                --env DOCKER_HOST=tcp://gitlab-dind:2375 \
-                               gitlab/gitlab-runner:v15.0.0
+                               gitlab/gitlab-runner:v15.1.0
                     '''[1:]),  # trim newline char at the beginning as dedent() only removes indent common to all lines
                 'tags': {
                     'Name': 'azul-gitlab',
