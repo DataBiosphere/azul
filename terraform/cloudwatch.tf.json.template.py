484b016c9b7b5b39d89029ba78b01c06cc1942df
Daniel Sotirhos <dsotirho@ucsc.edu>
2022-08-11 11:10:04 -0700

    Refactor list comprehension

--- terraform/cloudwatch.tf.json.template.py
+++ terraform/cloudwatch.tf.json.template.py
@@ -7,28 +7,30 @@ from azul.terraform import (
 
 emit_tf(None if config.disable_monitoring else {
     'resource': [
-        {
-            'aws_cloudwatch_metric_alarm': {
-                f'{lambda_}_5xx': {
-                    'alarm_name': config.qualified_resource_name(lambda_ + '_5xx'),
-                    'comparison_operator': 'GreaterThanThreshold',
-                    # This alarm will catch persistent 5XX errors occurring over
-                    # one hour, specifically when more than one occurrence is
-                    # sampled in a ten-minute period for six consecutive periods.
-                    'evaluation_periods': 6,
-                    'period': 60 * 10,
-                    'metric_name': '5XXError',
-                    'namespace': 'AWS/ApiGateway',
-                    'statistic': 'Sum',
-                    'threshold': 1,
-                    'treat_missing_data': 'notBreaching',
-                    'dimensions': {
-                        'ApiName': config.qualified_resource_name(lambda_),
-                        'Stage': config.deployment_stage,
+        *(
+            {
+                'aws_cloudwatch_metric_alarm': {
+                    f'{lambda_}_5xx': {
+                        'alarm_name': config.qualified_resource_name(lambda_ + '_5xx'),
+                        'comparison_operator': 'GreaterThanThreshold',
+                        # This alarm will catch persistent 5XX errors occurring over
+                        # one hour, specifically when more than one occurrence is
+                        # sampled in a ten-minute period for six consecutive periods.
+                        'evaluation_periods': 6,
+                        'period': 60 * 10,
+                        'metric_name': '5XXError',
+                        'namespace': 'AWS/ApiGateway',
+                        'statistic': 'Sum',
+                        'threshold': 1,
+                        'treat_missing_data': 'notBreaching',
+                        'dimensions': {
+                            'ApiName': config.qualified_resource_name(lambda_),
+                            'Stage': config.deployment_stage,
+                        }
                     }
                 }
             }
-        }
-        for lambda_ in config.lambda_names()
+            for lambda_ in config.lambda_names()
+        )
     ]
 })
