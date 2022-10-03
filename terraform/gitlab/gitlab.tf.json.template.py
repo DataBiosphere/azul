5e9e1dff6212eb47c7c6089854d73afb6eb77091
Abraham Chavez <achave11@ucsc.edu>
2022-10-03 16:21:11 -0700

    Update GitLab to 15.4.1 (#4014)

--- terraform/gitlab/gitlab.tf.json.template.py
+++ terraform/gitlab/gitlab.tf.json.template.py
@@ -347,8 +347,8 @@ dss_direct_access_policy_statement = {
 
 clamav_image = 'clamav/clamav:0.104'
 dind_image = 'docker:20.10.18-dind'
-gitlab_image = 'gitlab/gitlab-ce:15.3.3-ce.0'
-runner_image = 'gitlab/gitlab-runner:v15.3.0'
+gitlab_image = 'gitlab/gitlab-ce:15.4.1-ce.0'
+runner_image = 'gitlab/gitlab-runner:v15.4.0'
 
 # There are ways to dynamically determine the latest Amazon Linux AMI but in the
 # spirit of reproducable builds we would rather pin the AMI and adopt updates at
