ea6d5faf410c2d551abd77551a1138bdadfeba50
Abraham Chavez <achave11@ucsc.edu>
2022-10-14 10:59:03 -0700

    Fix: `systemctl status` falsely reports that `clamscan` succeeded (#4577)

--- terraform/gitlab/gitlab.tf.json.template.py
+++ terraform/gitlab/gitlab.tf.json.template.py
@@ -1428,7 +1428,7 @@ emit_tf({} if config.terraform_component != 'gitlab' else {
                                     qq(
                                         'freshclam',
                                         '&& echo freshclam succeeded',
-                                        '|| echo freshclam failed',
+                                        '|| (echo freshclam failed; false)',
                                         '&& clamscan',
                                         '--recursive',
                                         '--infected',  # Only print infected files
@@ -1441,7 +1441,7 @@ emit_tf({} if config.terraform_component != 'gitlab' else {
                                         '--exclude-dir=^/scan/dev',
                                         '/scan',
                                         '&& echo clamscan succeeded',
-                                        '|| echo clamscan failed'
+                                        '|| (echo clamscan failed; false)'
                                     )
                                 ),
                                 '[Install]',
