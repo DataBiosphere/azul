0b54df6a5a653a98d38e312320ae0fc941ceeaa3
Hannes Schmidt <hannes@ucsc.edu>
2022-10-27 18:28:11 -0700

    Normalize string literals in s3.tf.json.template.py

--- terraform/s3.tf.json.template.py
+++ terraform/s3.tf.json.template.py
@@ -6,74 +6,74 @@ from azul.terraform import (
 )
 
 emit_tf({
-    "resource": [
+    'resource': [
         {
-            "aws_s3_bucket": {
-                "storage": {
-                    "bucket": config.s3_bucket,
-                    "force_destroy": True
+            'aws_s3_bucket': {
+                'storage': {
+                    'bucket': config.s3_bucket,
+                    'force_destroy': True
                 },
-                "urls": {
-                    "bucket": config.url_redirect_full_domain_name,
-                    "force_destroy": config.is_sandbox_or_personal_deployment,
+                'urls': {
+                    'bucket': config.url_redirect_full_domain_name,
+                    'force_destroy': config.is_sandbox_or_personal_deployment,
                 }
             },
-            "aws_s3_bucket_lifecycle_configuration": {
-                "storage": {
-                    "bucket": "${aws_s3_bucket.storage.id}",
-                    "rule": {
-                        "id": "manifests",
-                        "status": "Enabled",
-                        "filter": {
-                            "prefix": "manifests/"
+            'aws_s3_bucket_lifecycle_configuration': {
+                'storage': {
+                    'bucket': '${aws_s3_bucket.storage.id}',
+                    'rule': {
+                        'id': 'manifests',
+                        'status': 'Enabled',
+                        'filter': {
+                            'prefix': 'manifests/'
                         },
-                        "expiration": {
-                            "days": config.manifest_expiration
+                        'expiration': {
+                            'days': config.manifest_expiration
                         },
-                        "abort_incomplete_multipart_upload": {
-                            "days_after_initiation": 1
+                        'abort_incomplete_multipart_upload': {
+                            'days_after_initiation': 1
                         }
                     }
                 }
             },
-            "aws_s3_bucket_acl": {
-                "storage": {
-                    "bucket": "${aws_s3_bucket.storage.id}",
-                    "acl": "private",
+            'aws_s3_bucket_acl': {
+                'storage': {
+                    'bucket': '${aws_s3_bucket.storage.id}',
+                    'acl': 'private',
                 },
-                "urls": {
-                    "bucket": "${aws_s3_bucket.urls.id}",
-                    "acl": "public-read",
+                'urls': {
+                    'bucket': '${aws_s3_bucket.urls.id}',
+                    'acl': 'public-read',
                 }
             },
-            "aws_s3_bucket_website_configuration": {
-                "urls": {
-                    "bucket": "${aws_s3_bucket.urls.id}",
-                    "index_document": {
-                        "suffix": "404.html"
+            'aws_s3_bucket_website_configuration': {
+                'urls': {
+                    'bucket': '${aws_s3_bucket.urls.id}',
+                    'index_document': {
+                        'suffix': '404.html'
                     }
                 }
             }
         },
         *([{
-            "aws_route53_record": {
-                "url_redirect_record": {
-                    "zone_id": "${data.aws_route53_zone.azul_url.zone_id}",
-                    "name": config.url_redirect_full_domain_name,
-                    "type": "CNAME",
-                    "ttl": "300",
-                    "records": ["${aws_s3_bucket_website_configuration.urls.website_endpoint}"]
+            'aws_route53_record': {
+                'url_redirect_record': {
+                    'zone_id': '${data.aws_route53_zone.azul_url.zone_id}',
+                    'name': config.url_redirect_full_domain_name,
+                    'type': 'CNAME',
+                    'ttl': '300',
+                    'records': ['${aws_s3_bucket_website_configuration.urls.website_endpoint}']
                 }
             }
         }] if config.url_redirect_base_domain_name else [])
     ],
     **({
-           "data": [
+           'data': [
                {
-                   "aws_route53_zone": {
-                       "azul_url": {
-                           "name": config.url_redirect_base_domain_name + ".",
-                           "private_zone": False
+                   'aws_route53_zone': {
+                       'azul_url': {
+                           'name': config.url_redirect_base_domain_name + '.',
+                           'private_zone': False
                        }
                    }
                }
