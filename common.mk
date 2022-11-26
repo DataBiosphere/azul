06ea1ff5fc736e79e7224b95b2ac8d491f66e0c7
Hannes Schmidt <hannes@ucsc.edu>
2022-11-25 21:05:48 -0800

    Rewrap comment in Makefile

--- common.mk
+++ common.mk
@@ -82,6 +82,7 @@ check_branch: check_python
 	python $< $@
 .FORCE:
 
-# The template output file depends on the template file, of course, as well as the environment. To be safe we force the
-# template creation. This is what the fake .FORCE target does. It still is necessary to declare a target's dependency on
-# a template to ensure correct ordering.
+# The template output file depends on the template file, of course, as well as
+# the environment. To be safe we force the template creation. This is what
+# the fake .FORCE target does. It still is necessary to declare a target's
+# dependency on a template to ensure correct ordering.
