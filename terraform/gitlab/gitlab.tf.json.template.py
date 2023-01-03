f603f119ab9aef7309aa15bd8e53c592e43560cc
Abraham Chavez <achave11@ucsc.edu>
2023-01-03 15:45:01 -0800

    Update GitLab to 15.7.0 and ClamAV to 1.0.0-1 (#4014)

--- terraform/gitlab/gitlab.tf.json.template.py
+++ terraform/gitlab/gitlab.tf.json.template.py
@@ -292,10 +292,10 @@ def remove_inconsequential_statements(statements: list[JSON]) -> list[JSON]:
     return [s for s in statements if s['actions'] and s['resources']]
 
 
-clamav_image = 'clamav/clamav:0.105'
+clamav_image = 'clamav/clamav:1.0.0-1'
 dind_image = 'docker:20.10.18-dind'
-gitlab_image = 'gitlab/gitlab-ce:15.6.1-ce.0'
-runner_image = 'gitlab/gitlab-runner:v15.6.1'
+gitlab_image = 'gitlab/gitlab-ce:15.7.0-ce.0'
+runner_image = 'gitlab/gitlab-runner:v15.7.1'
 
 # There are ways to dynamically determine the latest Amazon Linux AMI but in the
 # spirit of reproducable builds we would rather pin the AMI and adopt updates at
