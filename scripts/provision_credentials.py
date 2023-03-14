DataBiosphere/azul

commit 15a272135a9ae481931efcd467cd47166e1bb9e5

Refactor misleading naming in provision_credentials.py

scripts/provision_credentials.py
@@ -13,6 +13,7 @@ from googleapiclient.errors import (
 )
 
 from azul import (
+    cached_property,
     config,
 )
 from azul.deployment import (
@@ -29,14 +30,14 @@ def parse_google_key(response):
     return base64.decodebytes(bytes(response['privateKeyData'], 'ascii')).decode()
 
 
-def get_google_service():
-    credentials, project_id = google.auth.default(
-        scopes=['https://www.googleapis.com/auth/cloud-platform']
-    )
-    return googleapiclient.discovery.build('iam', 'v1', credentials=credentials)
-
-
 class CredentialsProvisioner:
+
+    @cached_property
+    def google_iam(self):
+        credentials, project_id = google.auth.default(
+            scopes=['https://www.googleapis.com/auth/cloud-platform']
+        )
+        return googleapiclient.discovery.build('iam', 'v1', credentials=credentials)
 
     @property
     def secrets_manager(self):
@@ -101,8 +102,8 @@ class CredentialsProvisioner:
             return False
 
     def _create_service_account_creds(self, service_account_email):
-        service = get_google_service()
-        key = service.projects().serviceAccounts().keys().create(
+        iam = self.google_iam()
+        key = iam.projects().serviceAccounts().keys().create(
             name='projects/-/serviceAccounts/' + service_account_email, body={}
         ).execute()
         logger.info("Successfully created service account key for user '%s'", service_account_email)
@@ -147,9 +148,9 @@ class CredentialsProvisioner:
             return
         else:
             key_id = json.loads(creds['SecretString'])['private_key_id']
-            service = get_google_service()
+            iam = self.google_iam()
             try:
-                service.projects().serviceAccounts().keys().delete(
+                iam.projects().serviceAccounts().keys().delete(
                     name='projects/-/serviceAccounts/' + service_account_email + '/keys/' + key_id).execute()
             except HttpError as e:
                 if e.resp.reason != 'Not Found':

commit 79139f9d640420d1c4c00e8a9f486822cc8e8048

Fix: TypeError in provision_credentials.py
Regression from 15a272135a9ae481931efcd467cd47166e1bb9e5

scripts/provision_credentials.py
@@ -102,7 +102,7 @@ class CredentialsProvisioner:
             return False
 
     def _create_service_account_creds(self, service_account_email):
-        iam = self.google_iam()
+        iam = self.google_iam
         key = iam.projects().serviceAccounts().keys().create(
             name='projects/-/serviceAccounts/' + service_account_email, body={}
         ).execute()
@@ -148,7 +148,7 @@ class CredentialsProvisioner:
             return
         else:
             key_id = json.loads(creds['SecretString'])['private_key_id']
-            iam = self.google_iam()
+            iam = self.google_iam
             try:
                 iam.projects().serviceAccounts().keys().delete(
                     name='projects/-/serviceAccounts/' + service_account_email + '/keys/' + key_id).execute()