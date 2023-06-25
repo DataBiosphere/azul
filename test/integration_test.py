3797e535718b4bd075bef14675c5110550c6776c
Hannes Schmidt <hannes@ucsc.edu>
2023-06-25 16:45:27 -0700

    [r] Index AnVIL snapshots from TDR prod in anvilprod (#5283)

--- test/integration_test.py
+++ test/integration_test.py
@@ -212,7 +212,9 @@ class IntegrationTestCase(AzulTestCase, metaclass=ABCMeta):
             # FIXME: Unpin the seed once underlying issue is fixed
             #        https://github.com/DataBiosphere/azul/issues/5168
             6634795309975096822
-            if config.deployment_stage == 'anvilprod' else
+            # FIXME: Change to anvildev
+            #        https://github.com/DataBiosphere/azul/issues/5283
+            if config.deployment_stage == '' else
             randint(0, sys.maxsize)
         )
         self.random = Random(self.random_seed)
