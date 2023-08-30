48e95e8d8c5ef1a68ca366087ddca78b5565deea
Noa Aviel Dove <nadove@ucsc.edu>
2023-08-29 19:25:37 -0700

    [r] Track replica hub IDs (#5360)

--- src/azul/plugins/metadata/hca/indexer/transform.py
+++ src/azul/plugins/metadata/hca/indexer/transform.py
@@ -503,7 +503,8 @@ class BaseTransformer(Transformer, metaclass=ABCMeta):
 
     def _add_replica(self,
                      contribution: MutableJSON,
-                     entity: Union[api.Entity, DatedEntity]
+                     entity: Union[api.Entity, DatedEntity],
+                     hub_ids: list[EntityID]
                      ) -> Transform:
         entity_ref = EntityReference(entity_id=str(entity.document_id),
                                      entity_type=self.entity_type())
@@ -511,7 +512,7 @@ class BaseTransformer(Transformer, metaclass=ABCMeta):
             replica = None
         else:
             assert isinstance(entity, api.Entity), entity
-            replica = self._replica(entity.json, entity_ref)
+            replica = self._replica(entity.json, entity_ref, hub_ids)
         return self._contribution(contribution, entity_ref), replica
 
     def _find_ancestor_samples(self,
@@ -1465,7 +1466,8 @@ class FileTransformer(PartitionedTransformer[api.File]):
                         additional_contents = self.matrix_stratification_values(file)
                         for entity_type, values in additional_contents.items():
                             contents[entity_type].extend(values)
-                yield self._add_replica(contents, file)
+                hub_ids = list(map(str, visitor.files))
+                yield self._add_replica(contents, file, hub_ids)
 
     def matrix_stratification_values(self, file: api.File) -> JSON:
         """
@@ -1560,7 +1562,8 @@ class CellSuspensionTransformer(PartitionedTransformer):
                             ),
                             dates=[self._date(cell_suspension)],
                             projects=[self._project(self._api_project)])
-            yield self._add_replica(contents, cell_suspension)
+            hub_ids = list(map(str, visitor.files))
+            yield self._add_replica(contents, cell_suspension, hub_ids)
 
 
 class SampleTransformer(PartitionedTransformer):
@@ -1605,7 +1608,8 @@ class SampleTransformer(PartitionedTransformer):
                             ),
                             dates=[self._date(sample)],
                             projects=[self._project(self._api_project)])
-            yield self._add_replica(contents, sample)
+            hub_ids = list(map(str, visitor.files))
+            yield self._add_replica(contents, sample, hub_ids)
 
 
 class BundleAsEntity(DatedEntity):
@@ -1705,7 +1709,8 @@ class SingletonTransformer(BaseTransformer, metaclass=ABCMeta):
                         contributed_analyses=contributed_analyses,
                         dates=[self._date(self._singleton_entity())],
                         projects=[self._project(self._api_project)])
-        return self._add_replica(contents, self._singleton_entity())
+        hub_ids = list(map(str, visitor.files))
+        return self._add_replica(contents, self._singleton_entity(), hub_ids)
 
 
 class ProjectTransformer(SingletonTransformer):
