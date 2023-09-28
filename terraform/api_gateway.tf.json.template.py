272c68c14dfec7b128c16f1a30a97ef7df9268d0
Daniel Sotirhos <dsotirho@ucsc.edu>
2023-09-28 09:33:12 -0700

    Enable WAF logging of blocked requests

--- terraform/api_gateway.tf.json.template.py
+++ terraform/api_gateway.tf.json.template.py
@@ -317,6 +317,34 @@ emit_tf({
                         'sampled_requests_enabled': True,
                     }
                 }
+            },
+            'aws_cloudwatch_log_group': {
+                'waf_api_gateway': {
+                    # WAF logging requires this specific log group name prefix
+                    # https://docs.aws.amazon.com/waf/latest/developerguide/logging-cw-logs.html#logging-cw-logs-naming
+                    'name': 'aws-waf-logs-' + config.qualified_resource_name('api_gateway'),
+                    'retention_in_days': config.audit_log_retention_days
+                }
+            },
+            'aws_wafv2_web_acl_logging_configuration': {
+                'waf_api_gateway': {
+                    'log_destination_configs': [
+                        '${aws_cloudwatch_log_group.waf_api_gateway.arn}'
+                    ],
+                    'resource_arn': '${aws_wafv2_web_acl.api_gateway.arn}',
+                    'logging_filter': {
+                        'default_behavior': 'DROP',
+                        'filter': {
+                            'behavior': 'KEEP',
+                            'requirement': 'MEETS_ALL',
+                            'condition': {
+                                'action_condition': {
+                                    'action': 'BLOCK'
+                                }
+                            }
+                        }
+                    }
+                }
             }
         },
         *([
