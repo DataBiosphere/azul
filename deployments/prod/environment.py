b7a28ea8cb5a29ee24a34e9917767cf751ff3485
Abraham Chavez <achave11@ucsc.edu>
2024-01-10 14:28:22 -0800

    Make dcp34 the default catalog on prod (#5840)

--- deployments/prod/environment.py
+++ deployments/prod/environment.py
@@ -1084,7 +1084,6 @@ def env() -> Mapping[str, Optional[str]]:
                                                     repository=dict(name='tdr_hca')),
                                        sources=mklist(sources))
             for atlas, catalog, sources in [
-                ('hca', 'dcp33', dcp33_sources),
                 ('hca', 'dcp34', dcp34_sources),
                 ('hca', 'pilot', pilot_sources),
                 ('hca', 'dcp1', dcp1_sources),
