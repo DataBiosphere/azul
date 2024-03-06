433e3af9ad1f8ef2d63953b53f089d65800f7371
Daniel Sotirhos <dsotirho@ucsc.edu>
2024-03-05 17:53:35 -0800

    Update ClamAV to 1.3.0-41 (#6007)

--- environment.py
+++ environment.py
@@ -280,7 +280,7 @@ def env() -> Mapping[str, Optional[str]]:
             # Updating any of the four images below additionally requires
             # redeploying the `gitlab` TF component.
             'clamav': {
-                'ref': 'docker.io/clamav/clamav:1.3.0-40'
+                'ref': 'docker.io/clamav/clamav:1.3.0-41'
             },
             'gitlab': {
                 'ref': 'docker.io/gitlab/gitlab-ce:16.9.1-ce.0'
