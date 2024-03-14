d8d5b3da7dd7acf6c61fa6127e4589f2b2b00100
Hannes Schmidt <hannes@ucsc.edu>
2024-03-13 22:41:26 -0700

    [u r] Fix: VPC CIDR in `dev` is wrong (#6045)

--- deployments/dev.gitlab/environment.py
+++ deployments/dev.gitlab/environment.py
@@ -26,6 +26,6 @@ def env() -> Mapping[str, Optional[str]]:
     """
     return {
         'azul_terraform_component': 'gitlab',
-        'azul_vpc_cidr': '172.71.0.0/16',
+        'azul_vpc_cidr': '172.21.0.0/16',
         'azul_vpn_subnet': '10.42.0.0/16'
     }
