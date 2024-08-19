7bfdcf89e816d60165c755266876278d72525523
Daniel Sotirhos <dsotirho@ucsc.edu>
2024-08-19 16:49:49 -0700

    Update BigQuery Emulator to 0.4.4-10 (#6505)

--- environment.py
+++ environment.py
@@ -293,7 +293,7 @@ def env() -> Mapping[str, Optional[str]]:
                 'is_custom': True
             },
             'bigquery_emulator': {
-                'ref': 'docker.io/ucscgi/azul-bigquery-emulator:0.4.4-9',
+                'ref': 'docker.io/ucscgi/azul-bigquery-emulator:0.4.4-10',
                 'url': 'https://hub.docker.com/repository/docker/ucscgi/azul-bigquery-emulator',
                 'is_custom': True
             },
