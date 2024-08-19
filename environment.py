741c26ec50898cfbc8679dec6ede09ea13562cec
Daniel Sotirhos <dsotirho@ucsc.edu>
2024-08-19 16:40:44 -0700

    Update PyCharm to 2024.2-31 (#6505)

--- environment.py
+++ environment.py
@@ -283,7 +283,7 @@ def env() -> Mapping[str, Optional[str]]:
                 'url': 'https://hub.docker.com/_/python',
             },
             'pycharm': {
-                'ref': 'docker.io/ucscgi/azul-pycharm:2024.1.6-30',
+                'ref': 'docker.io/ucscgi/azul-pycharm:2024.2-31',
                 'url': 'https://hub.docker.com/repository/docker/ucscgi/azul-pycharm',
                 'is_custom': True
             },
