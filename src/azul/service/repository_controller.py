0d3096d065dec564ebc98556079bfc5b361b8baa
Daniel Sotirhos <dsotirho@ucsc.edu>
2024-09-04 11:26:06 -0700

    Fix rounding of /repository/file retry-after value

--- src/azul/service/repository_controller.py
+++ src/azul/service/repository_controller.py
@@ -4,6 +4,9 @@ from collections.abc import (
 )
 import json
 import logging
+from math import (
+    ceil,
+)
 from typing import (
     Any,
     Callable,
@@ -297,7 +300,7 @@ class RepositoryController(SourceController):
                     pass
                 elif wait == '1':
                     time_slept = self.server_side_sleep(float(retry_after))
-                    retry_after = round(retry_after - time_slept)
+                    retry_after = ceil(retry_after - time_slept)
                 else:
                     assert False, wait
             query_params = self._file_to_request(download.file) | adict(
