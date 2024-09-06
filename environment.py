bea49fcadfbfae4e9aa9c52c02a03e79d381e1f9
Abraham Chavez <achave11@ucsc.edu>
2024-09-05 19:08:52 -0700

    Update GitLab to 17.3.1 (#6537)

--- environment.py
+++ environment.py
@@ -303,11 +303,11 @@ def env() -> Mapping[str, Optional[str]]:
                 'url': 'https://hub.docker.com/r/clamav/clamav'
             },
             'gitlab': {
-                'ref': 'docker.io/gitlab/gitlab-ce:17.3.0-ce.0',
+                'ref': 'docker.io/gitlab/gitlab-ce:17.3.1-ce.0',
                 'url': 'https://hub.docker.com/r/gitlab/gitlab-ce'
             },
             'gitlab_runner': {
-                'ref': 'docker.io/gitlab/gitlab-runner:ubuntu-v17.3.0',
+                'ref': 'docker.io/gitlab/gitlab-runner:ubuntu-v17.3.1',
                 'url': 'https://hub.docker.com/r/gitlab/gitlab-runner'
             },
             'dind': {
