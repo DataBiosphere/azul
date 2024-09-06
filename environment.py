4fa5a50493b81de258ac2659e65001a14b415bdc
Abraham Chavez <achave11@ucsc.edu>
2024-09-05 19:05:29 -0700

    Update Docker to 27.2.0 (#6537)

--- environment.py
+++ environment.py
@@ -234,7 +234,7 @@ def env() -> Mapping[str, Optional[str]]:
         # `gitlab` components, as well as building and pushing the executor
         # image (see terraform/gitlab/runner/Dockerfile for how).
         #
-        'azul_docker_version': '27.1.2',
+        'azul_docker_version': '27.2.0',
 
         # The version of Python used throughout the system.
         #
