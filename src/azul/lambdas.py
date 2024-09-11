f612650d42903a5fc5e78672d31fc701e940ad82
Daniel Sotirhos <dsotirho@ucsc.edu>
2024-09-11 09:25:39 -0700

    fixup! Fix: Race between layer and Lambda update (#5927)

--- src/azul/lambdas.py
+++ src/azul/lambdas.py
@@ -204,15 +204,15 @@ class Lambdas:
 
     def delete_published_function_versions(self):
         """
-        Delete all but the latest published version of every AWS Lambda function
-        in the current deployment.
+        Delete the published versions of every AWS Lambda function in the
+        current deployment.
         """
         log.info('Deleting stale versions of AWS Lambda functions')
         for function in self.list_lambdas(deployment=config.deployment_stage,
                                           all_versions=True):
             if function.version == '$LATEST':
-                log.info('Skipping latest version %r', function.name)
+                log.info('Skipping the unpublished version of %r', function.name)
             else:
-                log.info('Deleting version %r of %r', function.version, function.name)
+                log.info('Deleting published version %r of %r', function.version, function.name)
                 self._lambda.delete_function(FunctionName=function.name,
                                              Qualifier=function.version)
