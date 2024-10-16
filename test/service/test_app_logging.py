83e5d3e64187a450903e613820840027ff8fab27
Abraham Chavez <achave11@ucsc.edu>
2024-10-16 16:30:26 -0700

    Modify TestServiceAppLogging to simplify test case additions

--- test/service/test_app_logging.py
+++ test/service/test_app_logging.py
@@ -6,9 +6,6 @@ from logging import (
 
 import requests
 
-from app_test_case import (
-    LocalAppTestCase,
-)
 from azul.chalice import (
     log,
 )
@@ -21,6 +18,9 @@ from azul.strings import (
 from indexer import (
     DCP1CannedBundleTestCase,
 )
+from service import (
+    WebServiceTestCase,
+)
 
 
 # noinspection PyPep8Naming
@@ -28,7 +28,17 @@ def setUpModule():
     configure_test_logging()
 
 
-class TestServiceAppLogging(DCP1CannedBundleTestCase, LocalAppTestCase):
+class TestServiceAppLogging(DCP1CannedBundleTestCase, WebServiceTestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls._setup_indices()
+
+    @classmethod
+    def tearDownClass(cls):
+        cls._teardown_indices()
+        super().tearDownClass()
 
     @classmethod
     def lambda_name(cls) -> str:
@@ -38,7 +48,7 @@ class TestServiceAppLogging(DCP1CannedBundleTestCase, LocalAppTestCase):
         for level in INFO, DEBUG:
             for authenticated in False, True:
                 with self.subTest(level=level, authenticated=authenticated):
-                    url = self.base_url.set(path='/health/basic')
+                    url = self.base_url.set(path='/index/projects')
                     headers = {'authorization': 'Bearer foo_token'} if authenticated else {}
                     with self.assertLogs(logger=log, level=level) as logs:
                         requests.get(str(url), headers=headers)
@@ -54,8 +64,9 @@ class TestServiceAppLogging(DCP1CannedBundleTestCase, LocalAppTestCase):
                     self.assertEqual(logs, [
                         (
                             INFO,
-                            f"Received GET request for '/health/basic', "
-                            f"with {json.dumps({'query': None, 'headers': headers})}."),
+                            f"Received GET request for '/index/projects', "
+                            f"with {json.dumps({'query': None, 'headers': headers})}."
+                        ),
                         (
                             INFO,
                             "Authenticated request as OAuth2(access_token='foo_token')"
@@ -77,6 +88,19 @@ class TestServiceAppLogging(DCP1CannedBundleTestCase, LocalAppTestCase):
                             '"X-XSS-Protection": "1; mode=block", '
                             '"Cache-Control": "no-store"}. '
                             'See next line for the first 1024 characters of the body.\n'
-                            '{"up": true}'
+                            '{"pagination": {"count": 1, "total": 1, "size": 10, "next": null, "previous":'
+                            ' null, "pages": 1, "sort": "projectTitle", "order": "asc"}, "termFacets": '
+                            '{"organ": {"terms": [{"term": "pancreas", "count": 1}], "total": 1, "type": '
+                            '"terms"}, "sampleEntityType": {"terms": [{"term": "specimens", "count": 1}], '
+                            '"total": 1, "type": "terms"}, "dataUseRestriction": {"terms": [{"term": null, '
+                            '"count": 1}], "total": 1, "type": "terms"}, "project": {"terms": [{"term": '
+                            '"Single of human pancreas", "count": 1, "projectId": '
+                            '["e8642221-4c2c-4fd7-b926-a68bce363c88"]}], "total": 1, "type": "terms"}, '
+                            '"sampleDisease": {"terms": [{"term": "normal", "count": 1}], "total": 1, "type": '
+                            '"terms"}, "nucleicAcidSource": {"terms": [{"term": "single cell", "count": 1}], '
+                            '"total": 1, "type": "terms"}, "assayType": {"terms": [{"term": null, "count": 1}], '
+                            '"total": 0, "type": "terms"}, "instrumentManufacturerModel": {"terms": [{"term": '
+                            '"Illumina NextSeq 500", "count": 1}], "total": 1, "type": "terms"}, "institution": '
+                            '{"terms": [{"term": "Farmers Tru',
                         )
                     ])
