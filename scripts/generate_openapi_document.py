d4d83e3bdccef2eeeae039b8ac604910b2a0f042
Daniel Sotirhos <dsotirho@ucsc.edu>
2024-10-24 15:33:51 -0700

    [h] Patch enable_replicas config for OpenAPI doc

--- scripts/generate_openapi_document.py
+++ scripts/generate_openapi_document.py
@@ -53,16 +53,21 @@ def main():
                               new_callable=PropertyMock,
                               return_value=False):
                 assert not config.enable_log_forwarding
-                lambda_endpoint = furl('http://localhost')
-                with patch.object(target=AzulChaliceApp,
-                                  attribute='base_url',
-                                  new=lambda_endpoint):
-                    app_module = load_app_module(lambda_name)
-                    assert app_module.app.base_url == lambda_endpoint
-                    app_spec = app_module.app.spec()
-                    doc_path = Path(config.project_root) / 'lambdas' / lambda_name / 'openapi.json'
-                    with write_file_atomically(doc_path) as file:
-                        json.dump(app_spec, file, indent=4)
+                with patch.object(target=type(config),
+                                  attribute='enable_replicas',
+                                  new_callable=PropertyMock,
+                                  return_value=True):
+                    assert config.enable_replicas
+                    lambda_endpoint = furl('http://localhost')
+                    with patch.object(target=AzulChaliceApp,
+                                      attribute='base_url',
+                                      new=lambda_endpoint):
+                        app_module = load_app_module(lambda_name)
+                        assert app_module.app.base_url == lambda_endpoint
+                        app_spec = app_module.app.spec()
+                        doc_path = Path(config.project_root) / 'lambdas' / lambda_name / 'openapi.json'
+                        with write_file_atomically(doc_path) as file:
+                            json.dump(app_spec, file, indent=4)
 
 
 if __name__ == '__main__':
