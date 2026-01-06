441410d6d1d4bc4ea7370ab826751d819b6ebc87
Daniel Sotirhos <dsotirho@ucsc.edu>
2026-01-06 09:48:40 -0800

    [r A] Conditionally remove aggregation of specimens.biomaterial_id in HCA (#6793)

--- src/azul/plugins/metadata/hca/indexer/aggregate.py
+++ src/azul/plugins/metadata/hca/indexer/aggregate.py
@@ -120,7 +120,17 @@ class SampleAggregator(SimpleAggregator):
 
 
 class SpecimenAggregator(SimpleAggregator):
-    pass
+
+    def _accumulator(self, field) -> Accumulator | None:
+        if field == 'biomaterial_id':
+            # These fields are only aggregated for files, where they are needed
+            # for compact and PFB manifests
+            if self.outer_entity_type == 'files':
+                return super()._accumulator(field)
+            else:
+                return None
+        else:
+            return super()._accumulator(field)
 
 
 class CellSuspensionAggregator(GroupingAggregator):
