b5eb9b43e0940cac0a3496c1d56ca677eb8a4e89
Daniel Sotirhos <dsotirho@ucsc.edu>
2026-01-06 17:32:08 -0800

    [r] Conditionally remove aggregation of biosamples.source_datarepo_row_ids in AnVIL (#6793)

--- src/azul/plugins/metadata/anvil/indexer/aggregate.py
+++ src/azul/plugins/metadata/anvil/indexer/aggregate.py
@@ -42,7 +42,8 @@ class BiosampleAggregator(SimpleAggregator):
     def _accumulator(self, field: str) -> Accumulator | None:
         if field in {
             'biosample_id',
-            'document_id'
+            'document_id',
+            'source_datarepo_row_ids'
         } and self.outer_entity_type != 'files':
             # These fields are only aggregated for files, where they are needed
             # for compact and PFB manifests
