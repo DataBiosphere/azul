8a5af88307c324a57c5d5c74fdfe77593f7677de
Daniel Sotirhos <dsotirho@ucsc.edu>
2026-01-20 10:46:37 -0800

    [r] Increase accumulator limit for specimens.biomaterial_id and document_id in HCA (#6793)

--- src/azul/plugins/metadata/hca/indexer/aggregate.py
+++ src/azul/plugins/metadata/hca/indexer/aggregate.py
@@ -136,7 +136,7 @@ class SpecimenAggregator(SimpleAggregator):
             # These fields are only aggregated for files, where they are needed
             # for compact and PFB manifests
             if self.outer_entity_type == 'files':
-                return super()._accumulator(field)
+                return SetAccumulator(max_size=int(1209 * 1.25))
             # `document_id` is included in the sample aggregate so that the
             # summary response field `specimenCount` can be calculated. This
             # should not be a problem since there should only ever be one
