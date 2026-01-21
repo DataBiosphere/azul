f383eaf5a7db50c6a3cdc8fc2be07c1d59ac6a38
Daniel Sotirhos <dsotirho@ucsc.edu>
2026-01-21 10:52:33 -0800

    [r] Increase accumulator limit for sequencing_inputs.biomaterial_id and document_id in HCA (#6793)

--- src/azul/plugins/metadata/hca/indexer/aggregate.py
+++ src/azul/plugins/metadata/hca/indexer/aggregate.py
@@ -305,7 +305,7 @@ class SequencingInputAggregator(SimpleAggregator):
             # These fields are only aggregated for files, where they are needed
             # for compact and PFB manifests
             if self.outer_entity_type == 'files':
-                return super()._accumulator(field)
+                return SetAccumulator(max_size=int(7302 * 1.25))
             else:
                 return None
         else:
