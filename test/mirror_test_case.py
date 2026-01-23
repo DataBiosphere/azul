1321d0f9e1dc22048e52c5cad952757c5f65c4ef
Noa Dove <nadove@ucsc.edu>
2026-01-22 17:08:25 -0800

    Move patches from setUp to setUpClass

--- test/mirror_test_case.py
+++ test/mirror_test_case.py
@@ -14,12 +14,16 @@ from s3_test_case import (
 class MirrorTestCase(S3TestCase):
     mirror_bucket = 'test-mirror-bucket'
 
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.addClassPatch(patch.object(type(config),
+                                       'enable_mirroring',
+                                       new=PropertyMock(return_value=True)))
+        cls.addClassPatch(patch.object(type(config),
+                                       'mirror_bucket',
+                                       new=PropertyMock(return_value=cls.mirror_bucket)))
+
     def setUp(self):
         super().setUp()
-        self.addPatch(patch.object(type(config),
-                                   'enable_mirroring',
-                                   new=PropertyMock(return_value=True)))
-        self.addPatch(patch.object(type(config),
-                                   'mirror_bucket',
-                                   new=PropertyMock(return_value=self.mirror_bucket)))
         self._create_test_bucket(self.mirror_bucket)
