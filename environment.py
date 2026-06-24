b5dd63b63fdf33a7d796aba2df155e8f55244096
Noa Dove <nadove@ucsc.edu>
2026-06-24 16:03:33 -0700

    Update Terraform to 1.15.7

--- environment.py
+++ environment.py
@@ -244,7 +244,7 @@ def env() -> Mapping[str, str | None]:
         # `make -C terraform update_schema`, and committing the resulting
         # changes.
         #
-        'azul_terraform_version': '1.15.6',
+        'azul_terraform_version': '1.15.7',
 
         # The version of the AWS CLI v2 used throughout the system.
         #
