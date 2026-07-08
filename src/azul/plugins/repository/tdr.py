0123f7fcc5734aabeb22b3483881ddf64157a361
Noa Dove <nadove@ucsc.edu>
2026-07-08 16:10:15 -0700

    Remove TDR auth fallback (#7793)

--- src/azul/plugins/repository/tdr.py
+++ src/azul/plugins/repository/tdr.py
@@ -9,14 +9,10 @@ import datetime
 import logging
 import time
 from typing import (
-    Callable,
     Iterable,
     TypeVar,
 )
 
-from chalice import (
-    UnauthorizedError,
-)
 from furl import (
     furl,
 )
@@ -98,28 +94,6 @@ class TDRPlugin[TDR_BUNDLE: TDRBundle,
     ]
 ):
 
-    def _auth_fallback(self,
-                       authentication: Authentication | None,
-                       tdr_callback: Callable[[TDRClient], T]
-                       ) -> T:
-        """
-        Terra rejects credentials for users are not registered with Terra's SAM.
-        This method invokes the given callback with the given credentials but
-        invokes the callback again if they are rejected for that reason.
-        """
-        # The line below raises UnauthorizedError for invalid tokens. We don't
-        # want to fall back to anonymous authentication in that case.
-        tdr = self._authenticated_tdr(authentication)
-        try:
-            return tdr_callback(tdr)
-        except UnauthorizedError:
-            if authentication is None or tdr.is_registered():
-                raise
-            else:
-                log.info('Falling back to anonymous access')
-                tdr = self._authenticated_tdr(None)
-                return tdr_callback(tdr)
-
     @cached_property
     def _common_source_filter(self) -> str:
         # We filter by prefix of snapshot names in an attempt to speed up the
@@ -132,11 +106,8 @@ class TDRPlugin[TDR_BUNDLE: TDRBundle,
     def list_sources(self,
                      authentication: Authentication | None
                      ) -> list[TDRSourceRef]:
-        def list_snapshots(tdr: TDRClient):
-            return tdr.list_snapshots(filter=self._common_source_filter)
-
-        snapshots_by_id = self._auth_fallback(authentication, list_snapshots)
-
+        tdr = self._authenticated_tdr(authentication)
+        snapshots_by_id = tdr.list_snapshots(filter=self._common_source_filter)
         return [
             TDRSourceRef(id=id,
                          spec=TDRSourceSpec(name=snapshot['name'],
@@ -150,10 +121,8 @@ class TDRPlugin[TDR_BUNDLE: TDRBundle,
     def list_source_ids(self,
                         authentication: Authentication | None
                         ) -> set[str]:
-        def list_snapshot_ids(tdr: TDRClient):
-            return tdr.list_snapshot_ids()
-
-        return self._auth_fallback(authentication, list_snapshot_ids)
+        tdr = self._authenticated_tdr(authentication)
+        return tdr.list_snapshot_ids()
 
     @property
     def tdr(self):
