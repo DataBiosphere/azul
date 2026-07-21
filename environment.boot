b37aeb4c079d228931869f4da2b8f2b5c50fb4be
Noa Dove <nadove@ucsc.edu>
2026-07-20 23:35:20 -0700

    Update Terraform to 1.15.8 (#8164)

--- environment.boot
+++ environment.boot
@@ -1,6 +1,6 @@
 azul_python_version=3.14.6
 azul_python_image=docker.io/library/python@sha256:2e037675ca662c3547ac5bd0bf71fdf8388848dd73e4ee603e03b4fc5aadc250
 azul_docker_version=29.6
-azul_terraform_version=1.15.7
+azul_terraform_version=1.15.8
 azul_awscli_version=2.35.14
 azul_ghcli_version=2.95.0
--- environment.py
+++ environment.py
@@ -244,7 +244,7 @@ def env() -> Mapping[str, str | None]:
         # `make -C terraform update_schema`, and committing the resulting
         # changes.
         #
-        'azul_terraform_version': '1.15.7',
+        'azul_terraform_version': '1.15.8',
 
         # The version of the AWS CLI v2 used throughout the system.
         #
