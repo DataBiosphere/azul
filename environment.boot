2ab44199ae4c762a8135b42bc2c9a10d5d00f424
Noa Dove <nadove@ucsc.edu>
2026-08-03 23:03:46 -0700

    Update AWS CLI to 2.36.15 (#8196)

--- environment.boot
+++ environment.boot
@@ -2,5 +2,5 @@ azul_python_version=3.14.6
 azul_python_image=docker.io/library/python@sha256:1024754b8689b76ec7a8a70cab2956c9f8a63e5af4ebb2c1d43eeb796db1fca3
 azul_docker_version=29.7.1
 azul_terraform_version=1.15.8
-azul_awscli_version=2.36.6
+azul_awscli_version=2.36.15
 azul_ghcli_version=2.96.0
--- environment.py
+++ environment.py
@@ -257,7 +257,7 @@ def env() -> Mapping[str, str | None]:
         # Modifying this variable requires running `make environment.boot` and
         # committing the resulting changes.
         #
-        'azul_awscli_version': '2.36.6',
+        'azul_awscli_version': '2.36.15',
 
         # The version of the GitHub CLI used in the `github_schedule` GitLab CI
         # job to create GitHub issues from the templates.
