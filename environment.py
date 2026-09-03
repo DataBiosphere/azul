806ab1552c3bdfd5e3cb60877a37109a160ffefb
Noa Dove <nadove@ucsc.edu>
2026-09-02 20:11:01 -0700

    Update BigQuery Emulator image (#8268)

--- environment.py
+++ environment.py
@@ -339,7 +339,7 @@ def env() -> Mapping[str, str | None]:
                 'is_custom': False
             },
             'bigquery_emulator': {
-                'ref': 'docker.io/ucscgi/azul-bigquery-emulator:0.4.4-69',
+                'ref': 'docker.io/ucscgi/azul-bigquery-emulator:0.4.4-70',
                 'url': 'https://hub.docker.com/repository/docker/ucscgi/azul-bigquery-emulator',
                 'is_custom': True
             },
